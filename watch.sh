2447a15a73f7c0e1305f55446fdf73b496ce9420
Jeremy Kauffman <jeremy@usetopscore.com>
2016-04-10 16:25:26 -0400

    Update watch.sh

--- watch.sh
+++ watch.sh
@@ -6,10 +6,4 @@ mkdir -p dist/css
 mkdir -p dist/js
 
 sass --watch scss:dist/css --sourcemap=none &
-
-if [ $1 != "jeremyisbadatnode" ]; then
-  babel --presets es2015,react --out-dir dist/js/ --watch js/
-else
-  #yes I am dumb, but not a high prio fix - Jeremy
-  babel --presets /home/jeremy/local/lib/node_modules/babel-preset-es2015,/home/jeremy/local/lib/node_modules/babel-preset-react --out-dir dist/js/ --watch ~/code/lbry-web-ui/js/
-fi
+babel --presets es2015,react --out-dir dist/js/ --watch js/
