4924b8d272447f9e635568cd8b74205b655c2a58
Alex Liebowitz <alex@alexliebowitz.com>
2016-04-22 06:29:13 -0400

    Keep trying to load video until it's available (temporary)

--- js/page/watch.js
+++ js/page/watch.js
@@ -8,10 +8,20 @@ var WatchPage = React.createClass({
   propTypes: {
     name: React.PropTypes.string,
   },
+  updateVideo: function(video, firstRun=true) {
+    if (!video.readyState) {
+      if (!firstRun) { // On the first run, give the browser's initial load a chance to work
+         video.load();
+      }
+      setTimeout(() => {
+        this.updateVideo(video, false);
+      }, 3000);
+    }
+  },
   render: function() {
     return (
       <main>
-        <video style={videoStyle} src={"/view?name=" + this.props.name} controls></video>
+        <video style={videoStyle} src={"/view?name=" + this.props.name} ref={this.updateVideo} controls></video>
       </main>
     );
   }
