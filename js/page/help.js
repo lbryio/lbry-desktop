bbe3bb486257e69859f20a4193ebeb8e07f08e38
Jeremy Kauffman <jeremy@usetopscore.com>
2016-05-20 11:54:53 -0400

    verbiage on help

--- js/page/help.js
+++ js/page/help.js
@@ -5,24 +5,24 @@ var HelpPage = React.createClass({
     return (
       <main>
         <h1>Troubleshooting</h1>
-        <p>Here are the most commonly encountered problems and what to try doing about them</p>
+        <p>Here are the most commonly encountered problems and what to try doing about them.</p>
 
-        <h3>Nothing seems to start downloading</h3>
-        <p>Not all content that you find in the search window is necessarily hosted; LBRY is still young. However, 'wonderfullife' should assuredly be accessible. If you can't download it, and you're not experiencing the below problem, try forwarding ports 4444 and 3333 on your firewall or router.</p>
+        <h3>Nothing seems to start downloading.</h3>
+        <p>If you can't download anything, including 'wonderfullife', try forwarding ports 4444 and 3333 on your firewall or router. If you can access 'wonderfullife' but not other content, it's possible the content is not longer hosted on the network.</p>
 
-        <h3>Videos have trouble playing</h3>
-        <p>This is caused by your video player trying to start the file  while it's still empty. Try reloading the page after a few seconds, it should work. You should also see the file appear in the downloads folder configured in your LBRY settings, which is the gear icon at the top of the main menu.</p>
+        <h3>Videos have trouble playing.</h3>
+        <p>Sometimes your video player will start the file while it's still empty. Try reloading the page after a few seconds and it may work. You should also see the file appear in your downloads folder (configured in <a href="/?settings">settings</a>).</p>
 
         <p>A real fix for this is underway!</p>
 
         <h3>How do I turn LBRY off?</h3>
-        <p>If you're on OS X you can find the app running in the notification area at the top right of your screen; simply click the LBRY icon and choose "Quit."</p>
+        <p>If you're on OS X you can find the app running in the notification area at the top right of your screen. Click the LBRY icon and choose <code>Quit</code>.</p>
 
-        <p>On Linux, you'll find a Close button in the menu at the top right of LBRY.</p>
+        <p>On Linux, you'll find a close button in the menu at the top right of LBRY.</p>
 
-        <p>If you're running LBRY from the command line, you may also close the app with the command "stop-lbrynet-daemon."</p>
+        <p>If you're running LBRY from the command line, you may also close the app with the command <code>stop-lbrynet-daemon</code></p>
 
-        <h3>None of this applies to me, or it didn't work</h3>
+        <h3>None of this applies to me, or it didn't work.</h3>
         <p>Please <Link href="/?report" label="send us a bug report" />. Thanks!</p>
         <section>
           <Link href="/" label="<< Return" />
