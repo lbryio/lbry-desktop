13f7f1995e5425f31c99e5dfb7ce4d7e24dbbfe4
Alex Liebowitz <alex@alexliebowitz.com>
2016-08-22 05:38:17 -0400

    Improve wording and formatting on Help page

--- js/page/help.js
+++ js/page/help.js
@@ -15,7 +15,7 @@ var HelpPage = React.createClass({
         <section className="card">
           <h3>Get Live Help</h3>
           <p>
-            Live help is available most hours in the #help channel of our Slack chat room.
+            Live help is available most hours in the <strong>#help</strong> channel of our Slack chat room.
           </p>
           <p>
             <Link button="alt" label="Join Our Slack" icon="icon-slack" href="https://slack.lbry.io" />
@@ -24,15 +24,17 @@ var HelpPage = React.createClass({
         <section className="card">
           <h3>Common Issues</h3>
           <h4>Nothing seems to start downloading.</h4>
-          <p>If you can't download anything, including 'wonderfullife', try forwarding ports 4444 and 3333 on your firewall or router. If you can access 'wonderfullife' but not other content, it's possible the content is not longer hosted on the network.</p>
+          <p>If you can't download anything, including the Featured Content on the front page, your system may be unable to receive connections from other LBRY users hosting content. If you're able, try forwarding ports 4444 and 3333 on your firewall or router.</p>
+
+          <p>If only certain content is failing to download, the user(s) hosting the file may have disconnected from LBRY, or are having issues with their own connection. We are currently rolling out improvements to the network that will make content more consistently available.</p>
 
           <h4>Videos have trouble playing.</h4>
-          <p>Sometimes your video player will start the file while it's still empty. Try reloading the page after a few seconds and it may work. You should also see the file appear in your downloads folder (configured in <a href="/?settings">settings</a>).</p>
+          <p>Sometimes the video player will start before enough of the file has downloaded to start playing. Try reloading the page after a few seconds. You should also see the file appear in your downloads folder (configured on the <Link href="/?settings" label="Settings page" />).</p>
 
           <p>A real fix for this is underway!</p>
 
           <h4>How do I turn LBRY off?</h4>
-          <p>If you're on OS X you can find the app running in the notification area at the top right of your screen. Click the LBRY icon and choose <code>Quit</code>.</p>
+          <p>If you're on OS X, you can find the app running in the notification area at the top right of your screen. Click the LBRY icon and choose <code>Quit</code>.</p>
 
           <p>On Linux, you'll find a close button in the menu at the top right of LBRY.</p>
 
