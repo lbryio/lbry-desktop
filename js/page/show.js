78ed1b9dcd68d3e09d2daf25e19e8a5a2d00a531
Alex Liebowitz <alex@alexliebowitz.com>
2016-12-07 15:14:47 -0500

    lbry.lighthouse.search() -> lighthouse.search() in two files

--- js/page/show.js
+++ js/page/show.js
@@ -121,7 +121,7 @@ var DetailPage = React.createClass({
   componentWillMount: function() {
     document.title = 'lbry://' + this.props.name;
 
-    lbry.lighthouse.search(this.props.name, (results) => {
+    lighthouse.search(this.props.name, (results) => {
       var result = results[0];
 
       if (result.name != this.props.name) {
