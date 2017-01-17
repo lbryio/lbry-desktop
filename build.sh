42e3681a8dd23a4eed29d7a4c9496231017e4dcd
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2017-01-17 00:58:31 -0600

    make sure to reset each submodule

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ git reset --hard origin/master
 git cherry-pick bd75e88ebebb67897c62a1ee1d3228fd269677dc
 pip install -r requirements.txt
 pip install .
+git reset --hard origin/master
 
 cd ../lbrynet
 pyinstaller lbry.py -y --windowed --onefile --icon=../lbry/packaging/osx/lbry-osx-app/app.icns
@@ -23,6 +24,7 @@ git cherry-pick 06224b1d2cf4bf1f63d95031502260dd9c3ec5c1
 npm install
 node_modules/.bin/node-sass --output dist/css --sourcemap=none scss/
 webpack
+git reset --hard origin/master
 
 cd ..
 cp -R lbry-web-ui/dist electron/
