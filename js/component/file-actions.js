13886f41563e236f9d94fcc2865bdd1949871494
Alex Liebowitz <alex@alexliebowitz.com>
2017-01-23 23:06:29 -0500

    Fix padding issue with download progress bar
    Label was not wrapped in span, so the wrong styles were applying

--- js/component/file-actions.js
+++ js/component/file-actions.js
@@ -176,7 +176,7 @@ let FileActionsRow = React.createClass({
       const
         progress = this.state.fileInfo ? this.state.fileInfo.written_bytes / this.state.fileInfo.total_bytes * 100 : 0,
         label = this.state.fileInfo ? progress.toFixed(0) + '% complete' : 'Connecting...',
-        labelWithIcon = <span className="button__content"><Icon icon="icon-download" />{label}</span>;
+        labelWithIcon = <span className="button__content"><Icon icon="icon-download" /><span>{label}</span></span>;
 
       linkBlock = (
         <div className="faux-button-block file-actions__download-status-bar">
