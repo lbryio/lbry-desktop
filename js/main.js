24d339b5832cfdef8b9b2987740f20e5b93d051b
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2017-01-24 22:20:41 -0500

    only show the splash screen on initial load

--- js/main.js
+++ js/main.js
@@ -13,24 +13,27 @@ var init = function() {
   }
 
   var canvas = document.getElementById('canvas');
-
-  ReactDOM.render(
-    <SplashScreen message="Connecting" onLoadDone={function() {
-      // Redirect to the claim code page if needed. Find somewhere better for this logic
-      if (!localStorage.getItem('claimCodeDone') && window.location.search == '' || window.location.search == '?' || window.location.search == 'discover') {
-        lbry.getBalance((balance) => {
-          if (balance <= 0) {
-            window.location.href = '?claim';
-          } else {
+  if (window.sessionStorage.getItem('loaded') == 'y') {
+    ReactDOM.render(<App/>, canvas)
+  } else {
+    ReactDOM.render(
+	<SplashScreen message="Connecting" onLoadDone={function() {
+	  // Redirect to the claim code page if needed. Find somewhere better for this logic
+	  if (!localStorage.getItem('claimCodeDone') && window.location.search == '' || window.location.search == '?' || window.location.search == 'discover') {
+            lbry.getBalance((balance) => {
+              if (balance <= 0) {
+		window.location.href = '?claim';
+              } else {
+		ReactDOM.render(<App/>, canvas);
+              }
+            });
+	  } else {
             ReactDOM.render(<App/>, canvas);
-          }
-        });
-      } else {
-        ReactDOM.render(<App/>, canvas);
-      }
-    }}/>,
-    canvas
-  );
+	  }
+	}}/>,
+      canvas
+    );
+  }
 };
 
 init();
