34d04255de22bb35b4760ff7533e7a80eab71836
Alex Liebowitz <alex@alexliebowitz.com>
2017-01-31 01:58:25 -0500

    Convert load screens to use CSS

--- js/component/load_screen.js
+++ js/component/load_screen.js
@@ -2,22 +2,6 @@ import React from 'react';
 import lbry from '../lbry.js';
 import {BusyMessage, Icon} from './common.js';
 
-var loadScreenStyle = {
-  color: 'white',
-  backgroundImage: 'url(' + lbry.imagePath('lbry-bg.png') + ')',
-  backgroundSize: 'cover',
-  minHeight: '100vh',
-  minWidth: '100vw',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center'
-}, loadScreenMessageStyle = {
-  marginTop: '24px',
-  width: '325px',
-  textAlign: 'center',
-};
-
 var LoadScreen = React.createClass({
   propTypes: {
     message: React.PropTypes.string.isRequired,
@@ -39,13 +23,13 @@ var LoadScreen = React.createClass({
   render: function() {
     var imgSrc = lbry.imagePath('lbry-white-485x160.png');
     return (
-      <div className="load-screen" style={loadScreenStyle}>
+      <div className="load-screen">
         <img src={imgSrc} alt="LBRY"/>
-        <div style={loadScreenMessageStyle}>
+        <div className="load-screen__message">
           <h3>
             <BusyMessage message={this.props.message} />
           </h3>
-          <Icon icon='icon-warning' style={this.props.isWarning ? {} : { display: 'none' }}/> <span style={ this.state.isWarning ? {} : {'color': '#c3c3c3'} }>{this.props.details}</span>
+          {this.props.isWarning ? <Icon icon="icon-warning" /> : null} <span className={'load-screen__details ' + (this.props.isWarning ? 'load-screen__details--warning' : '')}>{this.props.details}</span>
         </div>
       </div>
     );
