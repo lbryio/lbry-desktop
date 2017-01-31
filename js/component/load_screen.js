810b94fa77b65f0d5dc49fa354c60c2b96146b4f
Alex Liebowitz <alex@alexliebowitz.com>
2017-01-31 02:02:15 -0500

    Add Cancel link when loading screen is lagging

--- js/component/load_screen.js
+++ js/component/load_screen.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import lbry from '../lbry.js';
 import {BusyMessage, Icon} from './common.js';
+import {Link} from '../component/link.js'
 
 var LoadScreen = React.createClass({
   propTypes: {
     message: React.PropTypes.string.isRequired,
     details: React.PropTypes.string,
     isWarning: React.PropTypes.bool,
+  },
+  handleCancelClick: function() {
+    history.back();
   },
   getDefaultProps: function() {
     return {
@@ -21,7 +25,7 @@ var LoadScreen = React.createClass({
     }
   },
   render: function() {
-    var imgSrc = lbry.imagePath('lbry-white-485x160.png');
+    const imgSrc = lbry.imagePath('lbry-white-485x160.png');
     return (
       <div className="load-screen">
         <img src={imgSrc} alt="LBRY"/>
@@ -29,7 +33,10 @@ var LoadScreen = React.createClass({
           <h3>
             <BusyMessage message={this.props.message} />
           </h3>
-          {this.props.isWarning ? <Icon icon="icon-warning" /> : null} <span className={'load-screen__details ' + (this.props.isWarning ? 'load-screen__details--warning' : '')}>{this.props.details}</span>
+          {!this.props.isWarning ? <Icon icon="icon-warning" /> : null} <span className={'load-screen__details ' + (!this.props.isWarning ? 'load-screen__details--warning' : '')}>{this.props.details}</span>
+          {!this.props.isWarning
+            ? <div><Link label="Cancel" onClick={this.handleCancelClick} className='load-screen__cancel-link button-text' /></div>
+            : null}
         </div>
       </div>
     );
