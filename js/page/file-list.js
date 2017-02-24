d3666b119fb433df135166732a3599b13471a542
Alex Liebowitz <alex@alexliebowitz.com>
2017-02-24 00:32:32 -0500

    My Files: Fix sorting comparison functions
     - Properly distinguish between < and = cases
     - Make a copy before sorting (this wasn't causing problems; just good
       practice)

--- js/page/file-list.js
+++ js/page/file-list.js
@@ -133,18 +133,32 @@ export let FileListPublished = React.createClass({
 export let FileList = React.createClass({
   _sortFunctions: {
     date: function(fileInfos) {
-      return fileInfos.reverse();
+      return fileInfos.slice().reverse();
     },
     title: function(fileInfos) {
-      return fileInfos.sort(function(a, b) {
-        return ((a.metadata ? a.metadata.title.toLowerCase() : a.name) >
-                (b.metadata ? b.metadata.title.toLowerCase() : b.name));
+      return fileInfos.slice().sort(function(fileInfo1, fileInfo2) {
+        const title1 = fileInfo1.metadata ? fileInfo1.metadata.title.toLowerCase() : fileInfo1.name;
+        const title2 = fileInfo2.metadata ? fileInfo2.metadata.title.toLowerCase() : fileInfo2.name;
+        if (title1 < title2) {
+          return -1;
+        } else if (title1 > title2) {
+          return 1;
+        } else {
+          return 0;
+        }
       });
     },
     filename: function(fileInfos) {
-      return fileInfos.sort(function(a, b) {
-        return (a.file_name.toLowerCase() >
-                b.file_name.toLowerCase());
+      return fileInfos.slice().sort(function({file_name: fileName1}, {file_name: fileName2}) {
+        const fileName1Lower = fileName1.toLowerCase();
+        const fileName2Lower = fileName2.toLowerCase();
+        if (fileName1Lower < fileName2Lower) {
+          return -1;
+        } else if (fileName2Lower > fileName1Lower) {
+          return 1;
+        } else {
+          return 0;
+        }
       });
     },
   },
