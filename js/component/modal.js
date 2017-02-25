08c4a5173a3d786a077b8fbfb15d2977d7308084
Alex Liebowitz <alex@alexliebowitz.com>
2017-02-25 02:56:57 -0500

    Add new ExpandableModal component

--- js/component/modal.js
+++ js/component/modal.js
@@ -3,7 +3,7 @@ import ReactModal from 'react-modal';
 import {Link} from './link.js';
 
 
-var Modal = React.createClass({
+export const Modal = React.createClass({
   propTypes: {
     type: React.PropTypes.oneOf(['alert', 'confirm', 'custom']),
     onConfirmed: React.PropTypes.func,
@@ -39,6 +39,45 @@ var Modal = React.createClass({
               <Link button="primary" label={this.props.confirmButtonLabel} className="modal__button" disabled={this.props.confirmButtonDisabled} onClick={this.props.onConfirmed} />
             </div>}
       </ReactModal>
+    );
+  }
+});
+
+export const ExpandableModal = React.createClass({
+  propTypes: {
+    expandButtonLabel: React.PropTypes.string,
+    extraContent: React.PropTypes.element,
+  },
+  getDefaultProps: function() {
+    return {
+      confirmButtonLabel: 'OK',
+      expandButtonLabel: 'Show More...',
+      hideButtonLabel: 'Show Less',
+    }
+  },
+  getInitialState: function() {
+    return {
+      expanded: false,
+    }
+  },
+  toggleExpanded: function() {
+    this.setState({
+      expanded: !this.state.expanded,
+    });
+  },
+  render: function() {
+    return (
+      <Modal type="custom" {... this.props}>
+        {this.props.children}
+        {this.state.expanded
+          ? this.props.extraContent
+          : null}
+        <div className="modal__buttons">
+          <Link button="primary" label={this.props.confirmButtonLabel} className="modal__button" onClick={this.props.onConfirmed} />
+          <Link button="alt" label={!this.state.expanded ? this.props.expandButtonLabel : this.props.hideButtonLabel}
+                className="modal__button" onClick={this.toggleExpanded} />
+        </div>
+      </Modal>
     );
   }
 });
