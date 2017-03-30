9be8a92fc06ee0e02a3ba92686d84abe7b9d326f
Alex Liebowitz <alex@alexliebowitz.com>
2017-03-30 19:00:33 -0400

    Progress toward register page

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import lbry from './lbry.js';
+import lbryio from './lbryio.js';
 import lighthouse from './lighthouse.js';
 import App from './app.js';
 import SplashScreen from './component/splash.js';
@@ -16,7 +17,7 @@ window.addEventListener('contextmenu', (event) => {
   event.preventDefault();
 });
 
-var init = function() {
+let init = function() {
   window.lbry = lbry;
   window.lighthouse = lighthouse;
 
@@ -27,8 +28,10 @@ var init = function() {
     ReactDOM.render(
       (
         <SplashScreen message="Connecting" onLoadDone={function() {
-          // Redirect to the claim code page if needed. Find somewhere better for this logic
-          if (!localStorage.getItem('claimCodeDone') && ['', '?', 'discover'].contains(window.location.search)) {
+          // There are a couple of conditions where we want to preempt loading the app and send the user to a
+          // different page. TODO: Find a better place for this logic.
+
+          if (!localStorage.getItem('claimCodeDone') && ['', '?', 'discover'].includes(window.location.search)) {
             lbry.getBalance((balance) => {
               if (balance <= 0) {
                 window.location.href = '?claim';
@@ -40,9 +43,30 @@ var init = function() {
             ReactDOM.render(<App/>, canvas);
           }
         }}/>
-      ),
-      canvas);
+      ), canvas);
   }
 };
 
-init();
+if (localStorage.getItem('accessToken') || window.location.search == '?register') {
+  // User is already registered, or on the registration page
+  init();
+} else {
+  // Send 
+  lbry.status().then(({installation_id}) => {
+    installation_id += parseInt(Date.now(), 10); // temp
+    installation_id += "X".repeat(96 - installation_id.length); // temp
+    lbryio.call('user_install', 'exists', {app_id: installation_id}).then((userExists) => {
+      if (userExists) {
+        /* TODO: somehow user exists with the same installation ID, but we don't have the token recorded. What do we do here? */
+      } else {
+        lbryio.call('user', 'new', {
+          language: 'en',
+          app_id: installation_id,
+        }, 'post').then(({ID}) => {
+          localStorage.setItem('accessToken', ID);
+          window.location = '?register';
+        });
+      }
+    });
+  });
+}
