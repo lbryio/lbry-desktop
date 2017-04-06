487cccdc997ea853990210cf93bbfae0759f62e4
Alex Grintsvayg <alex@grin.io>
2017-04-06 18:38:18 -0400

    fixes for windows

--- daemon/build.ps1
+++ daemon/build.ps1
@@ -5,7 +5,7 @@ gcc --version
 mingw32-make --version
 
 mkdir temp
-Invoke-WebRequest "https://pypi.python.org/packages/55/90/e987e28ed29b571f315afea7d317b6bf4a551e37386b344190cffec60e72/miniupnpc-1.9.tar.gz" -OutFile "temp\miniupnpc-1.9.tar.gz"
+mv miniupnpc-1.9.tar.gz temp\miniupnpc-1.9.tar.gz
 cd temp
 tar zxf miniupnpc-1.9.tar.gz
 cd miniupnpc-1.9
@@ -16,11 +16,13 @@ python.exe setupmingw32.py install
 cd ..\..\
 Remove-Item -Recurse -Force temp
 
-# copy requirements from lbry, but remove lbryum (we'll add it back in below)
-Get-Content ..\lbry\requirements.txt | Where-Object {$_ -notmatch 'lbryum'} | Set-Content requirements.txt
+# copy requirements from lbry, but remove lbryum (we'll add it back in below) and gmpy and miniupnpc (installed manually)
+$Skip = @("*lbryum*","*gmpy*","*miniupnpc*")
+Get-Content ..\lbry\requirements.txt | Where-Object {$_ -notlike $Skip} | Set-Content requirements.txt
 # for electron, we install lbryum and lbry using submodules
 Add-Content requirements.txt "`n../lbryum"
 Add-Content requirements.txt "`n../lbry"
+Get-Content requirements.txt
 
 pip.exe install pyinstaller
 pip.exe install -r windows.txt
