57fbd2bd7aeb71c112d91e21fe1ad63176371551
Jeremy Kauffman <kauffj@gmail.com>
2017-04-20 21:11:58 -0400

    remove console

--- ui/js/component/link/view.js
+++ ui/js/component/link/view.js
@@ -14,8 +14,7 @@ const Link = (props) => {
     hidden,
     disabled,
   } = props
-
-console.log(props);
+  
   const className = (props.className || '') +
     (!props.className && !props.button ? 'button-text' : '') + // Non-button links get the same look as text buttons
     (props.button ? ' button-block button-' + props.button + ' button-set-item' : '') +
