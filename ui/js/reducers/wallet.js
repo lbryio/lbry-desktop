4fe3fe5dcb307c4d75e67f6f8423bfa8afc59402
6ea86b96 <6ea86b96@gmail.com>
2017-04-23 12:55:47 +0700

    Progress on sending transactions

--- ui/js/reducers/wallet.js
+++ ui/js/reducers/wallet.js
@@ -2,12 +2,18 @@ import * as types from 'constants/action_types'
 
 const reducers = {}
 const address = sessionStorage.getItem('receiveAddress')
+const buildDraftTransaction = () => ({
+  amount: undefined,
+  address: undefined
+})
+
 const defaultState = {
   balance: 0,
   transactions: [],
   fetchingTransactions: false,
   receiveAddress: address,
   gettingNewAddress: false,
+  draftTransaction: buildDraftTransaction()
 }
 
 reducers[types.FETCH_TRANSACTIONS_STARTED] = function(state, action) {
@@ -66,6 +72,55 @@ reducers[types.CHECK_ADDRESS_IS_MINE_STARTED] = function(state, action) {
 reducers[types.CHECK_ADDRESS_IS_MINE_COMPLETED] = function(state, action) {
   return Object.assign({}, state, {
     checkingAddressOwnership: false
+  })
+}
+
+reducers[types.SET_DRAFT_TRANSACTION_AMOUNT] = function(state, action) {
+  const oldDraft = state.draftTransaction
+  const newDraft = Object.assign({}, oldDraft, {
+    amount: parseFloat(action.data.amount)
+  })
+
+  return Object.assign({}, state, {
+    draftTransaction: newDraft
+  })
+}
+
+reducers[types.SET_DRAFT_TRANSACTION_ADDRESS] = function(state, action) {
+  const oldDraft = state.draftTransaction
+  const newDraft = Object.assign({}, oldDraft, {
+    address: action.data.address
+  })
+
+  return Object.assign({}, state, {
+    draftTransaction: newDraft
+  })
+}
+
+reducers[types.SEND_TRANSACTION_STARTED] = function(state, action) {
+  const newDraftTransaction = Object.assign({}, state.draftTransaction, {
+    sending: true
+  })
+
+  return Object.assign({}, state, {
+    draftTransaction: newDraftTransaction
+  })
+}
+
+reducers[types.SEND_TRANSACTION_COMPLETED] = function(state, action) {
+  return Object.assign({}, state, {
+    draftTransaction: buildDraftTransaction()
+  })
+}
+
+reducers[types.SEND_TRANSACTION_FAILED] = function(state, action) {
+  const newDraftTransaction = Object.assign({}, state.draftTransaction, {
+    sending: false,
+    error: action.data.error
+  })
+
+  return Object.assign({}, state, {
+    draftTransaction: newDraftTransaction
   })
 }
 
