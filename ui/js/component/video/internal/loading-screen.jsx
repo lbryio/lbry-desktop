11fa00504df2d650e9c24cd851a8d3955e09e3bc
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-06-07 09:06:14 -0700

    switch to BEM naming pattern

--- ui/js/component/video/internal/loading-screen.jsx
+++ ui/js/component/video/internal/loading-screen.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const LoadingScreen = ({ status }) =>
-  <div className="video--loading--screen">
-    <div className="loading--screen--content">
-      <div className="loading--spinner" />
+  <div className="video--loading-screen">
+    <div className="video--loading-screen-content">
+      <div className="video--loading-spinner" />
 
-      <div className="video--loading--status">
+      <div className="video--loading-status">
         {status}
       </div>
     </div>
