52b404b552038da52096268c71e6a82840509126
Akinwale Ariwodola <akinwale@gmail.com>
2017-07-06 04:14:45 +0100

    Issue #312 home page scroll position on back navigation

--- ui/js/page/discover/view.jsx
+++ ui/js/page/discover/view.jsx
@@ -3,6 +3,7 @@ import lbryio from "lbryio.js";
 import lbryuri from "lbryuri";
 import FileCard from "component/fileCard";
 import { BusyMessage } from "component/common.js";
+import { setSession, getSession } from "utils";
 import ToolTip from "component/tooltip.js";
 
 const FeaturedCategory = props => {
@@ -37,10 +38,27 @@ const FeaturedCategory = props => {
 class DiscoverPage extends React.PureComponent {
   componentWillMount() {
     this.props.fetchFeaturedUris();
+    this.scrollListener = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    const scrollY = parseInt(getSession("prefs_scrolly"));
+    if (!isNaN(scrollY)) {
+      const restoreScrollPosition = () => {
+        window.scrollTo(0, scrollY);
+      };
+      setTimeout(restoreScrollPosition, 100);
+    }
+    window.addEventListener("scroll", this.scrollListener);
+  }
+
+  handleScroll() {
+    setSession("prefs_scrolly", window.scrollY);
   }
 
   componentWillUnmount() {
     this.props.cancelResolvingUris();
+    window.removeEventListener("scroll", this.scrollListener);
   }
 
   render() {
@@ -51,7 +69,7 @@ class DiscoverPage extends React.PureComponent {
         (featuredUris !== undefined && Object.keys(featuredUris).length === 0));
 
     return (
-      <main>
+      <main ref="main">
         {fetchingFeaturedUris &&
           <BusyMessage message={__("Fetching content")} />}
         {typeof featuredUris === "object" &&
