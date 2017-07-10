271a7d82c9d3d450afbd8b080c5c560a0c0dbec5
Alex Liebowitz <alex@alexliebowitz.com>
2017-07-10 00:45:45 -0400

    Make app-level configuration available in UI

--- ui/js/app.js
+++ ui/js/app.js
@@ -2,7 +2,10 @@ import store from "store.js";
 import lbry from "./lbry.js";
 
 const env = ENV;
-const config = require(`./config/${env}`);
+const config = {
+  ...require(`./config/${env}`),
+  appConfig: require("../../app/config.json"),
+};
 const language = lbry.getClientSetting("language")
   ? lbry.getClientSetting("language")
   : "en";
