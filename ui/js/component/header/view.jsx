bbfc72b4b590d939e94441938bbf05cd11a6be82
Baltazar Gomez <btzr.io@gmail.com>
2017-08-07 22:10:42 -0600

    add forward button

--- ui/js/component/header/view.jsx
+++ ui/js/component/header/view.jsx
@@ -3,12 +3,15 @@ import Link from "component/link";
 import WunderBar from "component/wunderbar";
 
 export const Header = props => {
-  const { balance, back, navigate, publish } = props;
+  const { balance, back, forward, navigate, publish } = props;
 
   return (
     <header id="header">
       <div className="header__item">
         <Link onClick={back} button="alt button--flat" icon="icon-arrow-left" title={__("Back")} />
+      </div>
+       <div className="header__item">
+        <Link onClick={forward} button="alt button--flat" icon="icon-arrow-rigt" title={__("Forward")} />
       </div>
       <div className="header__item">
         <Link
