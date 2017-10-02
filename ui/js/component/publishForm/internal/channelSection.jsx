a7a4e8dc163b021a225cacf7f8d2dbfedf0ffcb4
Jeremy Kauffman <kauffj@gmail.com>
2017-10-02 08:40:42 -0400

    remove channel length minimum, fixes #645

--- ui/js/component/publishForm/internal/channelSection.jsx
+++ ui/js/component/publishForm/internal/channelSection.jsx
@@ -53,13 +53,6 @@ class ChannelSection extends React.PureComponent {
   }
 
   handleCreateChannelClick(event) {
-    if (this.state.newChannelName.length < 5) {
-      this.refs.newChannelName.showError(
-        __("LBRY channel names must be at least 5 characters in length.")
-      );
-      return;
-    }
-
     this.setState({
       creatingChannel: true,
     });
