c33b7df732f0481d6f0bfb1c000038abf63b91bf
Jeremy Kauffman <kauffj@gmail.com>
2017-11-07 09:35:49 -0500

    increase modal width by 20px, fixes #709

--- ui/js/modal/modalCreditIntro/view.jsx
+++ ui/js/modal/modalCreditIntro/view.jsx
@@ -4,8 +4,7 @@ import { CreditAmount, CurrencySymbol } from "component/common";
 import Link from "component/link/index";
 
 const ModalCreditIntro = props => {
-  const { closeModal, currentBalance, totalRewardValue, verifyAccount } = props;
-
+  const { closeModal, totalRewardValue, verifyAccount } = props;
   const totalRewardRounded = Math.round(totalRewardValue / 10) * 10;
 
   return (
