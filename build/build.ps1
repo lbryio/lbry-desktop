1953faf929a4dc4025d8939f61c0c47b236cb6e1
Liam Cardenas <me@liamcardenas.com>
2017-12-12 11:43:32 -0800

    Unencrpyted before building

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,13 @@
 pip install -r build\requirements.txt
 
+# sign binary
+nuget install secure-file -ExcludeVersion
+secure-file\tools\secure-file -decrypt build\lbry3.pfx.enc -secret "$env:pfx_key"
+Write-Host "Testing for lbry3"
+Test-Path build\lbry3.pfx
+
+# & ${env:SIGNTOOL_PATH} sign /f build\lbry3.pfx /p "$env:key_pass" /tr http://tsa.starfieldtech.com /td SHA256 /fd SHA256 dist\*.exe
+
 # Get the latest stable version of Node.js or io.js
 Install-Product node $env:nodejs_version
 npm install -g yarn
@@ -21,19 +29,8 @@ Expand-Archive daemon.zip -DestinationPath static\daemon\
 dir static\daemon\ # verify that daemon binary is there
 rm daemon.zip
 
-
 # build electron app
 yarn dist
 dir dist # verify that binary was built/named correctly
-
-
-# sign binary
-nuget install secure-file -ExcludeVersion
-secure-file\tools\secure-file -decrypt build\lbry3.pfx.enc -secret "$env:pfx_key"
-Write-Host "Testing for lbry3"
-Test-Path build\lbry3.pfx
-
-# & ${env:SIGNTOOL_PATH} sign /f build\lbry3.pfx /p "$env:key_pass" /tr http://tsa.starfieldtech.com /td SHA256 /fd SHA256 dist\*.exe
-
 
 python build\upload_assets.py
