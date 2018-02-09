2786f9bffe335a2f554119fa3004c5b15cddf826
Igor Gassmann <igor@lbry.io>
2018-02-08 21:58:58 -0300

    .

--- build/downloadDaemon.js
+++ build/downloadDaemon.js
@@ -1,13 +1,11 @@
 /* eslint-disable */
-var https = require('https');
-var path = require('path');
-var fs = require('fs');
-var packageJSON = require('../package.json');
-var AdmZip = require('adm-zip');
-var axios = require('axios');
-var decompress = require('decompress');
+const path = require('path');
+const fs = require('fs');
+const packageJSON = require('../package.json');
+const axios = require('axios');
+const decompress = require('decompress');
 
-module.exports = function (context) {
+module.exports = function(context) {
   const daemonURLTemplate = packageJSON.lbrySettings.lbrynetDaemonUrlTemplate;
   const daemonVersion = packageJSON.lbrySettings.lbrynetDaemonVersion;
   let currentPlatform = context.platform.toString();
@@ -18,8 +16,9 @@ module.exports = function (context) {
     .replace(/OSNAME/g, currentPlatform);
   const tmpZipPath = 'build/daemon.zip';
 
-  return new Promise((resolve) => {
-    axios.request({
+  return new Promise(resolve => {
+    axios
+      .request({
         responseType: 'arraybuffer',
         url: daemonURL,
         method: 'get',
@@ -27,17 +26,14 @@ module.exports = function (context) {
           'Content-Type': 'application/zip',
         },
       })
-      .then((result) => {
+      .then(result => {
         fs.writeFileSync(tmpZipPath, result.data);
         return true;
       })
       .then(() => {
         return decompress(tmpZipPath, 'static/daemon', {
-          filter: file => path.basename(file.path) === 'lbrynet-daemon'
+          filter: file => path.basename(file.path) === 'lbrynet-daemon',
         });
-        // const zip = new AdmZip(tmpZipPath);
-        // zip.extractEntryTo('lbrynet-daemon', 'static/daemon', false, true);
-        return true;
       })
       .then(() => {
         return resolve(true);
