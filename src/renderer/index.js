1c193dfeae2870169b64129d7761cb107d47771e
Jeremy Curcio <jeremy1026@gmail.com>
2018-02-15 22:30:53 -0500

    Fix for loss of data when pasting via context menu

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -23,8 +23,10 @@ const { autoUpdater } = remote.require('electron-updater');
 autoUpdater.logger = remote.require("electron-log");
 
 window.addEventListener('contextmenu', event => {
-  contextMenu(remote.getCurrentWindow(), event.x, event.y, app.env === 'development');
-  event.preventDefault();
+  if (typeof(contextMenu) !== 'undefined') {
+    contextMenu(remote.getCurrentWindow(), event.x, event.y, app.env === 'development');
+    event.preventDefault();
+  }
 });
 
 ipcRenderer.on('open-uri-requested', (event, uri, newSession) => {
