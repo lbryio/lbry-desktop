f786218e1e69aa52d015efd3dba8e2e977408fc7
travis <travis@lbry.io>
2018-02-26 12:47:09 -0500

    remove lbryuri import

--- src/renderer/component/speechUpload/view.jsx
+++ src/renderer/component/speechUpload/view.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import lbry from "lbry";
-import lbryuri from "lbryuri";
 import FormField from "component/formField";
 import { Form, FormRow, Submit } from "component/form.js";
 import Link from "component/link";
