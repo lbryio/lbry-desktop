83a548de72f351c2459ac74cdb6394246ba8b333
travis <travis@lbry.io>
2018-02-26 12:32:25 -0500

    add speech upload container

---
+++ src/renderer/component/speechUpload/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+import SpeechUpload from "./view";
+import { doOpenModal } from "redux/actions/app";
+import {
+  beginSpeechUpload,
+  resetSpeechUpload,
+  setThumbnailStatusManual,
+  setManualThumbnailUrl,
+} from "redux/actions/upload";
+
+const perform = dispatch => ({
+  openModal: (modal, props) => dispatch(doOpenModal(modal, props)),
+  upload: (path, nsfw) => dispatch(beginSpeechUpload(path, nsfw)),
+  resetUpload: () => dispatch(resetSpeechUpload()),
+  setManualStatus: () => dispatch(setThumbnailStatusManual()),
+  setManualUrl: url => dispatch(setManualThumbnailUrl(url)),
+});
+
+export default connect(null, perform)(SpeechUpload);
