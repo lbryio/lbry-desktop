24df153a996a1e0a0c2cdfb9c23286de110e87e6
travis <travis@lbry.io>
2018-03-02 10:22:33 -0500

    copy subscriptions methods and components to discover

--- src/renderer/page/discover/index.js
+++ src/renderer/page/discover/index.js
@@ -2,15 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { doFetchFeaturedUris } from 'redux/actions/content';
 import { selectFeaturedUris, selectFetchingFeaturedUris } from 'redux/selectors/content';
+import {
+  selectSubscriptionsFromClaims,
+  selectSubscriptions,
+  selectHasFetchedSubscriptions,
+} from 'redux/selectors/subscriptions';
+import { doFetchClaimsByChannel } from 'redux/actions/content';
+import { setHasFetchedSubscriptions } from 'redux/actions/subscriptions';
 import DiscoverPage from './view';
+
+
 
 const select = state => ({
   featuredUris: selectFeaturedUris(state),
   fetchingFeaturedUris: selectFetchingFeaturedUris(state),
+  hasFetchedSubscriptions: state.subscriptions.hasFetchedSubscriptions,
+  savedSubscriptions: selectSubscriptions(state),
+  subscriptions: selectSubscriptionsFromClaims(state),
 });
 
 const perform = dispatch => ({
   fetchFeaturedUris: () => dispatch(doFetchFeaturedUris()),
+  doFetchClaimsByChannel: channel => doFetchClaimsByChannel(channel),
+  setHasFetchedSubscriptions: () => setHasFetchedSubscriptions(),
 });
 
 export default connect(select, perform)(DiscoverPage);
--- src/renderer/page/discover/view.jsx
+++ src/renderer/page/discover/view.jsx
@@ -9,17 +9,74 @@ import SubHeader from 'component/subHeader';
 import classnames from 'classnames';
 import Link from 'component/link';
 import FeaturedCategory from 'component/featuredCategory';
+import type { Subscription } from 'redux/reducers/subscriptions';
 
-class DiscoverPage extends React.PureComponent {
+type SavedSubscriptions = Array<Subscription>;
+
+type Props = {
+  doFetchClaimsByChannel: (string, number) => any,
+  savedSubscriptions: SavedSubscriptions,
+  // TODO build out claim types
+  subscriptions: Array<any>,
+  setHasFetchedSubscriptions: () => void,
+  hasFetchedSubscriptions: boolean,
+};
+
+
+class DiscoverPage extends React.PureComponent<Props> {
   componentWillMount() {
     this.props.fetchFeaturedUris();
   }
 
+
+  componentDidMount() {
+    const { savedSubscriptions, setHasFetchedSubscriptions } = this.props;
+    if (savedSubscriptions.length > 0) {
+      this.fetchSubscriptions(savedSubscriptions);
+      setHasFetchedSubscriptions();
+    }
+  }
+
+  componentWillReceiveProps(props: Props) {
+    const { savedSubscriptions, hasFetchedSubscriptions, setHasFetchedSubscriptions } = props;
+
+    if (!hasFetchedSubscriptions && savedSubscriptions.length) {
+      this.fetchSubscriptions(savedSubscriptions);
+      setHasFetchedSubscriptions();
+    }
+  }
+
+  fetchSubscriptions(savedSubscriptions: SavedSubscriptions) {
+    const { doFetchClaimsByChannel } = this.props;
+    if (savedSubscriptions.length) {
+      // can this use batchActions?
+      savedSubscriptions.forEach(sub => {
+        doFetchClaimsByChannel(sub.uri, 1);
+      });
+    }
+  }
+
+
   render() {
-    const { featuredUris, fetchingFeaturedUris } = this.props;
+    const {
+      featuredUris,
+      fetchingFeaturedUris,
+      subscriptions,
+      savedSubscriptions,
+    } = this.props;
     const hasContent = typeof featuredUris === 'object' && Object.keys(featuredUris).length,
       failedToLoad = !fetchingFeaturedUris && !hasContent;
 
+    if (subscriptions) console.log("subscriptions:", subscriptions);
+
+    const someClaimsNotLoaded = Boolean(
+      subscriptions.find(subscription => !subscription.claims.length)
+    );
+
+    const fetchingSubscriptions =
+      !!savedSubscriptions.length &&
+      (subscriptions.length !== savedSubscriptions.length || someClaimsNotLoaded);
+  
     return (
       <main
         className={classnames('main main--no-margin', {
@@ -31,17 +88,37 @@ class DiscoverPage extends React.PureComponent {
         {hasContent &&
           Object.keys(featuredUris).map(
             uri =>
-              featuredUris[uri].length ? (
+              featuredUris[uri].length > 0 ? (
                 <FeaturedCategory
                   key={uri}
                   category={uri}
                   uris={featuredUris[uri]}
                 />
-              ) : (
-                ''
-              )
+              ) : null
           )}
         {failedToLoad && <div className="empty">{__('Failed to load landing content.')}</div>}
+        {!!savedSubscriptions.length && (
+          <div>
+            {!!subscriptions.length &&
+              subscriptions.map(subscription => {
+                if (!subscription.claims.length) {
+                  // will need to update when you can subscribe to empty channels
+                  // for now this prevents issues with FeaturedCategory being rendered
+                  // before the names (claim uris) are populated
+                  return '';
+                }
+
+                return (
+                  <FeaturedCategory
+                    key={subscription.channelName}
+                    categoryLink={subscription.uri}
+                    category={subscription.channelName}
+                    uris={subscription.claims}
+                  />
+                );
+              })}
+          </div>
+        )}
       </main>
     );
   }
