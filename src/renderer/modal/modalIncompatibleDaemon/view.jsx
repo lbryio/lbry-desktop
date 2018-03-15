0230757bb644faf5d902726701cdd1a0cb6f2bd6
Igor Gassmann <igor@lbry.io>
2018-03-15 16:39:52 -0400

    Add option to kill running daemon when incompatible

--- src/renderer/modal/modalIncompatibleDaemon/view.jsx
+++ src/renderer/modal/modalIncompatibleDaemon/view.jsx
@@ -1,18 +1,28 @@
+// @flow
+
 import React from 'react';
 import { Modal } from 'modal/modal';
 import Link from 'component/link/index';
+import type { Dispatch } from 'src/renderer/redux/actions/shape_shift';
 
-class ModalIncompatibleDaemon extends React.PureComponent {
+type Props = {
+  quit: Dispatch,
+  quitAnyDaemon: Dispatch,
+};
+
+class ModalIncompatibleDaemon extends React.PureComponent<Props> {
   render() {
-    const { quit } = this.props;
+    const { quit, quitAnyDaemon } = this.props;
 
     return (
       <Modal
         isOpen
         contentLabel={__('Incompatible daemon running')}
         type="alert"
-        confirmButtonLabel={__('Quit')}
-        onConfirmed={quit}
+        confirmButtonLabel={__('Quit daemon')}
+        abortButtonLabel={__('Do nothing')}
+        onConfirmed={quitAnyDaemon}
+        onAborted={quit}
       >
         {__(
           'This browser is running with an incompatible version of the LBRY protocol and your install must be repaired. '
