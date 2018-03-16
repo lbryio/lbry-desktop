f74f4fbc229d9bc765b71a82afe3217d62037441
liamcardenas <liamcardenas@lbry.io>
2018-03-16 12:04:22 -0700

    Updated copy of subscriptions modal

--- src/renderer/modal/modalFirstSubscription/view.jsx
+++ src/renderer/modal/modalFirstSubscription/view.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Modal } from 'modal/modal';
-import Link from 'component/link/index';
+import Link from 'component/link';
 
 const ModalFirstSubscription = props => {
-  const { closeModal } = props;
+  const { closeModal, navigate } = props;
 
   return (
-    <Modal type="custom" isOpen contentLabel="Your first subscription!">
+    <Modal type="custom" isOpen contentLabel="Subscriptions 101">
       <section>
-        <h3 className="modal__header">{__('Your first subscription!')}</h3>
+        <h3 className="modal__header">{__('Subscriptions 101')}</h3>
+        <p>{__('You just subscribed to your first channel. Awesome!')}</p>
+        <p>{__('A few quick things to know:')}</p>
+        <p>
+          {__('1) You can use the')}{' '}
+          <Link
+            label={__('Subscriptions Page')}
+            onClick={() => {
+              navigate('/subscriptions');
+              closeModal();
+            }}
+          />{' '}
+          {__('to view content across all of your subscribed channels.')}
+        </p>
         <p>
           {__(
-            'When you subscribe to a channel, you will automatically download, and be notified of, all of its new content.'
+            '2) This app will automatically download new free content from channels you are subscribed to.'
+          )}
+        </p>
+        <p>
+          {__(
+            '3) If we have your email address, we may send you notifications and rewards related to new content.'
           )}
         </p>
         <div className="modal__buttons">
