0a74a0e520d63d0d5dcf86d45b847187d0e11c95
btzr-io <btzr.io@gmail.com>
2018-03-21 22:46:15 -0700

    fix array iterations

--- src/renderer/util/parseData.js
+++ src/renderer/util/parseData.js
@@ -17,23 +17,28 @@ const parseJson = (data, filters = []) => {
 // https://gist.github.com/btzr-io/55c3450ea3d709fc57540e762899fb85
 const parseCsv = (data, filters = []) => {
   // Get items for header
-  const getHeaders = temp =>
-    Object.entries(temp)
-      .map(([key]) => {
-        if (!filters.includes(key)) return key;
-      })
-      .join(',');
+  const getHeaders = item => {
+    const list = [];
+    // Apply filters
+    Object.entries(temp).forEach(([key]) => {
+      if (!filters.includes(key)) list.push(key);
+    });
+    // return headers
+    return list.join(',');
+  };
 
   // Get rows content
   const getData = list =>
     list
-      .map(item =>
-        Object.entries(item)
-          .map(([key, value]) => {
-            if (!filters.includes(key)) return value;
-          })
-          .join(',')
-      )
+      .map(item => {
+        const row = [];
+        // Apply filters
+        Object.entries(item).forEach(([key, value]) => {
+          if (!filters.includes(key)) row.push(value);
+        });
+        // return rows
+        return row.join(',');
+      })
       .join('\n');
 
   // Return CSV string
