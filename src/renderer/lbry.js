0a6be3e34519c3243f7d1965c6c1ded401907008
btzr-io <btzr.io@gmail.com>
2018-03-22 11:09:39 -0700

    make getMediaType return full mimeType

--- src/renderer/lbry.js
+++ src/renderer/lbry.js
@@ -174,24 +174,19 @@ Lbry.publishDeprecated = (params, fileListedCallback, publishedCallback, errorCa
 
 Lbry.imagePath = file => `file://${__static}/img/${file}`;
 
-Lbry.getMediaType = (contentType, fileName) => {
-  if (contentType) {
-    return /^[^/]+/.exec(contentType)[0];
-  } else if (fileName) {
-    const dotIndex = fileName.lastIndexOf('.');
-    if (dotIndex === -1) {
-      return 'unknown';
-    }
+Lbry.getMediaType = (fileName = '') => {
+  // No need for this: if(contentType) return /^[^/]+/.exec(contentType)[0];
+  const dotIndex = fileName.lastIndexOf('.');
+  if (dotIndex === -1) return 'unknown';
 
-    const ext = fileName.substr(dotIndex + 1);
-    if (/^mp4|m4v|webm|flv|f4v|ogv$/i.test(ext)) {
-      return 'video';
-    } else if (/^mp3|m4a|aac|wav|flac|ogg|opus$/i.test(ext)) {
-      return 'audio';
-    } else if (/^html|htm|xml|pdf|odf|doc|docx|md|markdown|txt|epub|org$/i.test(ext)) {
-      return 'document';
-    }
-    return 'unknown';
+  const ext = fileName.substr(dotIndex + 1);
+
+  if (/^mp4|m4v|webm|flv|f4v|ogv$/i.test(ext)) {
+    return `video/${ext}`;
+  } else if (/^mp3|m4a|aac|wav|flac|ogg|opus$/i.test(ext)) {
+    return `audio/${ext}`;
+  } else if (/^html|htm|xml|pdf|odf|doc|docx|md|markdown|txt|epub|org$/i.test(ext)) {
+    return `document/${ext}`;
   }
   return 'unknown';
 };
