de8f116709132f44d7cbf5d6f6f640c685597aa9
Travis Eden <daovist@gmail.com>
2018-04-02 09:59:23 -0400

    allow custom labels in file-selector

--- src/renderer/component/common/file-selector.jsx
+++ src/renderer/component/common/file-selector.jsx
@@ -9,6 +9,8 @@ type Props = {
   type: string,
   currentPath: ?string,
   onFileChosen: (string, string) => void,
+  fileLabel: ?string,
+  directoryLabel: ?string,
 };
 
 class FileSelector extends React.PureComponent<Props> {
@@ -47,15 +49,14 @@ class FileSelector extends React.PureComponent<Props> {
   input: ?HTMLInputElement;
 
   render() {
-    const { type, currentPath } = this.props;
+    const { type, currentPath, fileLabel, directoryLabel } = this.props;
+
+    const label =
+      type === 'file' ? fileLabel || __('Choose File') : directoryLabel || __('Choose Directory');
 
     return (
       <FormRow verticallyCentered padded>
-        <Button
-          button="primary"
-          onClick={() => this.handleButtonClick()}
-          label={type === 'file' ? __('Choose File') : __('Choose Directory')}
-        />
+        <Button button="primary" onClick={() => this.handleButtonClick()} label={label} />
         <input
           webkitdirectory="true"
           className="input-copyable"
