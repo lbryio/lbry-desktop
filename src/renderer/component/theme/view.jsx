66b63a27c767d8dca7bcfecab2dfbb2151601e12
Sean Yesmunt <sean.yesmunt@gmail.com>
2018-04-04 00:07:17 -0400

    fix: force light mode

--- src/renderer/component/theme/view.jsx
+++ src/renderer/component/theme/view.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 const Theme = props => {
   const { themePath } = props;
 
-  if (!themePath) {
-    return null;
-  }
+  // Force light mode while until dark mode is ready
+  // This is so we don't have to change users settings for them
+  return null;
 
-  return <link href={themePath} rel="stylesheet" type="text/css" media="screen,print" />;
+  // if (!themePath) {
+  //   return null;
+  // }
+  //
+  // return <link href={themePath} rel="stylesheet" type="text/css" media="screen,print" />;
 };
 
 export default Theme;
