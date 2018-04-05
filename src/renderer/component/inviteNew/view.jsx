1e2a0bd36da00b73b7aae238a09f56e52e4080b6
YULIUS KURNIAWAN KRISTIANTO <yuliuskristianto9@gmail.com>
2018-04-05 21:55:24 +0700

    simple is better

--- src/renderer/component/inviteNew/view.jsx
+++ src/renderer/component/inviteNew/view.jsx
@@ -92,7 +92,7 @@ class InviteNew extends React.PureComponent {
         <p className="help help--padded">
           {__('Read our')}{' '}
           <Button button="link" label={__('FAQ')} href="https://lbry.io/faq/referrals" />{' '}
-          {__('to learn more about LBRY Referral Program')}.
+          {__('to learn more about Referrals')}.
         </p>
       </section>
     );
