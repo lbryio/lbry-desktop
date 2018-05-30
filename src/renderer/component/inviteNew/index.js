8b895caf151035e4b8e16cde9bb34f78beafa14a
Travis Eden <daovist@gmail.com>
2018-05-30 13:51:20 -0400

    remove amount from new invite component

--- src/renderer/component/inviteNew/index.js
+++ src/renderer/component/inviteNew/index.js
@@ -1,26 +1,17 @@
-import React from 'react';
 import { connect } from 'react-redux';
-import InviteNew from './view';
 import {
   selectUserInvitesRemaining,
   selectUserInviteNewIsPending,
   selectUserInviteNewErrorMessage,
 } from 'redux/selectors/user';
-import rewards from 'rewards';
-import { makeSelectRewardAmountByType } from 'redux/selectors/rewards';
-
 import { doUserInviteNew } from 'redux/actions/user';
+import InviteNew from './view';
 
-const select = state => {
-  const selectReward = makeSelectRewardAmountByType();
-
-  return {
-    errorMessage: selectUserInviteNewErrorMessage(state),
-    invitesRemaining: selectUserInvitesRemaining(state),
-    isPending: selectUserInviteNewIsPending(state),
-    rewardAmount: selectReward(state, { reward_type: rewards.TYPE_REFERRAL }),
-  };
-};
+const select = state => ({
+  errorMessage: selectUserInviteNewErrorMessage(state),
+  invitesRemaining: selectUserInvitesRemaining(state),
+  isPending: selectUserInviteNewIsPending(state),
+});
 
 const perform = dispatch => ({
   inviteNew: email => dispatch(doUserInviteNew(email)),
