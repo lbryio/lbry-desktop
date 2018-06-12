a2a12ef6cdcf37226c0096f823b8a6a3defb1a06
Travis Eden <daovist@gmail.com>
2018-06-12 09:22:53 -0400

    add togglable QR code to wallet address component

--- src/renderer/component/common/qr-code.jsx
+++ src/renderer/component/common/qr-code.jsx
@@ -6,22 +6,27 @@ import classnames from 'classnames';
 type Props = {
   value: string,
   paddingRight?: boolean,
+  paddingTop?: boolean,
+  size?: number,
 };
 
 class QRCode extends React.Component<Props> {
   static defaultProps = {
     paddingRight: false,
+    paddingTop: false,
+    size: 128,
   };
 
   render() {
-    const { value, paddingRight } = this.props;
+    const { value, paddingRight, paddingTop, size } = this.props;
     return (
       <div
         className={classnames('qr-code', {
           'qr-code--right-padding': paddingRight,
+          'qr-code--top-padding': paddingTop,
         })}
       >
-        <QRCodeElement value={value} />
+        <QRCodeElement value={value} size={size} />
       </div>
     );
   }
