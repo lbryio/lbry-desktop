41e5277fbd60386ef13bca024dea5b4f804bb1de
Sean Yesmunt <sean@lbry.io>
2018-08-13 12:37:02 -0400

    change copy to 'Starting up'

--- src/renderer/component/splash/view.jsx
+++ src/renderer/component/splash/view.jsx
@@ -30,7 +30,7 @@ export class SplashScreen extends React.PureComponent<Props, State> {
     super(props);
 
     this.state = {
-      details: __('Starting daemon'),
+      details: __('Starting up'),
       message: __('Connecting'),
       isRunning: false,
       launchedModal: false,
