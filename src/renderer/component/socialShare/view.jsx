42988daa59540ddb73d425aad0f27ad1372b3ceb
Jessop Breth <jessopb@gmail.com>
2018-10-04 09:39:14 -0400

    adds LBRY app sharing to socialShare

--- src/renderer/component/socialShare/view.jsx
+++ src/renderer/component/socialShare/view.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import type { Claim } from 'types/claim';
 import Button from 'component/button';
 import * as icons from 'constants/icons';
-import Tooltip from 'component/common/tooltip';
-import Address from 'component/address';
+import CopyableText from 'component/copyableText';
+import ToolTip from 'component/common/tooltip';
 
 type Props = {
   claim: Claim,
   onDone: () => void,
+  speechShareable: boolean,
 };
 
 class SocialShare extends React.PureComponent<Props> {
@@ -27,46 +28,79 @@ class SocialShare extends React.PureComponent<Props> {
       channel_name: channelName,
       value,
     } = this.props.claim;
+    const { speechShareable, onDone } = this.props;
     const channelClaimId =
       value && value.publisherSignature && value.publisherSignature.certificateId;
-    const { onDone } = this.props;
     const speechPrefix = 'http://spee.ch/';
+    const lbryPrefix = 'http://open.lbry.io/';
+
     const speechURL =
       channelName && channelClaimId
         ? `${speechPrefix}${channelName}:${channelClaimId}/${claimName}`
         : `${speechPrefix}${claimName}#${claimId}`;
 
+    const lbryURL = `${lbryPrefix}${claimName}#${claimId}`;
+
     return (
       <section className="card__content">
-        <Address address={speechURL} noSnackbar />
-        <div className="card__actions card__actions--center">
-          <Tooltip onComponent body={__('Facebook')}>
-            <Button
-              iconColor="blue"
-              icon={icons.FACEBOOK}
-              button="alt"
-              label={__('')}
-              href={`https://facebook.com/sharer/sharer.php?u=${speechURL}`}
-            />
-          </Tooltip>
-          <Tooltip onComponent body={__('Twitter')}>
-            <Button
-              iconColor="blue"
-              icon={icons.TWITTER}
-              button="alt"
-              label={__('')}
-              href={`https://twitter.com/home?status=${speechURL}`}
-            />
-          </Tooltip>
-          <Tooltip onComponent body={__('View on Spee.ch')}>
-            <Button
-              icon={icons.GLOBE}
-              iconColor="blue"
-              button="alt"
-              label={__('')}
-              href={`${speechURL}`}
-            />
-          </Tooltip>
+        {speechShareable && (
+          <div className="card__content">
+            <label className="card__subtitle">{__('Web link')}</label>
+            <CopyableText copyable={speechURL} noSnackbar />
+            <div className="card__actions card__actions--center">
+              <ToolTip onComponent body={__('Facebook')}>
+                <Button
+                  iconColor="blue"
+                  icon={icons.FACEBOOK}
+                  button="alt"
+                  label={__('')}
+                  href={`https://facebook.com/sharer/sharer.php?u=${speechURL}`}
+                />
+              </ToolTip>
+              <ToolTip onComponent body={__('Twitter')}>
+                <Button
+                  iconColor="blue"
+                  icon={icons.TWITTER}
+                  button="alt"
+                  label={__('')}
+                  href={`https://twitter.com/home?status=${speechURL}`}
+                />
+              </ToolTip>
+              <ToolTip onComponent body={__('View on Spee.ch')}>
+                <Button
+                  icon={icons.GLOBE}
+                  iconColor="blue"
+                  button="alt"
+                  label={__('')}
+                  href={`${speechURL}`}
+                />
+              </ToolTip>
+            </div>
+          </div>
+        )}
+        <div className="card__content">
+          <label className="card__subtitle">{__('LBRY App link')}</label>
+          <CopyableText copyable={lbryURL} noSnackbar />
+          <div className="card__actions card__actions--center">
+            <ToolTip onComponent body={__('Facebook')}>
+              <Button
+                iconColor="blue"
+                icon={icons.FACEBOOK}
+                button="alt"
+                label={__('')}
+                href={`https://facebook.com/sharer/sharer.php?u=${lbryURL}`}
+              />
+            </ToolTip>
+            <ToolTip onComponent body={__('Twitter')}>
+              <Button
+                iconColor="blue"
+                icon={icons.TWITTER}
+                button="alt"
+                label={__('')}
+                href={`https://twitter.com/home?status=${lbryURL}`}
+              />
+            </ToolTip>
+          </div>
         </div>
         <div className="card__actions">
           <Button button="link" label={__('Done')} onClick={onDone} />
