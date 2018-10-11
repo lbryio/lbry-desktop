45961e7d76f2e56b091f059705f559ebce21fdc4
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2018-10-11 02:39:58 -0400

    Update view.jsx

--- src/renderer/component/wunderbar/view.jsx
+++ src/renderer/component/wunderbar/view.jsx
@@ -107,7 +107,7 @@ class WunderBar extends React.PureComponent<Props> {
           onSubmit(uri, params);
         } else {
           this.props.doShowSnackBar({
-              message: __('Invalid LBRY URL requested. Only A-Z, a-z, and - allowed.'),
+              message: __('Invalid LBRY URL entered. Only A-Z, a-z, and - allowed.'),
               displayType: ['snackbar'],
             })
         }
