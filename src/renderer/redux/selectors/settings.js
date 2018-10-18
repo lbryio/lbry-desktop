7aae6baaa9aa73996019898f333b6bddf1e82f7a
ポール ウェッブ <netopwibby@thenetwork.email>
2018-10-18 16:10:15 -0500

    Removed themes folder

--- src/renderer/redux/selectors/settings.js
+++ src/renderer/redux/selectors/settings.js
@@ -30,7 +30,6 @@ export const selectThemePath = createSelector(
   selectIsNight,
   (theme, automaticDarkModeEnabled, isNight) => {
     const dynamicTheme = automaticDarkModeEnabled && isNight ? 'dark' : theme;
-    // return `${staticResourcesPath}/themes/${dynamicTheme || 'light'}.css`;
     return dynamicTheme || 'light';
   }
 );
