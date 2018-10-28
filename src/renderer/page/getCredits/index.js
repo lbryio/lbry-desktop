1359e7873ba4012204a3acac320c4d2efb603a45
Anna Melzer <anna@data-and-dragons.com>
2018-10-28 19:33:31 +0100

    Reduce unused var warnings. Remove unnecessary redux injections (#2064)

--- src/renderer/page/getCredits/index.js
+++ src/renderer/page/getCredits/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import { connect } from 'react-redux';
 import GetCreditsPage from './view';
 
-export default connect(null, null)(GetCreditsPage);
+export default GetCreditsPage;
--- src/renderer/page/sendCredits/index.js
+++ src/renderer/page/sendCredits/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import { connect } from 'react-redux';
 import SendReceivePage from './view';
 
-export default connect(null, null)(SendReceivePage);
+export default SendReceivePage;
