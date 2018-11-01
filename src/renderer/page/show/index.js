52db19dd9c3b71ed1278a370e4de7d6f72c141e5
Anna Melzer <anna@data-and-dragons.com>
2018-11-01 15:59:38 +0100

    fix linting for page files

--- src/renderer/page/show/index.js
+++ src/renderer/page/show/index.js
@@ -17,4 +17,7 @@ const perform = dispatch => ({
   resolveUri: uri => dispatch(doResolveUri(uri)),
 });
 
-export default connect(select, perform)(ShowPage);
+export default connect(
+  select,
+  perform
+)(ShowPage);
