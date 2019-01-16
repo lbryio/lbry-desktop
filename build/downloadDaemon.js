a895a653faa12ffd9e027ec03f04ac528c154533
Sean Yesmunt <sean@lbry.io>
2019-01-15 23:33:06 -0500

    add support for .lbry sandbox applications

--- build/downloadDaemon.js
+++ build/downloadDaemon.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const fs = require('fs');
 const packageJSON = require('../package.json');
-const axios = require('axios');
+const fetch = require('node-fetch');
 const decompress = require('decompress');
 const os = require('os');
 const del = require('del');
@@ -11,16 +11,16 @@ const downloadDaemon = targetPlatform =>
   new Promise((resolve, reject) => {
     const daemonURLTemplate = packageJSON.lbrySettings.lbrynetDaemonUrlTemplate;
     const daemonVersion = packageJSON.lbrySettings.lbrynetDaemonVersion;
-    const daemonDir = path.join(__dirname,'..',packageJSON.lbrySettings.lbrynetDaemonDir);
+    const daemonDir = path.join(__dirname, '..', packageJSON.lbrySettings.lbrynetDaemonDir);
     let daemonFileName = packageJSON.lbrySettings.lbrynetDaemonFileName;
 
-    let currentPlatform = os.platform();
+    const currentPlatform = os.platform();
 
-    var daemonPlatform = process.env.TARGET || targetPlatform || currentPlatform;
+    let daemonPlatform = process.env.TARGET || targetPlatform || currentPlatform;
     if (daemonPlatform === 'mac' || daemonPlatform === 'darwin') daemonPlatform = 'mac';
     if (daemonPlatform === 'win32' || daemonPlatform === 'windows') {
-     daemonPlatform = 'windows';
-     daemonFileName = daemonFileName + '.exe';
+      daemonPlatform = 'windows';
+      daemonFileName += '.exe';
     }
     const daemonFilePath = path.join(daemonDir, daemonFileName);
     const daemonVersionPath = path.join(__dirname, 'daemon.ver');
@@ -34,24 +34,21 @@ const downloadDaemon = targetPlatform =>
     const hasDaemonVersion = fs.existsSync(daemonVersionPath);
     let downloadedDaemonVersion;
     if (hasDaemonVersion) {
-      downloadedDaemonVersion = fs.readFileSync(daemonVersionPath, "utf8");
+      downloadedDaemonVersion = fs.readFileSync(daemonVersionPath, 'utf8');
     }
 
     if (hasDaemonDownloaded && hasDaemonVersion && downloadedDaemonVersion === daemonVersion) {
       console.log('\x1b[34minfo\x1b[0m Daemon already downloaded');
       resolve('Done');
-      return;
     } else {
       console.log('\x1b[34minfo\x1b[0m Downloading daemon...');
-      axios
-        .request({
-          responseType: 'arraybuffer',
-          url: daemonURL,
-          method: 'get',
-          headers: {
-            'Content-Type': 'application/zip',
-          },
-        })
+      fetch(daemonURL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/zip',
+        },
+      })
+        .then(response => response.buffer())
         .then(
           result =>
             new Promise((newResolve, newReject) => {
@@ -62,24 +59,25 @@ const downloadDaemon = targetPlatform =>
                 fs.mkdirSync(distPath);
               }
 
-              fs.writeFile(tmpZipPath, result.data, error => {
+              fs.writeFile(tmpZipPath, result, error => {
                 if (error) return newReject(error);
                 return newResolve();
               });
-          })
+            })
         )
         .then(() => del(`${daemonFilePath}*`))
-        .then(() => decompress(tmpZipPath, daemonDir, {
-          filter: file =>
-            path.basename(file.path) === daemonFileName,
-        }))
+        .then(() =>
+          decompress(tmpZipPath, daemonDir, {
+            filter: file => path.basename(file.path) === daemonFileName,
+          })
+        )
         .then(() => {
           console.log('\x1b[32msuccess\x1b[0m Daemon downloaded!');
           if (hasDaemonVersion) {
             del(daemonVersionPath);
           }
 
-          fs.writeFileSync(daemonVersionPath, daemonVersion, "utf8")
+          fs.writeFileSync(daemonVersionPath, daemonVersion, 'utf8');
           resolve('Done');
         })
         .catch(error => {
@@ -87,12 +85,12 @@ const downloadDaemon = targetPlatform =>
             `\x1b[31merror\x1b[0m Daemon download failed due to: \x1b[35m${error}\x1b[0m`
           );
           reject(error);
-        })
-      };
+        });
+    }
   });
 
 module.exports = downloadDaemon;
 
 require('make-runnable/custom')({
-  printOutputFrame: false
+  printOutputFrame: false,
 });
--- src/renderer/component/fileRender/view.jsx
+++ src/renderer/component/fileRender/view.jsx
@@ -1,4 +1,5 @@
 // @flow
+import { remote } from 'electron';
 import React from 'react';
 import LoadingScreen from 'component/common/loading-screen';
 import PdfViewer from 'component/viewers/pdfViewer';
@@ -20,6 +21,70 @@ type Props = {
 };
 
 class FileRender extends React.PureComponent<Props> {
+  constructor(props) {
+    super(props);
+
+    this.escapeListener = this.escapeListener.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.escapeListener, true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.escapeListener, true);
+  }
+
+  processSandboxRef(element) {
+    if (!element) {
+      return;
+    }
+
+    window.sandbox = element;
+
+    element.addEventListener('permissionrequest', e => {
+      // eslint-disable-next-line no-console
+      console.log('permissionrequest', e);
+    });
+
+    element.addEventListener('console-message', e => {
+      if (/^\$LBRY_IPC:/.test(e.message)) {
+        // Process command
+        let message = {};
+        try {
+          message = JSON.parse(/^\$LBRY_IPC:(.*)/.exec(e.message)[1]);
+        } catch (err) {}
+        // eslint-disable-next-line no-console
+        console.log('IPC', message);
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('Sandbox:', e.message);
+      }
+    });
+
+    element.addEventListener('enter-html-full-screen', () => {
+      // stub
+    });
+
+    element.addEventListener('leave-html-full-screen', () => {
+      // stub
+    });
+  }
+
+  escapeListener(e) {
+    if (e.keyCode === 27) {
+      e.preventDefault();
+
+      this.exitFullscreen();
+
+      return false;
+    }
+  }
+
+  exitFullscreen() {
+    remote.getCurrentWindow().setFullScreen(false);
+  }
+
   renderViewer() {
     const { source, mediaType, currentTheme } = this.props;
 
@@ -32,6 +97,19 @@ class FileRender extends React.PureComponent<Props> {
     // Supported mediaTypes
     const mediaTypes = {
       '3D-file': <ThreeViewer source={{ fileType, downloadPath }} theme={currentTheme} />,
+      application: (
+        <webview
+          ref={element => this.processSandboxRef(element)}
+          title=""
+          sandbox="allow-scripts allow-forms allow-pointer-lock"
+          src={source.url}
+          autosize="on"
+          style={{ border: 0, width: '100%', height: '100%' }}
+          useragent="Mozilla/5.0 AppleWebKit/537 Chrome/60 Safari/537"
+          enableremotemodule="false"
+          webpreferences="sandbox=true,contextIsolation=true,webviewTag=false,enableRemoteModule=false,devTools=false"
+        />
+      ),
       // Add routes to viewer...
     };
 
