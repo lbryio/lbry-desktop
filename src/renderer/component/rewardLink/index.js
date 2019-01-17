afe82c478e537ce5a376189045b2ab7def4da11e
Sean Yesmunt <sean@lbry.io>
2019-01-17 01:34:31 -0500

    change: remove error modal from rewards

--- src/renderer/component/rewardLink/index.js
+++ src/renderer/component/rewardLink/index.js
@@ -1,35 +1,19 @@
 import { connect } from 'react-redux';
-import {
-  makeSelectClaimRewardError,
-  makeSelectRewardByType,
-  makeSelectIsRewardClaimPending,
-  doClaimRewardType,
-  doClaimRewardClearError,
-} from 'lbryinc';
+import { makeSelectRewardByType, makeSelectIsRewardClaimPending, doClaimRewardType } from 'lbryinc';
 import { doNavigate } from 'redux/actions/navigation';
 import RewardLink from './view';
 
-const makeSelect = () => {
-  const selectIsPending = makeSelectIsRewardClaimPending();
-  const selectReward = makeSelectRewardByType();
-  const selectError = makeSelectClaimRewardError();
-
-  const select = (state, props) => ({
-    errorMessage: selectError(state, props),
-    isPending: selectIsPending(state, props),
-    reward: selectReward(state, props.reward_type),
-  });
-
-  return select;
-};
+const select = (state, props) => ({
+  isPending: makeSelectIsRewardClaimPending()(state, props),
+  reward: makeSelectRewardByType()(state, props.reward_type),
+});
 
 const perform = dispatch => ({
-  claimReward: reward => dispatch(doClaimRewardType(reward.reward_type)),
-  clearError: reward => dispatch(doClaimRewardClearError(reward)),
+  claimReward: reward => dispatch(doClaimRewardType(reward.reward_type, { notifyError: true })),
   navigate: path => dispatch(doNavigate(path)),
 });
 
 export default connect(
-  makeSelect,
+  select,
   perform
 )(RewardLink);
--- src/renderer/component/rewardLink/view.jsx
+++ src/renderer/component/rewardLink/view.jsx
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import { Modal } from 'modal/modal';
 import Button from 'component/button';
 
 type Reward = {
@@ -10,15 +9,13 @@ type Reward = {
 type Props = {
   isPending: boolean,
   label: ?string,
-  errorMessage: ?string,
   reward: Reward,
   button: ?boolean,
-  clearError: Reward => void,
   claimReward: Reward => void,
 };
 
 const RewardLink = (props: Props) => {
-  const { reward, claimReward, clearError, errorMessage, label, isPending, button } = props;
+  const { reward, claimReward, label, isPending, button } = props;
 
   return !reward ? null : (
     <div className="reward-link">
@@ -30,23 +27,6 @@ const RewardLink = (props: Props) => {
           claimReward(reward);
         }}
       />
-      {errorMessage ? (
-        // TODO: This should be moved to redux
-        <Modal
-          isOpen
-          title={__('Error Claiming Reward')}
-          contentLabel="Reward Claim Error"
-          onConfirmed={() => {
-            clearError(reward);
-          }}
-        >
-          <section className="card__content">
-            <div className="error-modal__error-list">{errorMessage}</div>
-          </section>
-        </Modal>
-      ) : (
-        ''
-      )}
     </div>
   );
 };
