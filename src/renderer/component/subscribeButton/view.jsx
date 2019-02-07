85fbea818008b5f61f78d775fb0076f76cb80117
kuesa <me@noahgrove.me>
2019-02-06 22:28:37 -0800

    Added "Unsubscribe" broken heart icon.

--- src/renderer/component/subscribeButton/view.jsx
+++ src/renderer/component/subscribeButton/view.jsx
@@ -43,7 +43,7 @@ export default (props: Props) => {
   return (
     <Button
       iconColor="red"
-      icon={isSubscribed ? undefined : ICONS.SUBSCRIPTION}
+      icon={isSubscribed ? ICONS.UNSUBSCRIBE : ICONS.SUBSCRIPTION}
       button={buttonStyle || 'alt'}
       label={subscriptionLabel}
       onClick={e => {
