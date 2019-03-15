3c3c12fa5daa06d9c3ab40e4f682af43c0afd09d
Shawn <shawn@lbry.io>
2019-03-15 13:58:42 -0500

    Fix keytar and bundling

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const { DefinePlugin, ProvidePlugin } = require('webpack');
 const { getIfUtils, removeEmpty } = require('webpack-config-utils');
