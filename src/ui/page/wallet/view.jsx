beb54a218ed6e901de47dbc1096ab433e5e09ae7
Sean Yesmunt <sean@lbry.io>
2019-03-18 11:40:42 -0400

    update disabled pages

--- src/ui/page/wallet/view.jsx
+++ src/ui/page/wallet/view.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import classnames from 'classnames';
 import WalletBalance from 'component/walletBalance';
 import RewardSummary from 'component/rewardSummary';
 import TransactionListRecent from 'component/transactionListRecent';
 import WalletAddress from 'component/walletAddress';
 import Page from 'component/page';
+import UnsupportedOnWeb from 'component/common/unsupported-on-web';
 
 const WalletPage = () => (
   <Page>
-    <div className="columns">
-      <WalletBalance />
-      <RewardSummary />
+    {IS_WEB && <UnsupportedOnWeb />}
+    <div className={classnames({ 'card--disabled': IS_WEB })}>
+      <div className='columns'>
+        <WalletBalance />
+        <RewardSummary />
+      </div>
+      <WalletAddress />
+      <TransactionListRecent />
     </div>
-    <WalletAddress />
-    <TransactionListRecent />
   </Page>
 );
 
