03ebcd953d88cab229c0cf940a3ea1854950c37f
Sean Yesmunt <sean@lbry.io>
2019-05-13 22:53:04 -0400

    simplify default channel color creation

--- src/ui/component/channelThumbnail/view.jsx
+++ src/ui/component/channelThumbnail/view.jsx
@@ -12,29 +12,15 @@ type Props = {
 function ChannelThumbnail(props: Props) {
   const { thumbnail, uri } = props;
 
-  function getColorClass() {
-    // Generate a random color class based on the first letter of the channel name
-    const { channelName } = parseURI(uri);
-    const initializer = channelName.charCodeAt(0) - 65; // will be between 0 and 57
-
-    let thumbnailColorClass = `channel-thumbnail__default--`;
-
-    switch (true) {
-      case initializer < 15:
-        return (thumbnailColorClass += '1');
-      case initializer < 30:
-        return (thumbnailColorClass += '2');
-      case initializer < 45:
-        return (thumbnailColorClass += '3');
-      default:
-        return (thumbnailColorClass += '4');
-    }
-  }
+  // Generate a random color class based on the first letter of the channel name
+  const { channelName } = parseURI(uri);
+  const initializer = channelName.charCodeAt(0) - 65; // will be between 0 and 57
+  const className = `channel-thumbnail__default--${initializer % 4}`;
 
   return (
     <div
       className={classnames('channel-thumbnail', {
-        [getColorClass()]: !thumbnail,
+        [className]: !thumbnail,
       })}
     >
       {!thumbnail && <img className="channel-thumbnail__default" src={Gerbil} />}
