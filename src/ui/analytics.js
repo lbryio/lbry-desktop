01bc69839c6b20ba4c3327f7382a445dbd37962b
Sean Yesmunt <sean@lbry.io>
2019-05-14 15:24:59 -0400

    enable desktop ga

--- src/ui/analytics.js
+++ src/ui/analytics.js
@@ -76,7 +76,7 @@ const analytics: Analytics = {
 // Initialize google analytics
 // Set `debug: true` for debug info
 // Will change once we have separate ids for desktop/web
-const UA_ID = IS_WEB ? 'UA-60403362-12' : 'UA-60403362-12';
+const UA_ID = IS_WEB ? 'UA-60403362-12' : 'UA-60403362-13';
 
 ReactGA.initialize(UA_ID, {
   gaOptions: { name: IS_WEB ? 'web' : 'desktop' },
