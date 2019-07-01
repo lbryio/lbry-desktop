27a6b75fc4b158ff2b7d57e7cbb01006e6889768
Sean Yesmunt <sean@lbry.io>
2019-06-30 21:52:38 -0400

    general fixes

--- src/ui/page/discover/index.js
+++ src/ui/page/discover/index.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import { selectFollowedTags } from 'lbry-redux';
+import { selectSubscriptions } from 'redux/selectors/subscriptions';
 import DiscoverPage from './view';
 
 const select = state => ({
   followedTags: selectFollowedTags(state),
+  subscribedChannels: selectSubscriptions(state),
 });
 
 const perform = {};
