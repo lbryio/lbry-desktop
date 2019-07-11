075f745ddd91a194a4f498cc1b3527a762b5d37b
Sean Yesmunt <sean@lbry.io>
2019-07-11 14:16:39 -0400

    add tag to wunderbar autocomplete

--- src/ui/component/common/icon-custom.jsx
+++ src/ui/component/common/icon-custom.jsx
@@ -253,6 +253,12 @@ export const icons = {
       <path d="M7 11V7a5 5 0 0 1 9.9-1" />
     </g>
   ),
+  [ICONS.TAG]: buildIcon(
+    <g>
+      <path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z" />
+      <line x1="7" y1="7" x2="7" y2="7" />
+    </g>
+  ),
   [ICONS.SUPPORT]: buildIcon(
     <g>
       <polyline points="23 6 13.5 15.5 8.5 10.5 1 18" />
