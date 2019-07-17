062899c48354e541831476b868aa7f9cccb11705
jessop <jessop@lbry.com>
2019-07-17 16:56:36 -0400

    improves comment display adds channel link adds support for newline better handles long channel names

--- src/ui/component/comment/view.jsx
+++ src/ui/component/comment/view.jsx
@@ -1,25 +1,28 @@
 // @flow
 import React from 'react';
 import relativeDate from 'tiny-relative-date';
+import Button from '../button';
 
 type Props = {
   author: string,
+  authorUri: string,
   message: string,
   timePosted: number,
 };
 
 function Comment(props: Props) {
-  const { author, timePosted, message } = props;
+  const { author, authorUri, timePosted, message } = props;
 
   return (
     <li className="comment">
-      <div className="comment__meta card__actions--between">
-        <strong>{author || __('Anonymous')}</strong>
-
-        <time dateTime={timePosted}>{relativeDate(timePosted)}</time>
+      <div className="comment__meta">
+        <Button className="button--uri-indicator truncated-text comment__author" navigate={authorUri} label={author} />
+        <time className="comment__time" dateTime={timePosted}>
+          {relativeDate(timePosted)}
+        </time>
       </div>
 
-      <p>{message}</p>
+      <p className={'comment__message'}>{message}</p>
       {/* The following is for adding threaded replies, upvoting and downvoting */}
       {/* <div className="comment__actions card__actions--between"> */}
       {/*  <button className={'button button--primary'}>Reply</button> */}
