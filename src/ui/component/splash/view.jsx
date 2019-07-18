b735daf4e1ec6cdaa2f9a612ac51c67fc6680fc9
Thomas Zarebczan <thomas.zarebczan@gmail.com>
2019-07-17 20:04:17 -0400

    fix: bugs
    Manually set the sdk path to grab a fixed build from http://build.lbry.io/daemon/build-11443_commit-eae4ed7_branch-master/ - please revert on next commit.

--- src/ui/component/splash/view.jsx
+++ src/ui/component/splash/view.jsx
@@ -52,8 +52,6 @@ export default class SplashScreen extends React.PureComponent<Props, State> {
   }
 
   componentDidMount() {
-    this.props.onReadyToLaunch();
-
     const { checkDaemonVersion } = this.props;
     this.adjustErrorTimeout();
     Lbry.connect()
