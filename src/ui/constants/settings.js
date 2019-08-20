83d0d29954ef93b9a585edb4a7acbf7428898582
jessop <jessop@lbry.com>
2019-08-20 08:29:59 -0400

    recommit

--- src/ui/constants/settings.js
+++ src/ui/constants/settings.js
@@ -18,6 +18,7 @@ export const OS_NOTIFICATIONS_ENABLED = 'osNotificationsEnabled';
 export const AUTO_DOWNLOAD = 'autoDownload';
 export const SUPPORT_OPTION = 'supportOption';
 export const HIDE_BALANCE = 'hideBalance';
+export const ENABLE_SYNC = 'enableSync';
 export const HIDE_SPLASH_ANIMATION = 'hideSplashAnimation';
 export const FLOATING_PLAYER = 'floatingPlayer';
 export const DARK_MODE_TIMES = 'darkModeTimes';
