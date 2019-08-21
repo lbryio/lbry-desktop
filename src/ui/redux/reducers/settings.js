9e39cf16a07045c42f4b34b1ea267ee570566490
Julie Sigwart <jsigwart@gmail.com>
2019-08-21 09:26:13 -0700

    Add German for testing

--- src/ui/redux/reducers/settings.js
+++ src/ui/redux/reducers/settings.js
@@ -39,7 +39,7 @@ const defaultState = {
     }),
   },
   isNight: false,
-  languages: { en: 'English', pl: 'Polish', id: 'Bahasa Indonesia' }, // temporarily hard code these so we can advance i18n testing
+  languages: { en: 'English', pl: 'Polish', de_DE: 'German', id: 'Bahasa Indonesia' }, // temporarily hard code these so we can advance i18n testing
   daemonSettings: {},
 };
 
