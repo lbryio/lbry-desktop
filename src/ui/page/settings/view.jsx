c630ef35aafbca64949d3db5585a28a641835d93
Jeremy Kauffman <jeremy@lbry.io>
2019-09-04 17:43:37 -0400

    proof of concept of i18n component

--- src/ui/page/settings/view.jsx
+++ src/ui/page/settings/view.jsx
@@ -7,6 +7,7 @@ import * as PAGES from 'constants/pages';
 import * as React from 'react';
 import { FormField, FormFieldPrice, Form } from 'component/common/form';
 import Button from 'component/button';
+import I18n from 'component/i18n';
 import Page from 'component/page';
 import FileSelector from 'component/common/file-selector';
 
@@ -521,13 +522,19 @@ class SettingsPage extends React.PureComponent<Props, State> {
                   checked={supportOption}
                   label={__('Enable claim support')}
                   helper={
-                    <React.Fragment>
-                      {__('This will add a Support button along side tipping. Similar to tips, supports help ')}
-                      <Button button="link" label={__(' discovery ')} href="https://lbry.com/faq/trending" />
-                      {__(' but the LBC is returned to your wallet if revoked.')}
-                      {__(' Both also help secure ')}
-                      <Button button="link" label={__('vanity names')} href="https://lbry.com/faq/naming" />.
-                    </React.Fragment>
+                    <I18n
+                      tokens={{
+                        discovery_link: (
+                          <Button button="link" label={__('discovery')} href="https://lbry.com/faq/trending" />
+                        ),
+                        vanity_names_link: (
+                          <Button button="link" label={__('vanity names')} href="https://lbry.com/faq/naming" />
+                        ),
+                      }}
+                    >
+                      This will add a Support button along side tipping. Similar to tips, supports help %discovery_link%
+                      but the LBC is returned to your wallet if revoked. Both also help secure your %vanity_names_link%.
+                    </I18n>
                   }
                 />
 
