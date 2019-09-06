4e64a3efe8276827585ed0438506d5419ff96689
jessop <jessop@lbry.com>
2019-09-06 02:03:34 -0400

    ready to store position and volume

--- src/ui/component/viewers/videoViewer/view.jsx
+++ src/ui/component/viewers/videoViewer/view.jsx
@@ -37,17 +37,31 @@ function VideoViewer(props: Props) {
     const currentVideo: HTMLVideoElement | null = document.querySelector('video');
 
     function doEnded() {
+      // clear position
       setPlayingUri(null);
       onEndedCB();
+    }
+    function doPause(e: Event) {
+      // store position e.target.currentTime
+      console.log('lets store position', e.target.currentTime);
+    }
+    function doVolume(e: Event) {
+      // store volume e.target.volume
+      console.log('lets store volume', e.target.volume);
+      console.log(e);
     }
 
     if (currentVideo) {
       currentVideo.addEventListener('ended', doEnded);
+      currentVideo.addEventListener('pause', doPause);
+      currentVideo.addEventListener('volumechange', doVolume);
     }
     // cleanup function:
     return () => {
       if (currentVideo) {
         currentVideo.removeEventListener('ended', doEnded);
+        currentVideo.removeEventListener('pause', doPause);
+        currentVideo.removeEventListener('volumechange', doVolume);
       }
     };
   }, []);
