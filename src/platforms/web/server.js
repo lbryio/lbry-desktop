9bccc9f3654ea63ce0d17103b201afcf3e887b7a
Jeremy Kauffman <jeremy@lbry.io>
2019-09-06 13:54:39 -0400

    fixes several issues with lbry.tv metadata
    fix env variable

--- src/platforms/web/server.js
+++ src/platforms/web/server.js
@@ -1,10 +1,10 @@
 const { parseURI } = require('lbry-redux');
+const { generateStreamUrl } = require('../../src/ui/util/lbrytv');
 const { WEB_SERVER_PORT } = require('../../config');
 const { readFileSync } = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
-
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
@@ -15,11 +15,11 @@ const pool = mysql.createPool({
   database: 'chainquery',
 });
 
-const getClaim = (claimName, claimId, callback) => {
+const getClaim = (claimName, claimId, channelName, channelClaimId, callback) => {
   let params = [claimName];
 
   let sql =
-    'SELECT channel_claim.name as channel, claim.claim_id, claim.name, claim.description, claim.language, claim.thumbnail_url, claim.title ' +
+    'SELECT channel_claim.name as channel, claim.claim_id, claim.name, claim.description, claim.language, claim.thumbnail_url, claim.title, claim.source_media_type, claim.frame_width, claim.frame_height ' +
     'FROM claim ' +
     'LEFT JOIN claim channel_claim on claim.publisher_id = channel_claim.claim_id ' +
     'WHERE claim.name = ?';
@@ -29,6 +29,17 @@ const getClaim = (claimName, claimId, callback) => {
     params.push(claimId + '%');
   } else {
     sql += ' AND claim.bid_state = "controlling"';
+  }
+
+  if (claimName[0] !== '@' && channelName) {
+    sql += ' AND channel_claim.name = ?';
+    params.push('@' + channelName);
+    if (channelClaimId) {
+      sql += ' AND channel_claim.claim_id LIKE ?';
+      params.push(channelClaimId + '%');
+    } else {
+      sql += ' AND channel_claim.bid_state = "controlling"';
+    }
   }
 
   sql += ' LIMIT 1';
@@ -43,7 +54,7 @@ function truncateDescription(description) {
 }
 
 function insertToHead(fullHtml, htmlToInsert) {
-  return fullHtml.replace('%%HEAD_TOKEN%%', htmlToInsert);
+  return fullHtml.replace('<!-- HEAD_REPLACEMENT_TOKEN -->', htmlToInsert);
 }
 
 const defaultHead =
@@ -67,7 +78,7 @@ app.get('*', async (req, res) => {
     const claimName = isChannel ? '@' + channelName : streamName;
     const claimId = isChannel ? channelClaimId : streamClaimId;
 
-    getClaim(claimName, claimId, (err, rows) => {
+    getClaim(claimName, claimId, channelName, channelClaimId, (err, rows) => {
       if (!err && rows && rows.length > 0) {
         const claim = rows[0];
         const title = claim.title ? claim.title : claimName;
@@ -83,6 +94,7 @@ app.get('*', async (req, res) => {
         let head = '';
 
         head += '<meta charset="utf8"/>';
+        head += `<title>${claimTitle}</title>`;
         head += `<meta name="description" content="${claimDescription}"/>`;
         if (claim.tags) {
           head += `<meta name="keywords" content="${claim.tags.toString()}"/>`;
@@ -95,7 +107,17 @@ app.get('*', async (req, res) => {
         head += `<meta property="og:type" content="website"/>`;
         // below should be canonical_url, but not provided by chainquery yet
         head += `<meta property="og:url" content="https://beta.lbry.tv/${claim.name}:${claim.claim_id}"/>`;
-        head += `<title>${claimTitle}</title>`;
+
+        if (claim.source_media_type && claim.source_media_type.startsWith('video/')) {
+          const videoUrl = generateStreamUrl(claim.name, claim.claim_id);
+          head += `<meta property="og:video" content="${videoUrl}" />`;
+          head += `<meta property="og:video:secure_url" content="${videoUrl}" />`;
+          head += `<meta property="og:video:type" content="${claim.source_media_type}" />`;
+          if (claim.frame_width && claim.frame_height) {
+            head += `<meta property="og:video:width" content="${claim.frame_width}/>`;
+            head += `<meta property="og:video:height" content="${claim.frame_height}/>`;
+          }
+        }
 
         html = insertToHead(html, head);
       } else {
