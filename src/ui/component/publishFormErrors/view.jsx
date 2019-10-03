1942a5ce4e0971f48ca68198a2e5c66b78a58c63
Sean Yesmunt <sean@lbry.io>
2019-10-03 14:24:45 -0400

    Revert "fix translation string"
    This reverts commit 208111caebc8cd3626acd719e425095cb4d0861b.

--- src/ui/component/publishFormErrors/view.jsx
+++ src/ui/component/publishFormErrors/view.jsx
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 import { THUMBNAIL_STATUSES, isNameValid } from 'lbry-redux';
-import { INVALID_NAME_ERROR } from 'constants/claim';
 
 type Props = {
   title: ?string,
@@ -22,7 +21,7 @@ function PublishFormErrors(props: Props) {
     <div className="error-text">
       {!title && <div>{__('A title is required')}</div>}
       {!name && <div>{__('A URL is required')}</div>}
-      {!isNameValid(name, false) && INVALID_NAME_ERROR}
+      {!isNameValid(name, false) && __('LBRY names cannot contain spaces or reserved symbols ($#@;/"<>%{}|^~[]`)')}
       {!bid && <div>{__('A deposit amount is required')}</div>}
       {uploadThumbnailStatus === THUMBNAIL_STATUSES.IN_PROGRESS && (
         <div>{__('Please wait for thumbnail to finish uploading')}</div>
--- src/ui/constants/claim.js
+++ src/ui/constants/claim.js
@@ -3,6 +3,3 @@ export const MINIMUM_PUBLISH_BID = 0.00000001;
 export const CHANNEL_ANONYMOUS = 'anonymous';
 export const CHANNEL_NEW = 'new';
 export const PAGE_SIZE = 20;
-
-export const INVALID_NAME_ERROR =
-  __('LBRY names cannot contain spaces or reserved symbols') + ' ' + '($#@;/"<>%{}|^~[]`)';
