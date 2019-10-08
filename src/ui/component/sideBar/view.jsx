68267ff97787269f3bf8eaf7d7c84e52dd9d6997
utkarshiam <utkarshdhy@gmail.com>
2019-10-09 01:08:54 +0530

    empty div with actions from sidebar component removed

--- src/ui/component/sideBar/view.jsx
+++ src/ui/component/sideBar/view.jsx
@@ -32,7 +32,6 @@ function SideBar(props: Props) {
           <h2>LBRY</h2>
 
           <p>{__('The best decentralized content platform on the web.')}</p>
-          <div className="card__actions">{/* <Button button="primary" label={__('Do Something')} /> */}</div>
         </div>
       </div>
     </StickyBox>
