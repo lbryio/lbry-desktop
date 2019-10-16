ab93705ad6eb1b63f91883a17f9959abab71ca8f
Sean Yesmunt <sean@lbry.io>
2019-10-16 00:21:19 -0400

    fix settings link

--- src/ui/component/common/hidden-nsfw.jsx
+++ src/ui/component/common/hidden-nsfw.jsx
@@ -1,7 +1,8 @@
 // @flow
+import * as PAGES from 'constants/pages';
+import * as ICONS from 'constants/icons';
 import React from 'react';
 import Button from 'component/button';
-import * as ICONS from 'constants/icons';
 import Icon from 'component/common/icon';
 import I18nMessage from 'component/i18nMessage';
 
@@ -15,8 +16,15 @@ export default function HiddenNsfw(props: Props) {
   return (
     <div className="section--padded section__subtitle">
       <Icon className="icon--hidden" icon={ICONS.EYE_OFF} />
-      <I18nMessage tokens={{ type, settings: <Button button="link" label={__('settings')} href="/$/settings" /> }}>
-        Content may be hidden on this %type% because of your %settings%
+      <I18nMessage
+        tokens={{
+          type,
+          settings: (
+            <Button button="link" label={__('settings')} navigate={`/$/${PAGES.SETTINGS}`} requiresAuth={IS_WEB} />
+          ),
+        }}
+      >
+        Content may be hidden on this %type% because of your %settings%.
       </I18nMessage>
     </div>
   );
