5397efa76086e01ac372c7029c3cb14f55ed3631
Oleg Silkin <o.silkin98@gmail.com>
2019-10-23 03:04:40 -0400

    Adds comment thumbnails

--- ui/component/comment/index.js
+++ ui/component/comment/index.js
@@ -1,7 +1,28 @@
 import { connect } from 'react-redux';
+import {
+  doResolveUri,
+  makeSelectClaimIsPending,
+  makeSelectClaimForUri,
+  makeSelectThumbnailForUri,
+  makeSelectIsUriResolving,
+  selectChannelIsBlocked,
+} from 'lbry-redux';
+
 import Comment from './view';
 
+const select = (state, props) => ({
+  pending: props.authorUri && makeSelectClaimIsPending(props.authorUri)(state),
+  claim: props.authorUri && makeSelectClaimForUri(props.authorUri)(state),
+  isResolvingUri: props.authorUri && makeSelectIsUriResolving(props.authorUri)(state),
+  thumbnail: props.authorUri && makeSelectThumbnailForUri(props.authorUri)(state),
+  channelIsBlocked: props.authorUri && selectChannelIsBlocked(props.authorUri)(state),
+});
+
+const perform = dispatch => ({
+  resolveUri: uri => dispatch(doResolveUri(uri)),
+});
+
 export default connect(
-  null,
-  null
+  select,
+  perform
 )(Comment);
