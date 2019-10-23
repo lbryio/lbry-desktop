171cfe31aaff2bc9f24d0f6be8d65fc3e010a0c2
Oleg Silkin <o.silkin98@gmail.com>
2019-10-23 03:04:40 -0400

    Adds comment thumbnails

--- ui/component/comment/view.jsx
+++ ui/component/comment/view.jsx
@@ -1,33 +1,57 @@
 // @flow
-import React from 'react';
+import React, { useEffect } from 'react';
+import { isEmpty } from 'util/object';
 import relativeDate from 'tiny-relative-date';
 import Button from 'component/button';
 import Expandable from 'component/expandable';
 import MarkdownPreview from 'component/common/markdown-preview';
+import ChannelThumbnail from 'component/channelThumbnail';
 
 type Props = {
   author: string,
   authorUri: string,
   message: string,
   timePosted: number,
+  claim: ?Claim,
+  pending?: boolean,
+  resolveUri: string => void,
+  isResolvingUri: boolean,
+  channelIsBlocked: boolean,
 };
 
 function Comment(props: Props) {
-  const { author, authorUri, timePosted, message } = props;
+  const {
+    author,
+    authorUri,
+    timePosted,
+    message,
+    pending,
+    claim,
+    isResolvingUri,
+    resolveUri,
+    channelIsBlocked,
+  } = props;
+  // to debounce subsequent requests
+  const shouldFetch =
+    claim === undefined || (claim !== null && claim.value_type === 'channel' && isEmpty(claim.meta) && !pending);
+  useEffect(() => {
+    // If author was extracted from the URI, then it must be valid.
+    if (authorUri && author && !isResolvingUri && shouldFetch) {
+      resolveUri(authorUri);
+    }
+  }, [isResolvingUri, shouldFetch, author, authorUri, resolveUri]);
 
   return (
     <li className="comment">
-      <div className="comment__meta">
-        {!author ? (
-          <span className="comment__author">{__('Anonymous')}</span>
-        ) : (
-          <Button
-            className="button--uri-indicator truncated-text comment__author"
-            navigate={authorUri}
-            label={author}
-          />
-        )}
-
+      <div className="comment__author">
+        <div className="comment__author-thumbnail">
+          {authorUri ? <ChannelThumbnail uri={authorUri} obscure={channelIsBlocked} /> : <ChannelThumbnail />}
+        </div>
+        <Button
+          className="button--uri-indicator truncated-text comment__author-name"
+          navigate={authorUri}
+          label={author || __('Anonymous')}
+        />
         <time className="comment__time" dateTime={timePosted}>
           {relativeDate(timePosted)}
         </time>
