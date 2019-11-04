723d1fabbb5a9d90fb7ebedc5dac0d6430c98098
jessop <jessop@lbry.com>
2019-11-04 12:09:11 -0500

    adds anaytics to download button

--- ui/component/fileDownloadLink/view.jsx
+++ ui/component/fileDownloadLink/view.jsx
@@ -1,7 +1,7 @@
 // @flow
 import * as ICONS from 'constants/icons';
 import * as MODALS from 'constants/modal_types';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'component/button';
 import ToolTip from 'component/common/tooltip';
 
@@ -15,17 +15,27 @@ type Props = {
   openModal: (id: string, { path: string }) => void,
   pause: () => void,
   download: string => void,
+  triggerAnalyticsView: (string, number) => void,
 };
 
 function FileDownloadLink(props: Props) {
-  const { fileInfo, downloading, loading, openModal, pause, claimIsMine, download, uri } = props;
+  const [shouldTrigger, setShouldTrigger] = useState(false);
+  const { fileInfo, downloading, loading, openModal, pause, claimIsMine, download, uri, triggerAnalyticsView } = props;
 
   if (downloading || loading) {
+    if (!shouldTrigger) {
+      setShouldTrigger(true);
+    }
     const progress = fileInfo && fileInfo.written_bytes > 0 ? (fileInfo.written_bytes / fileInfo.total_bytes) * 100 : 0;
     const label =
       fileInfo && fileInfo.written_bytes > 0 ? progress.toFixed(0) + __('% downloaded') : __('Connecting...');
 
     return <span>{label}</span>;
+  }
+
+  if (fileInfo && fileInfo.download_path && fileInfo.completed && shouldTrigger) {
+    triggerAnalyticsView(uri, 0);
+    setShouldTrigger(false);
   }
 
   if (fileInfo && fileInfo.download_path && fileInfo.completed) {
