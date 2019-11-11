630056dd5ba90f349d9b24683fabd5af937e7e48
Sean Yesmunt <sean@lbry.io>
2019-11-11 13:19:31 -0500

    fix redirect middleware

--- lbrytv/index.js
+++ lbrytv/index.js
@@ -4,11 +4,13 @@ const Koa = require('koa');
 const serve = require('koa-static');
 const logger = require('koa-logger');
 const router = require('./src/routes');
+const redirectMiddleware = require('./middleware/redirect');
 
 const app = new Koa();
 const DIST_ROOT = path.resolve(__dirname, 'dist');
 
 app.use(logger());
+app.use(redirectMiddleware);
 app.use(serve(DIST_ROOT)); // Check if the request url matches any assets inside of /dist
 
 app.use(router.routes());
--- lbrytv/src/routes.js
+++ lbrytv/src/routes.js
@@ -1,14 +1,15 @@
 const { getHtml } = require('./html');
 const { generateStreamUrl } = require('../../ui/util/lbrytv');
+const { LBRY_TV_API } = require('../../config');
 const Router = require('@koa/router');
 const send = require('koa-send');
+
 const router = new Router();
 
-router.get(`/embed/:claimName/:claimId`, async ctx => {
-  const { claimName, claimId } = ctx.params;
-  const streamUrl = generateStreamUrl(claimName, claimName);
-  ctx.redirect = streamUrl;
-});
+// TODO
+// router.get(`/embed/:claimName/:claimId`, async ctx => {
+// Proxy request through lbrytv
+// });
 
 router.get('*', async ctx => {
   const html = await getHtml(ctx);
