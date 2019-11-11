0ca01c4d68cd7e5782fd41e14e9885d67d38e751
Sean Yesmunt <sean@lbry.io>
2019-11-11 13:24:22 -0500

    basic error handling

--- lbrytv/index.js
+++ lbrytv/index.js
@@ -9,6 +9,15 @@ const redirectMiddleware = require('./middleware/redirect');
 const app = new Koa();
 const DIST_ROOT = path.resolve(__dirname, 'dist');
 
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+  }
+});
+
 app.use(logger());
 app.use(redirectMiddleware);
 app.use(serve(DIST_ROOT)); // Check if the request url matches any assets inside of /dist
