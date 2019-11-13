15c37db6d2b66ea2485ec477b65a84eb642a19e2
Sean Yesmunt <sean@lbry.io>
2019-11-13 10:46:25 -0500

    beta.lbry.tv => lbry.tv

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   WEBPACK_WEB_PORT: 9090,
   WEBPACK_ELECTRON_PORT: 9091,
   WEB_SERVER_PORT: 1337,
-  DOMAIN: 'https://beta.lbry.tv',
+  DOMAIN: 'https://lbry.tv',
   SITE_TITLE: 'lbry.tv',
   LBRY_TV_API: 'https://api.lbry.tv',
 };
