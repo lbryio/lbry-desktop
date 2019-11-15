0bcb3f7b68f16949291cd27e90d9b2f000e418e5
Sean Yesmunt <sean@lbry.io>
2019-11-15 12:07:32 -0500

    don't re-resolve for claims that don't exist

--- ui/page/show/view.jsx
+++ ui/page/show/view.jsx
@@ -27,6 +27,7 @@ function ShowPage(props: Props) {
   const { channelName, channelClaimId, streamName, streamClaimId } = parseURI(uri);
   const signingChannel = claim && claim.signing_channel;
   const canonicalUrl = claim && claim.canonical_url;
+  const hasClaim = !!claim;
 
   useEffect(() => {
     // @if TARGET='web'
@@ -38,10 +39,10 @@ function ShowPage(props: Props) {
     }
     // @endif
 
-    if (resolveUri && !isResolvingUri && uri && (claim === undefined || !canonicalUrl)) {
+    if (resolveUri && !isResolvingUri && uri && (claim === undefined || (hasClaim && !canonicalUrl))) {
       resolveUri(uri);
     }
-  }, [resolveUri, isResolvingUri, canonicalUrl, uri]);
+  }, [resolveUri, isResolvingUri, canonicalUrl, uri, hasClaim]);
 
   useEffect(() => {
     if (title) {
