ab5f7592ad859c8cfd1053f434c4e067a10ed2ff
Sean Yesmunt <sean@lbry.io>
2019-11-21 12:38:15 -0500

    fix lbry.tv on edge

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = api => {
   return {
     presets: ['@babel/env', '@babel/react', '@babel/flow'],
     plugins: [
+      '@babel/plugin-proposal-object-rest-spread',
       '@babel/plugin-syntax-dynamic-import',
       'import-glob',
       '@babel/plugin-transform-runtime',
