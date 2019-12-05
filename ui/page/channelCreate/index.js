5515c94ce00522596692f39fb32e36e4f455d1f7
Oleg Silkin <o.silkin98@gmail.com>
2019-12-05 17:27:26 -0500

    Pushing up so I can work in class

--- ui/page/channelCreate/index.js
+++ ui/page/channelCreate/index.js
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
 import { selectBalance } from 'lbry-redux';
 import ChannelCreatePage from './view';
+import { withRouter } from 'react-router';
 
 const select = state => ({
   balance: selectBalance(state),
 });
 
-export default connect(select, null)(ChannelCreatePage);
+export default withRouter(connect(select, null)(ChannelCreatePage));
