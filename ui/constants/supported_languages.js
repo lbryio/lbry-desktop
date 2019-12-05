c302796f3929340924d9bd52d043a6fb81703f85
kodxana <romanrex19@int.pl>
2019-12-05 22:02:19 +0100

    Sort alphabetical + new languages

--- ui/constants/supported_languages.js
+++ ui/constants/supported_languages.js
@@ -1,16 +1,24 @@
 import LANGUAGES from './languages';
 
 const SUPPORTED_LANGUAGES = {
-  en: LANGUAGES.en[1],
-  pl: LANGUAGES.pl[1],
-  id: LANGUAGES.id[1],
-  de: LANGUAGES.de[1],
-  fr: LANGUAGES.fr[1],
-  sk: LANGUAGES.sk[1],
-  tr: LANGUAGES.tr[1],
   zh: LANGUAGES.zh[1],
+  hr: LANGUAGES.hr[1],
+  nl: LANGUAGES.nl[1],
+  fr: LANGUAGES.fr[1],
+  de: LANGUAGES.de[1],
+  gu: LANGUAGES.gu[1],
+  hi: LANGUAGES.hi[1],
+  id: LANGUAGES.id[1],
+  it: LANGUAGES.it[1],
+  ms: LANGUAGES.ms[1],
   ml: LANGUAGES.ml[1],
-  sr: LANGUAGES.sr[1]
+  pl: LANGUAGES.pl[1],
+  pt: LANGUAGES.pt[1],
+  ru: LANGUAGES.ru[1],
+  sr: LANGUAGES.sr[1],
+  sk: LANGUAGES.sk[1],
+  es: LANGUAGES.es[1],
+  tr: LANGUAGES.tr[1]
 };
 
 export default SUPPORTED_LANGUAGES;
