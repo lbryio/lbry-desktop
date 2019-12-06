53e2b9140a5dd6dee818b002a9e3515e00bdca08
Sean Yesmunt <sean@lbry.io>
2019-12-06 11:03:11 -0500

    update copy and only show welcome for unverified users

--- ui/component/youtubeWelcome/index.js
+++ ui/component/youtubeWelcome/index.js
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux';
 import { doOpenModal } from 'redux/actions/app';
+import { selectUser } from 'lbryinc';
 import YoutubeWelcome from './view';
 
+const select = state => ({
+  user: selectUser(state),
+});
+
 export default connect(
-  null,
+  select,
   {
     doOpenModal,
   }
