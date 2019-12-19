864d1546f84ecd19c615ea92b5a9ebcf31d23eea
Sean Yesmunt <sean@lbry.io>
2019-12-19 12:22:38 -0500

    cleanup

--- ui/component/publishAdditionalOptions/license-type.jsx
+++ ui/component/publishAdditionalOptions/license-type.jsx
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import { FormField } from 'component/common/form';
 import { CC_LICENSES, LEGACY_CC_LICENSES, COPYRIGHT, OTHER, PUBLIC_DOMAIN, NONE } from 'constants/licenses';
-import analytics from 'analytics';
 
 type Props = {
   licenseType: ?string,
@@ -11,7 +10,6 @@ type Props = {
   handleLicenseChange: (string, string) => void,
   handleLicenseDescriptionChange: (SyntheticInputEvent<*>) => void,
   handleLicenseUrlChange: (SyntheticInputEvent<*>) => void,
-  showToast: string => void,
 };
 
 class LicenseType extends React.PureComponent<Props> {
@@ -22,26 +20,14 @@ class LicenseType extends React.PureComponent<Props> {
   }
 
   handleLicenseOnChange(event: SyntheticInputEvent<*>) {
-    const { handleLicenseChange, showToast } = this.props;
+    const { handleLicenseChange } = this.props;
     // $FlowFixMe
     const { options, selectedIndex } = event.target;
+    const selectedOption = options[selectedIndex];
+    const licenseType = selectedOption.value;
+    const licenseUrl = selectedOption.getAttribute('data-url');
 
-    if (options !== null) {
-      const selectedOption = options[selectedIndex];
-      const licenseType = selectedOption.value;
-      const licenseUrl = selectedOption.getAttribute('data-url');
-
-      handleLicenseChange(licenseType, licenseUrl);
-    } else {
-      // There were users where this options were null for some reason
-      // This will at least make it so the app doesn't crash
-      // Hopefully this helps figure it out
-      analytics.error('Error changing the publish license\n' + JSON.stringify(this.props));
-
-      showToast(
-        __('There was an error updating the license type. If it continues to happen send an email to help@lbry.com.')
-      );
-    }
+    handleLicenseChange(licenseType, licenseUrl);
   }
 
   render() {
