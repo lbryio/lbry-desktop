ebfe11049257b3c563eb2fbe0d1041a8ec9889cc
Sean Yesmunt <sean@lbry.io>
2019-12-26 10:51:27 -0500

    fix typo

--- ui/component/cardMedia/view.jsx
+++ ui/component/cardMedia/view.jsx
@@ -20,7 +20,7 @@ class CardMedia extends React.PureComponent<Props> {
     let url;
     // @if TARGET='web'
     // Pass image urls through a compression proxy
-    url = thumbnail;
+    url = thumbnail || Placeholder;
     // ? 'https://ext.thumbnails.lbry.com/400x,q55/' +
     //   // The image server will redirect if we don't remove the double slashes after http(s)
     //   thumbnail.replace('https://', 'https:/').replace('http://', 'http:/')
