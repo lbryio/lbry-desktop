ce15a96139e054c336a150e6e3b4758f63141874
jessop <jessop@lbry.com>
2019-12-30 14:54:53 -0500

    app plays embed in iframe
    make player full screen and prepared for overlay actions
    
    small changes
    
    cleanup

--- lbrytv/src/routes.js
+++ lbrytv/src/routes.js
@@ -1,15 +1,9 @@
 const { getHtml } = require('./html');
-const { generateStreamUrl, generateDownloadUrl } = require('../../ui/util/lbrytv');
+const { generateDownloadUrl } = require('../../ui/util/lbrytv');
+const { LBRY_TV_API } = require('../../config');
 const Router = require('@koa/router');
-const send = require('koa-send');
 
 const router = new Router();
-
-router.get(`/$/embed/:claimName/:claimId`, async ctx => {
-  const { claimName, claimId } = ctx.params;
-  const streamUrl = generateStreamUrl(claimName, claimId);
-  ctx.redirect(streamUrl);
-});
 
 router.get(`/$/download/:claimName/:claimId`, async ctx => {
   const { claimName, claimId } = ctx.params;
