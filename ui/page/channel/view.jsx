9897d4ee369a34ae2100a7730b0b85e31385948f
Sean Yesmunt <sean@lbry.io>
2020-01-03 11:36:15 -0500

    fix channel page style

--- ui/page/channel/view.jsx
+++ ui/page/channel/view.jsx
@@ -165,110 +165,104 @@ function ChannelPage(props: Props) {
     <Page>
       <ClaimUri uri={uri} />
 
-      <div className="card">
-        <header className="channel-cover">
-          <div className="channel__quick-actions">
-            {!channelIsBlocked && !channelIsBlackListed && <ShareButton uri={uri} isChannel />}
-            {!channelIsMine && (
-              <Button
-                button="alt"
-                icon={ICONS.TIP}
-                label={__('Tip')}
-                title={__('Send a tip to this creator')}
-                onClick={() => openModal(MODALS.SEND_TIP, { uri, channelIsMine, isSupport: false })}
-              />
-            )}
-            {(channelIsMine || (!channelIsMine && supportOption)) && (
-              <Button
-                button="alt"
-                icon={ICONS.SUPPORT}
-                label={__('Support')}
-                title={__('Support this creator')}
-                onClick={() => openModal(MODALS.SEND_TIP, { uri, channelIsMine, isSupport: true })}
-              />
-            )}
-            {!channelIsBlocked && (!channelIsBlackListed || isSubscribed) && <SubscribeButton uri={permanentUrl} />}
-            {!isSubscribed && <BlockButton uri={permanentUrl} />}
-          </div>
-          {!editing && cover && (
-            <img
-              className={classnames('channel-cover__custom', { 'channel__image--blurred': channelIsBlocked })}
-              src={cover}
+      <header className="channel-cover">
+        <div className="channel__quick-actions">
+          {!channelIsBlocked && !channelIsBlackListed && <ShareButton uri={uri} isChannel />}
+          {!channelIsMine && (
+            <Button
+              button="alt"
+              icon={ICONS.TIP}
+              label={__('Tip')}
+              title={__('Send a tip to this creator')}
+              onClick={() => openModal(MODALS.SEND_TIP, { uri, channelIsMine, isSupport: false })}
             />
           )}
-          {editing && <img className="channel-cover__custom" src={coverPreview} />}
-          {/* component that offers select/upload */}
-          <div className="channel__primary-info">
-            {!editing && (
-              <ChannelThumbnail className="channel__thumbnail--channel-page" uri={uri} obscure={channelIsBlocked} />
-            )}
-            {editing && (
-              <ChannelThumbnail
-                className="channel__thumbnail--channel-page"
-                uri={uri}
-                thumbnailPreview={thumbPreview}
-              />
-            )}
-            <h1 className="channel__title">{title || '@' + channelName}</h1>
-            {channelIsMine && !editing && (
-              <Button button="alt" title={__('Edit')} onClick={() => setEditing(!editing)} icon={ICONS.EDIT} />
-            )}
-            <div className="channel__meta">
-              <span>
-                {subCount} {subCount !== 1 ? __('Followers') : __('Follower')}
-                <HelpLink href="https://lbry.com/faq/views" />
-              </span>
-            </div>
+          {(channelIsMine || (!channelIsMine && supportOption)) && (
+            <Button
+              button="alt"
+              icon={ICONS.SUPPORT}
+              label={__('Support')}
+              title={__('Support this creator')}
+              onClick={() => openModal(MODALS.SEND_TIP, { uri, channelIsMine, isSupport: true })}
+            />
+          )}
+          {!channelIsBlocked && (!channelIsBlackListed || isSubscribed) && <SubscribeButton uri={permanentUrl} />}
+          {!isSubscribed && <BlockButton uri={permanentUrl} />}
+        </div>
+        {!editing && cover && (
+          <img
+            className={classnames('channel-cover__custom', { 'channel__image--blurred': channelIsBlocked })}
+            src={cover}
+          />
+        )}
+        {editing && <img className="channel-cover__custom" src={coverPreview} />}
+        {/* component that offers select/upload */}
+        <div className="channel__primary-info">
+          {!editing && (
+            <ChannelThumbnail className="channel__thumbnail--channel-page" uri={uri} obscure={channelIsBlocked} />
+          )}
+          {editing && (
+            <ChannelThumbnail className="channel__thumbnail--channel-page" uri={uri} thumbnailPreview={thumbPreview} />
+          )}
+          <h1 className="channel__title">{title || '@' + channelName}</h1>
+          {channelIsMine && !editing && (
+            <Button button="alt" title={__('Edit')} onClick={() => setEditing(!editing)} icon={ICONS.EDIT} />
+          )}
+          <div className="channel__meta">
+            <span>
+              {subCount} {subCount !== 1 ? __('Followers') : __('Follower')}
+              <HelpLink href="https://lbry.com/faq/views" />
+            </span>
           </div>
-        </header>
-        <Tabs onChange={onTabChange} index={tabIndex}>
-          <TabList className="tabs__list--channel-page">
-            <Tab disabled={editing}>{__('Content')}</Tab>
-            <Tab>{editing ? __('Editing Your Channel') : __('About')}</Tab>
-            <Tab disabled={editing}>{__('Comments')}</Tab>
-            {/* only render searchbar on content page (tab index 0 === content page) */}
-            {tabIndex === 0 ? (
-              <Form onSubmit={handleSearch} className="wunderbar--inline">
-                <Icon icon={ICONS.SEARCH} />
-                <FormField
-                  className="wunderbar__input"
-                  value={searchQuery}
-                  onChange={handleInputChange}
-                  type="text"
-                  placeholder={__('Search')}
-                />
-              </Form>
-            ) : (
-              <div />
-            )}
-          </TabList>
+        </div>
+      </header>
+      <Tabs onChange={onTabChange} index={tabIndex}>
+        <TabList className="tabs__list--channel-page">
+          <Tab disabled={editing}>{__('Content')}</Tab>
+          <Tab>{editing ? __('Editing Your Channel') : __('About')}</Tab>
+          <Tab disabled={editing}>{__('Comments')}</Tab>
+          {/* only render searchbar on content page (tab index 0 === content page) */}
+          {tabIndex === 0 ? (
+            <Form onSubmit={handleSearch} className="wunderbar--inline">
+              <Icon icon={ICONS.SEARCH} />
+              <FormField
+                className="wunderbar__input"
+                value={searchQuery}
+                onChange={handleInputChange}
+                type="text"
+                placeholder={__('Search')}
+              />
+            </Form>
+          ) : (
+            <div />
+          )}
+        </TabList>
 
-          <TabPanels>
-            <TabPanel>
-              {searchResults ? (
-                searchResults.map(url => <ClaimPreview key={url} uri={url} />)
-              ) : (
-                <ChannelContent uri={uri} channelIsBlackListed={channelIsBlackListed} />
-              )}
-            </TabPanel>
-            <TabPanel>
-              {editing ? (
-                <ChannelEdit
-                  uri={uri}
-                  setEditing={setEditing}
-                  updateThumb={v => setThumbPreview(v)}
-                  updateCover={v => setCoverPreview(v)}
-                />
-              ) : (
-                <ChannelAbout uri={uri} />
-              )}
-            </TabPanel>
-            <TabPanel>
-              <ChannelDiscussion uri={uri} />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </div>
+        <TabPanels>
+          <TabPanel>
+            {searchResults ? (
+              searchResults.map(url => <ClaimPreview key={url} uri={url} />)
+            ) : (
+              <ChannelContent uri={uri} channelIsBlackListed={channelIsBlackListed} />
+            )}
+          </TabPanel>
+          <TabPanel>
+            {editing ? (
+              <ChannelEdit
+                uri={uri}
+                setEditing={setEditing}
+                updateThumb={v => setThumbPreview(v)}
+                updateCover={v => setCoverPreview(v)}
+              />
+            ) : (
+              <ChannelAbout uri={uri} />
+            )}
+          </TabPanel>
+          <TabPanel>
+            <ChannelDiscussion uri={uri} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Page>
   );
 }
