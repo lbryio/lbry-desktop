e7c0ee59e4cb09918638a25939c728b7c474d18a
Sean Yesmunt <sean@lbry.io>
2020-01-07 16:08:39 -0500

    add New icon

--- ui/component/common/icon-custom.jsx
+++ ui/component/common/icon-custom.jsx
@@ -348,6 +348,14 @@ export const icons = {
       <line x1="6" y1="20" x2="6" y2="16" />
     </g>
   ),
+  [ICONS.NEW]: buildIcon(
+    <g>
+      <circle cx="12" cy="12" r="7" />
+      <polyline points="12 9 12 12 13.5 13.5" />
+      <path d="M16.51 17.35l-.35 3.83a2 2 0 0 1-2 1.82H9.83a2 2 0 0 1-2-1.82l-.35-3.83m.01-10.7l.35-3.83A2 2 0 0 1 9.83 1h4.35a2 2 0 0 1 2 1.82l.35 3.83" />{' '}
+    </g>
+  ),
+
   [ICONS.INVITE]: buildIcon(
     <g>
       <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
--- ui/constants/icons.js
+++ ui/constants/icons.js
@@ -80,3 +80,4 @@ export const SIGN_OUT = 'SignOut';
 export const SIGN_IN = 'SignIn';
 export const TRENDING = 'Trending';
 export const TOP = 'Top';
+export const NEW = 'New';
