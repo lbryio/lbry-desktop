46927e931cb935cb03595b0fe09ad780c3b0c563
Oleg Silkin <o.silkin98@gmail.com>
2020-01-06 19:48:26 -0500

    Adds the three-dot icons; both horizontal and vertical alignments

--- ui/component/common/icon-custom.jsx
+++ ui/component/common/icon-custom.jsx
@@ -357,47 +357,11 @@ export const icons = {
       <path d="M16 3.13a4 4 0 0 1 0 7.75" />
     </g>
   ),
-  [ICONS.VIDEO]: buildIcon(
+  [ICONS.MORE_HORIZONTAL]: buildIcon(
     <g>
-      <polygon points="23 7 16 12 23 17 23 7" />
-      <rect x="1" y="5" width="15" height="14" rx="2" ry="2" />
-    </g>
-  ),
-  [ICONS.AUDIO]: buildIcon(
-    <g>
-      <path d="M3 18v-6a9 9 0 0 1 18 0v6" />
-      <path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z" />
-    </g>
-  ),
-  [ICONS.IMAGE]: buildIcon(
-    <g>
-      <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
-      <circle cx="8.5" cy="8.5" r="1.5" />
-      <polyline points="21 15 16 10 5 21" />
-    </g>
-  ),
-  [ICONS.TEXT]: buildIcon(
-    <g>
-      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-      <polyline points="14 2 14 8 20 8" />
-      <line x1="16" y1="13" x2="8" y2="13" />
-      <line x1="16" y1="17" x2="8" y2="17" />
-      <polyline points="10 9 9 9 8 9" />
-    </g>
-  ),
-  [ICONS.DOWNLOADABLE]: buildIcon(
-    <g>
-      <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z" />
-      <polyline points="17 21 17 13 7 13 7 21" />
-      <polyline points="7 3 7 8 15 8" />
-    </g>
-  ),
-  [ICONS.REPOST]: buildIcon(
-    <g>
-      <polyline points="17 1 21 5 17 9" />
-      <path d="M3 11V9a4 4 0 0 1 4-4h14" />
-      <polyline points="7 23 3 19 7 15" />
-      <path d="M21 13v2a4 4 0 0 1-4 4H3" />
+      <circle cx="12" cy="12" r="1" />
+      <circle cx="19" cy="12" r="1" />
+      <circle cx="5" cy="12" r="1" />
     </g>
   ),
   [ICONS.MORE_VERTICAL]: buildIcon(
