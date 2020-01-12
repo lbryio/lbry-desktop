6dcb6f48671a3097af9231c7b937750cac8a13b3
tuxfoo <tuxfoo@librem.one>
2020-01-12 17:35:03 +1300

    Turn img into canvas to load correct orientation

--- ui/component/viewers/imageViewer.jsx
+++ ui/component/viewers/imageViewer.jsx
@@ -1,17 +1,30 @@
 // @flow
 import React from 'react';
+import loadImage from 'blueimp-load-image';
 
 type Props = {
   source: string,
 };
 
+function loadPic(source) {
+  loadImage(
+    source,
+    function(img, data) {
+      if (img.type === 'error') {
+        console.error('Cannot load image');
+      } else {
+        document.getElementsByClassName('file-render__viewer')[0].appendChild(img);
+      }
+    },
+    {
+      orientation: true,
+    }
+  );
+}
+
 function ImageViewer(props: Props) {
   const { source } = props;
-  return (
-    <div className="file-render__viewer">
-      <img src={source} />
-    </div>
-  );
+  return <div className="file-render__viewer">{loadPic(source)}</div>;
 }
 
 export default ImageViewer;
