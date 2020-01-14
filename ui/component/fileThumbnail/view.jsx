2659ad17ebaa8725befe75748c310846bde0db5c
Sean Yesmunt <sean@lbry.io>
2020-01-14 18:03:49 -0500

    thumbnail server bad

--- ui/component/fileThumbnail/view.jsx
+++ ui/component/fileThumbnail/view.jsx
@@ -20,12 +20,12 @@ class CardMedia extends React.PureComponent<Props> {
     let url;
     // @if TARGET='web'
     // Pass image urls through a compression proxy
-    // url = thumbnail || Placeholder;
-    url = thumbnail
-      ? 'https://ext.thumbnails.lbry.com/400x,q55/' +
-        // The image server will redirect if we don't remove the double slashes after http(s)
-        thumbnail.replace('https://', 'https:/').replace('http://', 'http:/')
-      : Placeholder;
+    url = thumbnail || Placeholder;
+    // url = thumbnail
+    //   ? 'https://ext.thumbnails.lbry.com/400x,q55/' +
+    //     // The image server will redirect if we don't remove the double slashes after http(s)
+    //     thumbnail.replace('https://', 'https:/').replace('http://', 'http:/')
+    //   : Placeholder;
     // @endif
     // @if TARGET='app'
     url = thumbnail || Placeholder;
