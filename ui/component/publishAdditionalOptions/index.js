a18996b68150c286d0e038ca2b8ed5de505af0b7
Yamboy1 <yamboyd1@gmail.com>
2020-01-15 06:38:17 +1300

    Revert "Add selectDefaultLanguage for publishes"
    This reverts commit a4cdca9b

--- ui/component/publishAdditionalOptions/index.js
+++ ui/component/publishAdditionalOptions/index.js
@@ -1,22 +1,9 @@
 import { connect } from 'react-redux';
-import { createSelector } from 'reselect';
 import { selectPublishFormValues, doUpdatePublishForm } from 'lbry-redux';
-import { selectIsStillEditing } from 'redux/selectors/publish';
 import PublishPage from './view';
-
-const selectDefaultLanguage = createSelector(
-  selectPublishFormValues,
-  selectIsStillEditing,
-  (publishState, isStillEditing) => {
-    const { languages, language } = publishState;
-    // returns the language of the original claim if editing
-    return isStillEditing ? languages[0] : language;
-  }
-);
 
 const select = state => ({
   ...selectPublishFormValues(state),
-  defaultLanguage: selectDefaultLanguage(state),
 });
 
 const perform = dispatch => ({
