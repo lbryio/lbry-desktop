76e4281b4f0548a2b13c1663f56788f9f2c2c0f2
Oleg Silkin <o.silkin98@gmail.com>
2020-01-25 20:24:33 -0500

    Implements tip capability

--- ui/component/commentCreate/index.js
+++ ui/component/commentCreate/index.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { doCommentCreate, makeSelectClaimForUri } from 'lbry-redux';
+import { doCommentCreate, makeSelectClaimForUri, selectIsSendingSupport, selectBalnce, doSendTip } from 'lbry-redux';
 import { doOpenModal } from 'redux/actions/app';
 import { CommentCreate } from './view';
 import { selectUserVerifiedEmail } from 'lbryinc';
@@ -7,11 +7,14 @@ import { selectUserVerifiedEmail } from 'lbryinc';
 const select = (state, props) => ({
   commentingEnabled: IS_WEB ? Boolean(selectUserVerifiedEmail(state)) : true,
   claim: makeSelectClaimForUri(props.uri)(state),
+  balance: selectBalnce(state),
+  isPending: selectIsSendingSupport(state),
 });
 
 const perform = dispatch => ({
   createComment: (comment, claimId, channel) => dispatch(doCommentCreate(comment, claimId, channel)),
   openModal: (modal, props) => dispatch(doOpenModal(modal, props)),
+  sendSupport: (amount, claimId, isSupport) => dispatch(doSendTip(amount, claimId, isSupport)),
 });
 
 export default connect(
--- ui/component/commentCreate/view.jsx
+++ ui/component/commentCreate/view.jsx
@@ -1,12 +1,12 @@
 // @flow
-import { CHANNEL_NEW } from 'constants/claim';
+import { CHANNEL_NEW, MINIMUM_PUBLISH_BID } from 'constants/claim';
 import React, { useEffect, useState } from 'react';
 import { FormField, Form } from 'component/common/form';
 import Button from 'component/button';
 import ChannelSection from 'component/selectChannel';
 import usePersistedState from 'effects/use-persisted-state';
 import * as MODALS from 'constants/modal_types';
-import I18nMessage from '../i18nMessage/view';
+import I18nMessage from 'component/i18nMessage';
 
 type Props = {
   commentingEnabled: boolean,
@@ -14,15 +14,21 @@ type Props = {
   claim: StreamClaim,
   openModal: (id: string, { onCommentAcknowledge: () => void }) => void,
   createComment: (string, string, string) => void,
+  balance: number,
+  isPending: boolean,
+  sendSupport: (number, string, boolean) => void,
 };
 
 export function CommentCreate(props: Props) {
-  const { commentingEnabled, createComment, claim, openModal } = props;
+  const { commentingEnabled, createComment, claim, openModal, balance, isPending, sendSupport } = props;
   const { claim_id: claimId } = claim;
   const [commentValue, setCommentValue] = usePersistedState(`comment-${claimId}`, '');
   const [commentAck, setCommentAck] = usePersistedState('comment-acknowledge', false);
   const [channel, setChannel] = usePersistedState('comment-channel', 'anonymous');
   const [charCount, setCharCount] = useState(commentValue.length);
+  const [isTipping, setTippingStatus] = usePersistedState('comment-is-tipping', false);
+  const [tipAmount, setTipAmount] = usePersistedState('comment-tip', 0.0);
+  const [tipError, setTipError] = useState('');
 
   function handleCommentChange(event) {
     setCommentValue(event.target.value);
@@ -43,8 +49,40 @@ export function CommentCreate(props: Props) {
   }
 
   function handleSubmit() {
-    if (channel !== CHANNEL_NEW && commentValue.length) createComment(commentValue, claimId, channel);
+    if (channel !== CHANNEL_NEW && commentValue.length && !(isPending || tipError)) {
+      sendSupport(tipAmount, claimId, false);
+      createComment(commentValue, claimId, channel);
+    }
     setCommentValue('');
+    setTippingStatus(false);
+  }
+
+  function handleTippingStatus() {
+    setTippingStatus(!isTipping);
+  }
+
+  function handleTipAmountChanged(event) {
+    const regexp = RegExp(/^(\d*([.]\d{0,8})?)$/);
+    const validTipInput = regexp.test(event.target.value);
+    const tipAmount = parseFloat(event.target);
+    let _tipError = '';
+
+    if (!tipAmount) {
+      _tipError = __('Amount must be a number');
+    } else if (tipAmount <= 0) {
+      _tipError = __('Amount must be a positive number');
+    } else if (tipAmount < MINIMUM_PUBLISH_BID) {
+      _tipError = __('Amount must be higher');
+    } else if (!validTipInput) {
+      _tipError = __('Amount must have no more than 8 decimal places');
+    } else if (tipAmount === balance) {
+      _tipError = __('Please decrease the amount to account for transaction fees');
+    } else if (tipAmount > balance) {
+      _tipError = __('Not enough credits');
+    }
+
+    setTipAmount(event.target.value);
+    setTipError(_tipError);
   }
 
   useEffect(() => setCharCount(commentValue.length), [commentValue]);
@@ -64,20 +102,39 @@ export function CommentCreate(props: Props) {
         disabled={channel === CHANNEL_NEW}
         type="textarea"
         name="content_description"
-        label={__('Comment')}
         onFocus={onTextareaFocus}
         placeholder={__('Say something about this...')}
         value={commentValue}
         charCount={charCount}
         onChange={handleCommentChange}
       />
+      <FormField
+        name="coment-is-tipping"
+        type="checkbox"
+        label={__('Include Tip')}
+        checked={isTipping}
+        onChange={handleTippingStatus}
+      />
+      {isTipping && (
+        <FormField
+          name="comment-tip-amount"
+          className="form-field--price-amount"
+          min="0"
+          step="any"
+          type="number"
+          placeholder="1.23"
+          onChange={handleTipAmountChanged}
+          error={tipError}
+        />
+      )}
       <div className="section__actions">
         <Button
           button="primary"
-          disabled={channel === CHANNEL_NEW || !commentValue.length}
+          disabled={channel === CHANNEL_NEW || !commentValue.length || isPending || tipError}
           type="submit"
           label={__('Post')}
           requiresAuth={IS_WEB}
+          onClick={handleSubmit}
         />
       </div>
     </Form>
