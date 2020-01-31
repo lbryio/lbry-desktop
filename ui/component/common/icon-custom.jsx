e290cec7b8d52be082650485fe1072e7f58c1d36
Sean Yesmunt <sean@lbry.io>
2020-01-31 16:27:58 -0500

    fix rebase

--- ui/component/common/icon-custom.jsx
+++ ui/component/common/icon-custom.jsx
@@ -398,6 +398,8 @@ export const icons = {
       <path d="M3 11V9a4 4 0 0 1 4-4h14" />
       <polyline points="7 23 3 19 7 15" />
       <path d="M21 13v2a4 4 0 0 1-4 4H3" />
+    </g>
+  ),
   [ICONS.MORE_VERTICAL]: buildIcon(
     <g>
       <circle cx="12" cy="5" r="1" />
