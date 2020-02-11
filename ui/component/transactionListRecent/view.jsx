3ad923b4f81a03e1946ea1c29b80683220225617
Sean Yesmunt <sean@lbry.io>
2020-02-11 17:40:04 -0500

    fetch my claims on wallet page so reposts are deleteable

--- ui/component/transactionListRecent/view.jsx
+++ ui/component/transactionListRecent/view.jsx
@@ -5,31 +5,37 @@ import { TX_LIST } from 'lbry-redux';
 
 type Props = {
   fetchTransactions: (number, number) => void,
+  fetchClaimListMine: () => void,
   fetchingTransactions: boolean,
   hasTransactions: boolean,
   transactions: Array<Transaction>,
+  myClaims: ?Array<StreamClaim>,
 };
 
-class TransactionListRecent extends React.PureComponent<Props> {
-  componentDidMount() {
-    const { fetchTransactions } = this.props;
+function TransactionListRecent(props: Props) {
+  const { transactions, fetchTransactions, myClaims, fetchClaimListMine } = props;
 
+  React.useEffect(() => {
     fetchTransactions(1, TX_LIST.LATEST_PAGE_SIZE);
-  }
+  }, [fetchTransactions]);
 
-  render() {
-    const { transactions } = this.props;
-    return (
-      <section className="card">
-        <TransactionList
-          slim
-          title={__('Latest Transactions')}
-          transactions={transactions}
-          emptyMessage={__("Looks like you don't have any transactions.")}
-        />
-      </section>
-    );
-  }
+  const myClaimsString = myClaims && myClaims.map(channel => channel.permanent_url).join(',');
+  React.useEffect(() => {
+    if (myClaimsString === '') {
+      fetchClaimListMine();
+    }
+  }, [myClaimsString, fetchClaimListMine]);
+
+  return (
+    <section className="card">
+      <TransactionList
+        slim
+        title={__('Latest Transactions')}
+        transactions={transactions}
+        emptyMessage={__("Looks like you don't have any transactions.")}
+      />
+    </section>
+  );
 }
 
 export default TransactionListRecent;
