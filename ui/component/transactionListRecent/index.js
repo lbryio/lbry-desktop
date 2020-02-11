53b225d1ea14b360176f2eac6407ec96a75337b2
Sean Yesmunt <sean@lbry.io>
2020-02-11 17:40:04 -0500

    fetch my claims on wallet page so reposts are deleteable

--- ui/component/transactionListRecent/index.js
+++ ui/component/transactionListRecent/index.js
@@ -1,15 +1,22 @@
 import { connect } from 'react-redux';
-import { doFetchTransactions, makeSelectLatestTransactions } from 'lbry-redux';
+import {
+  doFetchTransactions,
+  makeSelectLatestTransactions,
+  doFetchClaimListMine,
+  selectMyClaimsWithoutChannels,
+} from 'lbry-redux';
 import TransactionListRecent from './view';
 
 const select = state => {
   return {
     transactions: makeSelectLatestTransactions(state),
+    myClaims: selectMyClaimsWithoutChannels(state),
   };
 };
 
 const perform = dispatch => ({
   fetchTransactions: (page, pageSize) => dispatch(doFetchTransactions(page, pageSize)),
+  fetchClaimListMine: () => dispatch(doFetchClaimListMine()),
 });
 
 export default connect(
