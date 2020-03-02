3ee25f8251c1d7756db7a63e469f50ecb4f6e5aa
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2020-03-02 12:11:14 -0500

    fixes: default channel bid and publish form (#3781)
    Changes channel default bid and fixes publish form errors around bid amount.

--- ui/component/userFirstChannel/view.jsx
+++ ui/component/userFirstChannel/view.jsx
@@ -4,7 +4,7 @@ import { isNameValid } from 'lbry-redux';
 import Button from 'component/button';
 import { Form, FormField } from 'component/common/form';
 import { INVALID_NAME_ERROR } from 'constants/claim';
-export const DEFAULT_BID_FOR_FIRST_CHANNEL = 0.5;
+export const DEFAULT_BID_FOR_FIRST_CHANNEL = 0.01;
 
 type Props = {
   createChannel: (string, number) => void,
