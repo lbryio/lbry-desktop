a5e2cb3e76176b3d037dd94cbf4f8d849d60ed37
Jeremy Kauffman <jeremy@lbry.io>
2020-03-12 10:57:38 -0400

    got frustrated about no claim id on channels and went wild
    update text

--- ui/component/channelAbout/index.js
+++ ui/component/channelAbout/index.js
@@ -1,14 +1,12 @@
 import { connect } from 'react-redux';
-import { makeSelectMetadataItemForUri } from 'lbry-redux';
+import { makeSelectMetadataItemForUri, makeSelectClaimForUri } from 'lbry-redux';
 import ChannelAbout from './view';
 
 const select = (state, props) => ({
+  claim: makeSelectClaimForUri(props.uri)(state),
   description: makeSelectMetadataItemForUri(props.uri, 'description')(state),
   website: makeSelectMetadataItemForUri(props.uri, 'website_url')(state),
   email: makeSelectMetadataItemForUri(props.uri, 'email')(state),
 });
 
-export default connect(
-  select,
-  null
-)(ChannelAbout);
+export default connect(select, null)(ChannelAbout);
