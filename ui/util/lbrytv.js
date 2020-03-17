2e9ed995908f162e8f4809bae8ba890c03acce77
Sean Yesmunt <sean@lbry.io>
2020-03-17 13:47:04 -0400

    Revert "Localized player urls for lbry.tv"

--- ui/util/lbrytv.js
+++ ui/util/lbrytv.js
@@ -1,33 +1,8 @@
-const { Lbryio } = require('lbryinc');
 const { URL, LBRY_TV_STREAMING_API } = require('../../config');
-const { getCookie, setCookie } = require('../../ui/util/saved-passwords');
-
-const CONTINENT_COOKIE = 'continent';
 
 function generateStreamUrl(claimName, claimId, apiUrl) {
-  let prefix = LBRY_TV_STREAMING_API || apiUrl;
-  const continent = getCookie(CONTINENT_COOKIE);
-
-  if (continent && prefix.split('//').length > 1) {
-    prefix = prefix.replace('//', '//' + continent + '.');
-  } else {
-    Lbryio.call('locale', 'get', {}, 'post').then(result => {
-      const userContinent = getSupportedCDN(result.continent);
-      setCookie(CONTINENT_COOKIE, userContinent, 1);
-    });
-  }
-
+  const prefix = LBRY_TV_STREAMING_API || apiUrl;
   return `${prefix}/content/claims/${claimName}/${claimId}/stream`;
-}
-
-function getSupportedCDN(continent) {
-  switch (continent) {
-    case 'NA':
-    case 'EU':
-      return continent;
-    default:
-      return 'NA';
-  }
 }
 
 function generateEmbedUrl(claimName, claimId) {
