7218b787460c505faaab10fb966035824375c9a5
Sean Yesmunt <sean@lbry.io>
2020-04-08 13:10:33 -0400

    fetch continent cookie on server-side and clean up video effects

--- ui/util/lbrytv.js
+++ ui/util/lbrytv.js
@@ -1,6 +1,5 @@
-const { Lbryio } = require('lbryinc/dist/bundle.es.js');
 const { URL, LBRY_TV_STREAMING_API } = require('../../config');
-const { getCookie, setCookie } = require('../../ui/util/saved-passwords');
+const { getCookie } = require('../../ui/util/saved-passwords');
 
 const CONTINENT_COOKIE = 'continent';
 
@@ -10,24 +9,9 @@ function generateStreamUrl(claimName, claimId, apiUrl, streamingContinent) {
 
   if (continent && prefix.split('//').length > 1) {
     prefix = prefix.replace('//', '//' + continent + '.');
-  } else {
-    Lbryio.call('locale', 'get', {}, 'post').then(result => {
-      const userContinent = getSupportedCDN(result.continent);
-      setCookie(CONTINENT_COOKIE, userContinent, 1);
-    });
   }
 
   return `${prefix}/content/claims/${claimName}/${claimId}/stream`;
-}
-
-function getSupportedCDN(continent) {
-  switch (continent) {
-    case 'NA':
-    case 'EU':
-      return continent;
-    default:
-      return 'NA';
-  }
 }
 
 function generateEmbedUrl(claimName, claimId) {
