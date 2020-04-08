c23bb89fcd7df77978862dcbcabce85fd4bb1fb5
Sean Yesmunt <sean@lbry.io>
2020-04-08 13:43:29 -0400

    Revert "fixes"
    This reverts commit 9187d0d71d80fc61e4be3aa4e34cc856edc98e80.

--- lbrytv/src/html.js
+++ lbrytv/src/html.js
@@ -63,10 +63,11 @@ function buildClaimOgMetadata(uri, claim, overrideOptions = {}) {
       ? escapeHtmlProperty(truncateDescription(claim.description))
       : `View ${claimTitle} on lbry.tv`;
   const claimLanguage = escapeHtmlProperty(claim.language) || 'en_US';
-
+  const isImage = claim && claim.value && claim.value.stream_type === 'image';
+  const isFree = claim && claim.value && (!claim.value.fee || Number(claim.value.fee.amount) <= 0);
   let imageThumbnail;
 
-  if (Number(claim.fee) <= 0 && claim.source_media_type.startsWith('image/')) {
+  if (claim && isImage && isFree) {
     imageThumbnail = generateStreamUrl(claimName, claim.claim_id);
   }
   const claimThumbnail = escapeHtmlProperty(claim.thumbnail_url) || imageThumbnail || `${URL}/v1-og.png`;
