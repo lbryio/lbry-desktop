382eb256f4478ecf40971570b9be71230664145a
Sean Yesmunt <sean@lbry.io>
2020-04-14 14:12:19 -0400

    remove tag edit from tag following page since it was added to first run

--- ui/page/tagsFollowing/view.jsx
+++ ui/page/tagsFollowing/view.jsx
@@ -3,22 +3,14 @@ import * as PAGES from 'constants/pages';
 import * as ICONS from 'constants/icons';
 import React from 'react';
 import ClaimListDiscover from 'component/claimListDiscover';
-import TagsSelect from 'component/tagsSelect';
 import Page from 'component/page';
 import Button from 'component/button';
 import Icon from 'component/common/icon';
 import * as CS from 'constants/claim_search';
 
-type Props = {
-  email: string,
-};
-
-function DiscoverPage(props: Props) {
-  const { email } = props;
-
+function DiscoverPage() {
   return (
     <Page>
-      {(email || !IS_WEB) && <TagsSelect showClose limitShow={300} title={__('Find New Tags To Follow')} />}
       <ClaimListDiscover
         headerLabel={
           <span>
@@ -26,7 +18,6 @@ function DiscoverPage(props: Props) {
             {__('Your Tags')}
           </span>
         }
-        hideCustomization={IS_WEB && !email}
         personalView
         defaultTags={CS.TAGS_FOLLOWED}
         meta={
