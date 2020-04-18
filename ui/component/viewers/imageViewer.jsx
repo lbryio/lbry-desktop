5a43f62181c262c52ede2c674f168b43e76b6c59
Jeffrey Fisher <jeff@slofish.io>
2020-04-17 18:41:15 -0700

    First attempt to fix #3993. Show error message if image is not able to be displayed by browser.

--- ui/component/viewers/imageViewer.jsx
+++ ui/component/viewers/imageViewer.jsx
@@ -1,5 +1,7 @@
 // @flow
 import React from 'react';
+import Card from 'component/common/card';
+import ErrorText from 'component/common/error-text';
 
 type Props = {
   source: string,
@@ -7,10 +9,23 @@ type Props = {
 
 function ImageViewer(props: Props) {
   const { source } = props;
+  const [loadingError, setLoadingError] = React.useState(false);
+
   return (
-    <div className="file-render__viewer">
-      <img src={source} />
-    </div>
+    <React.Fragment>
+      {loadingError && (
+        <Card
+          title={__('Error Displaying Image')}
+          defaultExpand
+          actions={<ErrorText>There was an error displaying the image. You may still download it.</ErrorText>}
+        />
+      )}
+      {!loadingError && (
+        <div className="file-render__viewer">
+          <img src={source} onError={() => setLoadingError(true)} />
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
