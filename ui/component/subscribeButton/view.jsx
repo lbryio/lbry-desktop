888ac6f3ca7473d71ce0e5996b50647822005d8a
Mr. X <mjkgoa@gmail.com>
2020-04-23 22:04:47 +0530

    fix: better subscribe button for both mobile and desktop

--- ui/component/subscribeButton/view.jsx
+++ ui/component/subscribeButton/view.jsx
@@ -36,27 +36,26 @@ export default function SubscribeButton(props: Props) {
     doToast,
     shrinkOnMobile = false,
   } = props;
+
   const buttonRef = useRef();
-  const isHovering = useHover(buttonRef);
   const isMobile = useIsMobile();
+  let isHovering = useHover(buttonRef);
+  isHovering = isMobile ? true : isHovering;
+
   const { channelName } = parseURI(permanentUrl);
   const claimName = '@' + channelName;
+
   const subscriptionHandler = isSubscribed ? doChannelUnsubscribe : doChannelSubscribe;
   const subscriptionLabel = isSubscribed ? __('Following') : __('Follow');
   const unfollowOverride = isSubscribed && isHovering && __('Unfollow');
+
   const label = isMobile && shrinkOnMobile ? '' : unfollowOverride || subscriptionLabel;
-
-  let longestStr = __('Following');
-
-  if (__('Following').length < __('Unfollow').length) {
-    longestStr = __('Unfollow');
-  }
 
   return permanentUrl ? (
     <Button
       ref={buttonRef}
       iconColor="red"
-      largestLabel={isSubscribed ? longestStr : undefined}
+      largestLabel={isMobile && shrinkOnMobile ? '' : subscriptionLabel}
       icon={unfollowOverride ? ICONS.UNSUBSCRIBE : ICONS.SUBSCRIBE}
       button={'alt'}
       requiresAuth={IS_WEB}
