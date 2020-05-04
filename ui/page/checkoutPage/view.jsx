a02fe517cbf8b416b2c409d3da46df2ee64358d4
Sean Yesmunt <sean@lbry.io>
2020-05-04 13:19:20 -0400

    add credit card image and fake cost text

--- ui/page/checkoutPage/view.jsx
+++ ui/page/checkoutPage/view.jsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Page from 'component/page';
 import Button from 'component/button';
 import Card from 'component/common/card';
+import CreditCards from './credit-card-logos.png';
 
 export default function CheckoutPage() {
   return (
@@ -13,11 +14,15 @@ export default function CheckoutPage() {
         body={
           <div className="card--inline card--section card--highlighted">
             <strong>{__('lbry.tv Premium - 1 month')}</strong>
+            <div>$5 per month</div>
           </div>
         }
         actions={
           <div className="section__actions">
             <Button button="primary" label={__('Checkout')} />
+            <div>
+              <img src={CreditCards} style={{ height: '2rem' }} />
+            </div>
           </div>
         }
       />
