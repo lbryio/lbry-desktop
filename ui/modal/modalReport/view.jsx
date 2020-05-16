0cf9fd9c47e6e389d2a66cda13695d5a78857880
root <root@localhost.localdomain>
2020-05-16 13:59:45 -0400

    commit scaffold

--- ui/modal/modalReport/view.jsx
+++ ui/modal/modalReport/view.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Modal } from 'modal/modal';
 import Button from 'component/button';
-import { FormField } from 'component/common/form';
 
 type Props = {
   closeModal: () => void,
@@ -12,22 +11,15 @@ const ModalReport = (props: Props) => {
   const { closeModal } = props;
 
   return (
-    <Modal type="custom" isOpen contentLabel="Report Content" title={__('Report Content')}>
-      <p>{__('Choose an option according to the violation')}</p>
+    <Modal type="custom" isOpen contentLabel="Welcome to LBRY" title={__('Welcome to LBRY')}>
       <p>
-        <hr> </hr>
+        {__('Using LBRY is like dating a centaur. Totally normal up top, and')} <em>{__('way different')}</em>{' '}
+        {__('underneath.')}
       </p>
-      <React.Fragment>
-        <FormField type="radio" name="Mistagged content" label={__('Mistagged Content')} />
-        <FormField type="radio" name="Spam" label={__('Spam')} />
-        <FormField type="radio" name="Illegal Promotions" label={__('Illegal Promotion')} />
-        <FormField type="radio" name="Violent or repulsive" label={__('Violent or repulsive')} />
-        <FormField type="radio" name="Child abuse" label={__('Child abuse')} />
-        <FormField type="radio" name="Promotes Terrorism" label={__('Promotes Terrorism')} />
-      </React.Fragment>
+      <p>{__('Up top, LBRY is similar to popular media sites.')}</p>
+      <p>{__('Below, LBRY is controlled by users -- you -- via blockchain and decentralization.')}</p>
       <div className="card__actions">
-        <Button button="primary" onClick={closeModal} label={__('Send')} />
-        <Button button="alt" onClick={closeModal} label={__('Close')} />
+        <Button button="primary" onClick={closeModal} label={__("I'm In")} />
       </div>
     </Modal>
   );
