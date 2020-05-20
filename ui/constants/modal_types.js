8e8b77984caac7883d448e780b3be72eb7a4d676
Harsh Khandeparkar <mjkgoa@gmail.com>
2020-05-20 23:15:20 +0530

    feat: add a DMCA report modal

--- ui/constants/modal_types.js
+++ ui/constants/modal_types.js
@@ -38,6 +38,7 @@ export const YOUTUBE_WELCOME = 'youtube_welcome';
 export const MOBILE_NAVIGATION = 'mobile_navigation';
 export const SET_REFERRER = 'set_referrer';
 export const REPORT = 'report';
+export const DMCA = 'dmca';
 export const REPOST = 'repost';
 export const SIGN_OUT = 'sign_out';
 export const LIQUIDATE_SUPPORTS = 'liquidate_supports';
