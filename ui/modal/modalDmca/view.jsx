be1db195bdd118789f7e87ebc00e158118606d49
eniamza0 <eniamza0@gmail.com>
2020-05-21 20:46:40 +0600

    Added necessary fields for dmca modal

--- ui/modal/modalDmca/view.jsx
+++ ui/modal/modalDmca/view.jsx
@@ -28,7 +28,10 @@ const ModalDmca = (props: Props) => {
       <hr />
       <br />
       <React.Fragment>
-        <FormField />
+        <FormField type="text" name="Name of the Original Author" label={__('Name of the Original Author')} />
+        <FormField type="text" name="Link to the original content" label={__('Link to the original Content')} />
+        <FormField type="text" name="Your Email" label={__('Your Email')} />
+        <FormField type="text" name="Anything more we should know?" label={__('Anything more we should know?')} />
       </React.Fragment>
       <div className="card__actions">
         <Button button="primary" onClick={confirmReport} label={__('Report')} />
