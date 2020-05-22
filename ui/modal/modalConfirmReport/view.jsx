59877b33069962d2aa5861f284a7a7a85484fc3d
Harsh Khandeparkar <mjkgoa@gmail.com>
2020-05-22 13:07:15 +0530

    feat: show refund wallet address in the confirmation modal

--- ui/modal/modalConfirmReport/view.jsx
+++ ui/modal/modalConfirmReport/view.jsx
@@ -7,10 +7,13 @@ import * as MODALS from 'constants/modal_types';
 type Props = {
   closeModal: () => void,
   openModal: (id: string, { amount: number, address: string }) => void,
+  reportFields: {
+    walletAddress: string,
+  },
 };
 
 const ModalConfirmReport = (props: Props) => {
-  const { closeModal, openModal } = props;
+  const { closeModal, openModal, reportFields } = props;
 
   const confirmReport = e => {
     e.preventDefault();
@@ -20,10 +23,12 @@ const ModalConfirmReport = (props: Props) => {
   return (
     <Modal type="custom" isOpen contentLabel="Confirm Report" title={__('Confirm Report')}>
       <p>
-        <b>DISCLAIMER</b>: YOU WILL HAVE TO SEND LBRY INC. A CERTAIN AMOUNT OF LBC AS A DEPOSIT AND WILL BE RETURNED TO
+        <b>DISCLAIMER</b>: YOU WILL HAVE TO SEND LBRY INC. A CERTAIN AMOUNT OF LBC AS A DEPOSIT AND WILL BE REFUNDED TO
         YOU IF AND ONLY IF THE REPORT IS VALID. WE USE THIS MEASURE TO PREVENT BOTS AND PEOPLE FROM ABUSING REPORT
         FORMS.
       </p>
+      <p>The refund will be received on the following wallet address:</p>
+      <blockquote>{reportFields.walletAddress}</blockquote>
 
       <div className="card__actions">
         <Button button="primary" onClick={confirmReport} label={__('Confirm')} />
