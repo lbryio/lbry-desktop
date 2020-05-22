72b6a66ef813934e1f8e5550978931e96522b331
Harsh Khandeparkar <mjkgoa@gmail.com>
2020-05-22 12:08:45 +0530

    feat: link confirmation modal in report modal

--- ui/modal/modalReport/index.js
+++ ui/modal/modalReport/index.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
-import { doHideModal } from 'redux/actions/app';
+import { doHideModal, doOpenModal } from 'redux/actions/app';
 import ModalReport from './view';
 
 const perform = dispatch => () => ({
   closeModal: () => {
     dispatch(doHideModal());
   },
+  openModal: (modal, props) => dispatch(doOpenModal(modal, props)),
 });
 
 export default connect(null, perform)(ModalReport);
