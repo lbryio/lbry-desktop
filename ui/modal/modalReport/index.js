de58f1665686304e4e24a4b12cd6f6907dc8fd6e
Harsh Khandeparkar <mjkgoa@gmail.com>
2020-05-22 13:06:28 +0530

    feat: send wallet address along with the report fields

--- ui/modal/modalReport/index.js
+++ ui/modal/modalReport/index.js
@@ -1,12 +1,20 @@
 import { connect } from 'react-redux';
 import { doHideModal, doOpenModal } from 'redux/actions/app';
+import { doCheckAddressIsMine, doGetNewAddress, selectReceiveAddress, selectGettingNewAddress } from 'lbry-redux';
 import ModalReport from './view';
+
+const select = state => ({
+  receiveAddress: selectReceiveAddress(state),
+  gettingNewAddress: selectGettingNewAddress(state),
+});
 
 const perform = dispatch => () => ({
   closeModal: () => {
     dispatch(doHideModal());
   },
   openModal: (modal, props) => dispatch(doOpenModal(modal, props)),
+  checkAddressIsMine: address => dispatch(doCheckAddressIsMine(address)),
+  getNewAddress: () => dispatch(doGetNewAddress()),
 });
 
-export default connect(null, perform)(ModalReport);
+export default connect(select, perform)(ModalReport);
