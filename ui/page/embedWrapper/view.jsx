ee15c5b8fb34533c2a7aebdccb26b18bbb234e3d
jessop <jessop@lbry.com>
2020-05-21 21:23:40 -0400

    prevent paid content in embeds

--- ui/page/embedWrapper/view.jsx
+++ ui/page/embedWrapper/view.jsx
@@ -7,29 +7,40 @@ type Props = {
   resolveUri: string => void,
   claim: Claim,
   doPlayUri: string => void,
+  costInfo: any,
+  streamingUrl: string,
+  doFetchCostInfoForUri: string => void,
+  isResolvingUri: boolean,
 };
 // $FlowFixMe apparently flow thinks this is wrong.
 export const EmbedContext = React.createContext();
 const EmbedWrapperPage = (props: Props) => {
-  const { resolveUri, claim, uri, doPlayUri } = props;
+  const { resolveUri, claim, uri, doPlayUri, costInfo, streamingUrl, doFetchCostInfoForUri, isResolvingUri } = props;
   const haveClaim = Boolean(claim);
 
   useEffect(() => {
     if (resolveUri && uri && !haveClaim) {
       resolveUri(uri);
     }
-    if (uri && haveClaim) {
+    if (uri && haveClaim && costInfo && costInfo.cost === 0) {
       doPlayUri(uri);
     }
-  }, [resolveUri, uri, doPlayUri, haveClaim]);
+  }, [resolveUri, uri, doPlayUri, haveClaim, costInfo]);
+
+  useEffect(() => {
+    if (haveClaim && uri && doFetchCostInfoForUri) {
+      doFetchCostInfoForUri(uri);
+    }
+  }, [uri, haveClaim, doFetchCostInfoForUri]);
 
   return (
     <div className={'embed__wrapper'}>
-      {claim && (
-        <EmbedContext.Provider value>
-          <FileRender uri={uri} embedded />
-        </EmbedContext.Provider>
-      )}
+      <EmbedContext.Provider value>
+        {claim && streamingUrl && <FileRender uri={uri} embedded />}
+        {!claim && isResolvingUri && <h1>Loading...</h1>}
+        {!claim && !isResolvingUri && <h1>No content at this link.</h1>}
+        {claim && costInfo && costInfo.cost > 0 && <h1>Paid content cannot be embedded.</h1>}
+      </EmbedContext.Provider>
     </div>
   );
 };
