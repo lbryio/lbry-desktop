1de689639336d93269cd89d6601cc3098b082019
Harsh Khandeparkar <mjkgoa@gmail.com>
2020-05-23 23:31:26 +0530

    fix<modalDmca>: deposit wallet address is shown

--- ui/modal/modalDmca/index.js
+++ ui/modal/modalDmca/index.js
@@ -1,12 +1,20 @@
 import { connect } from 'react-redux';
 import { doHideModal, doOpenModal } from 'redux/actions/app';
+import { doCheckAddressIsMine, doGetNewAddress, selectReceiveAddress, selectGettingNewAddress } from 'lbry-redux';
 import ModalDmca from './view';
+
+const select = state => ({
+  receiveAddress: selectReceiveAddress(state),
+  gettingNewAddress: selectGettingNewAddress(state),
+});
 
 const perform = dispatch => () => ({
   closeModal: () => {
     dispatch(doHideModal());
   },
   openModal: (modal, props) => dispatch(doOpenModal(modal, props)),
+  checkAddressIsMine: address => dispatch(doCheckAddressIsMine(address)),
+  getNewAddress: () => dispatch(doGetNewAddress()),
 });
 
-export default connect(null, perform)(ModalDmca);
+export default connect(select, perform)(ModalDmca);
