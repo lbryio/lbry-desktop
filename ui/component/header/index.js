a7812350ce188f98bef42209e9484e75532c7e97
Clayton Hickey <clayton.hickey@yahoo.com>
2020-05-26 14:39:40 -0400

    remove manual override in formatting for credits in header

--- ui/component/header/index.js
+++ ui/component/header/index.js
@@ -18,7 +18,7 @@ import Header from './view';
 const select = state => ({
   balance: selectBalance(state),
   language: makeSelectClientSetting(SETTINGS.LANGUAGE)(state), // trigger redraw on language change
-  roundedBalance: formatCredits(selectBalance(state) || 0, 2, true),
+  roundedBalance: formatCredits(selectBalance(state), 2, true),
   currentTheme: makeSelectClientSetting(SETTINGS.THEME)(state),
   automaticDarkModeEnabled: makeSelectClientSetting(SETTINGS.AUTOMATIC_DARK_MODE_ENABLED)(state),
   hideBalance: makeSelectClientSetting(SETTINGS.HIDE_BALANCE)(state),
