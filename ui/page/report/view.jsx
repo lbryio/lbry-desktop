ca988d38aeb19802dff34280367354f03c997aea
Sean Yesmunt <sean@lbry.io>
2020-06-15 14:01:48 -0400

    fix layout on '/$/report'

--- ui/page/report/view.jsx
+++ ui/page/report/view.jsx
@@ -48,80 +48,84 @@ class ReportPage extends React.Component {
   render() {
     return (
       <Page>
-        <Card
-          title={__('Report an Issue/Request a Feature')}
-          subtitle={__(
-            'Please describe the problem you experienced or the feature you want to see and any information you think might be useful to us. Links to screenshots are great!'
-          )}
-          actions={
-            <Fragment>
-              <FormField
-                type="textarea"
-                rows="10"
-                name="message"
-                stretch
-                value={this.state.message}
-                onChange={event => {
-                  this.onMessageChange(event);
-                }}
-                placeholder={__('Description of your issue or feature request')}
-              />
+        <div className="card-stack">
+          <Card
+            title={__('Report an Issue/Request a Feature')}
+            subtitle={__(
+              'Please describe the problem you experienced or the feature you want to see and any information you think might be useful to us. Links to screenshots are great!'
+            )}
+            actions={
+              <Fragment>
+                <FormField
+                  type="textarea"
+                  rows="10"
+                  name="message"
+                  stretch
+                  value={this.state.message}
+                  onChange={event => {
+                    this.onMessageChange(event);
+                  }}
+                  placeholder={__('Description of your issue or feature request')}
+                />
 
-              <Button
-                button="primary"
-                onClick={event => {
-                  this.submitMessage(event);
-                }}
-                className={`button-block button-primary ${this.state.submitting ? 'disabled' : ''}`}
-              >
-                {this.state.submitting ? __('Submitting...') : __('Submit Report')}
-              </Button>
-            </Fragment>
-          }
-        />
+                <div className="section__actions">
+                  <Button
+                    button="primary"
+                    onClick={event => {
+                      this.submitMessage(event);
+                    }}
+                    className={`button-block button-primary ${this.state.submitting ? 'disabled' : ''}`}
+                  >
+                    {this.state.submitting ? __('Submitting...') : __('Submit Report')}
+                  </Button>
+                </div>
+              </Fragment>
+            }
+          />
 
-        <Card
-          title={__('Developer?')}
-          actions={
-            <Fragment>
-              <div className="markdown-preview">
-                <p>{__('You can also:')}</p>
-                <ul>
-                  <li>
-                    <Button
-                      button="link"
-                      href="https://github.com/lbryio/lbry-desktop/issues"
-                      label={__('Submit an issue on GitHub')}
-                    />
-                    .
-                  </li>
-                  <li>
-                    <I18nMessage
-                      tokens={{
-                        technical_resources: (
-                          <Button button="link" href="https://lbry.tech" label={__('technical resources')} />
-                        ),
-                      }}
-                    >
-                      Explore our %technical_resources%
-                    </I18nMessage>
-                    .
-                  </li>
-                  <li>
-                    <I18nMessage
-                      tokens={{
-                        tech_forum: <Button button="link" href="https://forum.lbry.tech" label={__('tech forum')} />,
-                      }}
-                    >
-                      Join our %tech_forum%
-                    </I18nMessage>
-                    .
-                  </li>
-                </ul>
-              </div>
-            </Fragment>
-          }
-        />
+          <Card
+            title={__('Developer?')}
+            actions={
+              <Fragment>
+                <div className="markdown-preview">
+                  <p>{__('You can also:')}</p>
+                  <ul>
+                    <li>
+                      <Button
+                        button="link"
+                        href="https://github.com/lbryio/lbry-desktop/issues"
+                        label={__('Submit an issue on GitHub')}
+                      />
+                      .
+                    </li>
+                    <li>
+                      <I18nMessage
+                        tokens={{
+                          technical_resources: (
+                            <Button button="link" href="https://lbry.tech" label={__('technical resources')} />
+                          ),
+                        }}
+                      >
+                        Explore our %technical_resources%
+                      </I18nMessage>
+                      .
+                    </li>
+                    <li>
+                      <I18nMessage
+                        tokens={{
+                          tech_forum: <Button button="link" href="https://forum.lbry.tech" label={__('tech forum')} />,
+                        }}
+                      >
+                        Join our %tech_forum%
+                      </I18nMessage>
+                      .
+                    </li>
+                  </ul>
+                </div>
+              </Fragment>
+            }
+          />
+        </div>
       </Page>
     );
   }
