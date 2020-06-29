f2addc73e0bd1de88f3b3ae0e6e2b72d18d8efe9
infiinte-persistence <inf.persistence@gmail.com>
2020-06-30 02:26:08 +0800

    AbandonedChannelPreview: Handle blocked channels as well.
    Issue 3800

--- ui/component/abandonedChannelPreview/view.jsx
+++ ui/component/abandonedChannelPreview/view.jsx
@@ -5,6 +5,7 @@ import ChannelThumbnail from 'component/channelThumbnail';
 import Button from 'component/button';
 import { parseURI } from 'lbry-redux';
 import * as ICONS from '../../constants/icons';
+import * as MODALS from 'constants/modal_types';
 
 type SubscriptionArgs = {
   channelName: string,
@@ -15,11 +16,14 @@ type Props = {
   uri: string,
   doChannelUnsubscribe: SubscriptionArgs => void,
   type: string,
+  blockedChannelUris: Array<string>,
+  doOpenModal: (string, {}) => void,
 };
 
 function AbandonedChannelPreview(props: Props) {
-  const { uri, doChannelUnsubscribe, type } = props;
+  const { uri, doChannelUnsubscribe, type, blockedChannelUris, doOpenModal } = props;
   const { channelName } = parseURI(uri);
+  const isBlockedChannel = blockedChannelUris.includes(uri);
 
   return (
     <li className={classnames('claim-preview__wrapper', 'claim-preview__wrapper--notice')}>
@@ -33,20 +37,31 @@ function AbandonedChannelPreview(props: Props) {
             <div className="media__subtitle">{__(`This channel may have been unpublished.`)}</div>
           </div>
           <div className="claim-preview__actions">
+            {isBlockedChannel && (
+              <Button
+                iconColor="red"
+                icon={ICONS.UNBLOCK}
+                button={'alt'}
+                label={__('Unblock')}
+                onClick={() => doOpenModal(MODALS.REMOVE_BLOCKED, { blockedUri: uri })}
+              />
+            )}
             {/* SubscribeButton uses resolved permanentUri; modifying it didn't seem worth it. */}
-            <Button
-              iconColor="red"
-              icon={ICONS.UNSUBSCRIBE}
-              button={'alt'}
-              label={__('Unfollow')}
-              onClick={e => {
-                e.stopPropagation();
-                doChannelUnsubscribe({
-                  channelName: `@${channelName}`,
-                  uri,
-                });
-              }}
-            />
+            {!isBlockedChannel && (
+              <Button
+                iconColor="red"
+                icon={ICONS.UNSUBSCRIBE}
+                button={'alt'}
+                label={__('Unfollow')}
+                onClick={e => {
+                  e.stopPropagation();
+                  doChannelUnsubscribe({
+                    channelName: `@${channelName}`,
+                    uri,
+                  });
+                }}
+              />
+            )}
           </div>
         </div>
       </div>
