023e4c6ad84e381489e14dc00f3986ee7c2336d3
infiinte-persistence <inf.persistence@gmail.com>
2020-07-03 11:59:24 +0800

    Add password reset link to settings page (4468)

--- ui/component/settingAccountPassword/view.jsx
+++ ui/component/settingAccountPassword/view.jsx
@@ -4,6 +4,7 @@ import { FormField, Form } from 'component/common/form';
 import Button from 'component/button';
 import ErrorText from 'component/common/error-text';
 import Card from 'component/common/card';
+import UserPasswordReset from 'component/userPasswordReset';
 
 type Props = {
   user: ?User,
@@ -19,6 +20,7 @@ export default function SettingAccountPassword(props: Props) {
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [isAddingPassword, setIsAddingPassword] = useState(false);
+  const [forgotPassword, setForgotPassword] = useState(false);
   const hasPassword = user && user.password_set;
 
   function handleSubmit() {
@@ -44,34 +46,43 @@ export default function SettingAccountPassword(props: Props) {
       actions={
         isAddingPassword ? (
           <div>
-            <Form onSubmit={handleSubmit} className="section">
-              {hasPassword && (
-                <FormField
-                  type="password"
-                  name="setting_set_old_password"
-                  label={__('Old Password')}
-                  value={oldPassword}
-                  onChange={e => setOldPassword(e.target.value)}
-                />
-              )}
-              <FormField
-                type="password"
-                name="setting_set_new_password"
-                label={__('New Password')}
-                value={newPassword}
-                onChange={e => setNewPassword(e.target.value)}
-              />
-
+            {forgotPassword ? (
               <div className="section__actions">
-                <Button button="primary" type="submit" label={__('Set Password')} disabled={!newPassword} />
-                {!hasPassword && (
-                  <Button button="link" label={__('Cancel')} onClick={() => setIsAddingPassword(false)} />
-                )}
+                <UserPasswordReset onCancel={() => setForgotPassword(false)} />
               </div>
-            </Form>
-            {passwordSetError && (
-              <div className="section">
-                <ErrorText>{passwordSetError}</ErrorText>
+            ) : (
+              <div>
+                <Form onSubmit={handleSubmit} className="section">
+                  {hasPassword && (
+                    <FormField
+                      type="password"
+                      name="setting_set_old_password"
+                      label={__('Old Password')}
+                      value={oldPassword}
+                      onChange={e => setOldPassword(e.target.value)}
+                    />
+                  )}
+                  <FormField
+                    type="password"
+                    name="setting_set_new_password"
+                    label={__('New Password')}
+                    value={newPassword}
+                    onChange={e => setNewPassword(e.target.value)}
+                  />
+                  <div className="section__actions">
+                    <Button button="primary" type="submit" label={__('Set Password')} disabled={!newPassword} />
+                    {hasPassword ? (
+                      <Button button="link" label={__('Forgot Password?')} onClick={() => setForgotPassword(true)} />
+                    ) : (
+                      <Button button="link" label={__('Cancel')} onClick={() => setIsAddingPassword(false)} />
+                    )}
+                  </div>
+                </Form>
+                {passwordSetError && (
+                  <div className="section">
+                    <ErrorText>{passwordSetError}</ErrorText>
+                  </div>
+                )}
               </div>
             )}
           </div>
