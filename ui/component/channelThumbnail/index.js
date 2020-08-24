c136fd3c32fad0dab5487a22480efb418d14231e
Sean Yesmunt <sean@lbry.io>
2020-08-24 14:44:41 -0400

    comment cleanup

--- ui/component/channelThumbnail/index.js
+++ ui/component/channelThumbnail/index.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
-import { makeSelectThumbnailForUri, doResolveUri, makeSelectClaimForUri } from 'lbry-redux';
+import { makeSelectThumbnailForUri, doResolveUri, makeSelectClaimForUri, makeSelectIsUriResolving } from 'lbry-redux';
 import ChannelThumbnail from './view';
 
 const select = (state, props) => ({
   thumbnail: makeSelectThumbnailForUri(props.uri)(state),
   claim: makeSelectClaimForUri(props.uri)(state),
+  isResolving: makeSelectIsUriResolving(props.uri)(state),
 });
 
 export default connect(select, {
