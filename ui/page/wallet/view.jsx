5be26328bddeb8010fc1f5887728c78bf6ca88fc
Sean Yesmunt <sean@lbry.io>
2020-08-26 13:19:03 -0400

    create YrblWalletEmpty for generic yrbl with empty wallet message

--- ui/page/wallet/view.jsx
+++ ui/page/wallet/view.jsx
@@ -1,25 +1,20 @@
 // @flow
-import * as ICONS from 'constants/icons';
-import * as PAGES from 'constants/pages';
-import * as MODALS from 'constants/modal_types';
 import React from 'react';
 import { withRouter } from 'react-router';
 import WalletBalance from 'component/walletBalance';
 import TxoList from 'component/txoList';
 import Page from 'component/page';
-import Yrbl from 'component/yrbl';
-import Button from 'component/button';
 import Spinner from 'component/spinner';
+import YrblWalletEmpty from 'component/yrblWalletEmpty';
 
 type Props = {
   history: { action: string, push: string => void, replace: string => void },
   location: { search: string, pathname: string },
   balance: number,
-  doOpenModal: string => void,
 };
 
 const WalletPage = (props: Props) => {
-  const { location, balance, doOpenModal } = props;
+  const { location, balance } = props;
   const { search } = location;
   const showIntro = balance === 0;
   const loading = balance === undefined;
@@ -34,42 +29,7 @@ const WalletPage = (props: Props) => {
       {!loading && (
         <>
           {showIntro ? (
-            <div className="main--empty">
-              <Yrbl
-                type="sad"
-                title={__('Your wallet is empty')}
-                subtitle={
-                  <div>
-                    <p>{__('You need LBC to create a channel and upload content.')}</p>
-                    <p>
-                      {__(
-                        'Never fear though, there are tons of ways to earn LBC! You can earn or purchase LBC, or you can have your friends send you some.'
-                      )}
-                    </p>
-                    <div className="section__actions">
-                      <Button
-                        button="primary"
-                        icon={ICONS.REWARDS}
-                        label={__('Earn Rewards')}
-                        navigate={`/$/${PAGES.REWARDS}`}
-                      />
-                      <Button
-                        button="secondary"
-                        icon={ICONS.BUY}
-                        label={__('Buy Credits')}
-                        navigate={`/$/${PAGES.BUY}`}
-                      />
-                      <Button
-                        icon={ICONS.RECEIVE}
-                        button="secondary"
-                        label={__('Your Address')}
-                        onClick={() => doOpenModal(MODALS.WALLET_RECEIVE)}
-                      />
-                    </div>
-                  </div>
-                }
-              />
-            </div>
+            <YrblWalletEmpty includeWalletLink />
           ) : (
             <>
               <WalletBalance />
