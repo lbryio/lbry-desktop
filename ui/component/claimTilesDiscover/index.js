dd5636c2b383d14428e3883f9f6c59be3245b86b
Sean Yesmunt <sean@lbry.io>
2020-09-11 15:48:23 -0400

    prevent multiple claim_search's happening on the homepage

--- ui/component/claimTilesDiscover/index.js
+++ ui/component/claimTilesDiscover/index.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { doClaimSearch, selectClaimSearchByQuery, selectFetchingClaimSearch, SETTINGS } from 'lbry-redux';
+import { doClaimSearch, selectClaimSearchByQuery, selectFetchingClaimSearchByQuery, SETTINGS } from 'lbry-redux';
 import { selectBlockedChannels } from 'redux/selectors/blocked';
 import { doToggleTagFollowDesktop } from 'redux/actions/tags';
 import { makeSelectClientSetting } from 'redux/selectors/settings';
@@ -7,7 +7,7 @@ import ClaimListDiscover from './view';
 
 const select = state => ({
   claimSearchByQuery: selectClaimSearchByQuery(state),
-  loading: selectFetchingClaimSearch(state),
+  fetchingClaimSearchByQuery: selectFetchingClaimSearchByQuery(state),
   showNsfw: makeSelectClientSetting(SETTINGS.SHOW_MATURE)(state),
   hideReposts: makeSelectClientSetting(SETTINGS.HIDE_REPOSTS)(state),
   hiddenUris: selectBlockedChannels(state),
