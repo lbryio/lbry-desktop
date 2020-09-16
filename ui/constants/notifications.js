fba17fbe4d25758682c234b583f313a43e3abc05
Sean Yesmunt <sean@lbry.io>
2020-09-16 12:35:59 -0400

    fix target for notification for daily_watch_remind type

--- ui/constants/notifications.js
+++ ui/constants/notifications.js
@@ -2,3 +2,4 @@ export const NOTIFICATION_CREATOR_SUBSCRIBER = 'creator_subscriber';
 export const NOTIFICATION_COMMENT = 'comment';
 export const NOTIFICATION_REPLY = 'comment-reply';
 export const DAILY_WATCH_AVAILABLE = 'daily_watch_available';
+export const DAILY_WATCH_REMIND = 'daily_watch_remind';
