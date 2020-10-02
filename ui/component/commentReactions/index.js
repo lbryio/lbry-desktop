3f70537dc42ca5758337ab74050e216f423a7cfe
jessop <jessop@lbry.com>
2020-10-01 20:25:30 -0400

    debounce comment reacts on backend, not button, feedback immediate

--- ui/component/commentReactions/index.js
+++ ui/component/commentReactions/index.js
@@ -1,16 +1,11 @@
 import { connect } from 'react-redux';
 import Comment from './view';
-import {
-  makeSelectMyReactionsForComment,
-  makeSelectOthersReactionsForComment,
-  selectPendingCommentReacts,
-} from 'redux/selectors/comments';
+import { makeSelectMyReactionsForComment, makeSelectOthersReactionsForComment } from 'redux/selectors/comments';
 import { doCommentReact } from 'redux/actions/comments';
 
 const select = (state, props) => ({
   myReacts: makeSelectMyReactionsForComment(props.commentId)(state),
   othersReacts: makeSelectOthersReactionsForComment(props.commentId)(state),
-  pendingCommentReacts: selectPendingCommentReacts(state),
 });
 
 const perform = dispatch => ({
