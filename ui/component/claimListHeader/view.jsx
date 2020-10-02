56cd1ddfa79bac0116cbd255a415ec17b2feced2
Sean Yesmunt <sean@lbry.io>
2020-10-02 13:30:39 -0400

    bring in ClaimListDiscover changes from odysee

--- ui/component/claimListHeader/view.jsx
+++ ui/component/claimListHeader/view.jsx
@@ -31,6 +31,7 @@ type Props = {
   tileLayout: boolean,
   doSetClientSetting: (string, boolean, ?boolean) => void,
   setPage: number => void,
+  hideFilters: boolean,
 };
 
 function ClaimListHeader(props: Props) {
@@ -53,6 +54,7 @@ function ClaimListHeader(props: Props) {
     tileLayout,
     doSetClientSetting,
     setPage,
+    hideFilters,
   } = props;
   const { action, push, location } = useHistory();
   const { search } = location;
@@ -203,24 +205,25 @@ function ClaimListHeader(props: Props) {
       <div className="claim-search__wrapper">
         <div className="claim-search__top">
           <div className="claim-search__top-row">
-            {CS.ORDER_BY_TYPES.map(type => (
-              <Button
-                key={type}
-                button="alt"
-                onClick={e =>
-                  handleChange({
-                    key: CS.ORDER_BY_KEY,
-                    value: type,
-                  })
-                }
-                className={classnames(`button-toggle button-toggle--${type}`, {
-                  'button-toggle--active': orderParam === type,
-                })}
-                disabled={orderBy}
-                icon={toCapitalCase(type)}
-                label={__(toCapitalCase(type))}
-              />
-            ))}
+            {!hideFilters &&
+              CS.ORDER_BY_TYPES.map(type => (
+                <Button
+                  key={type}
+                  button="alt"
+                  onClick={e =>
+                    handleChange({
+                      key: CS.ORDER_BY_KEY,
+                      value: type,
+                    })
+                  }
+                  className={classnames(`button-toggle button-toggle--${type}`, {
+                    'button-toggle--active': orderParam === type,
+                  })}
+                  disabled={orderBy}
+                  icon={toCapitalCase(type)}
+                  label={__(toCapitalCase(type))}
+                />
+              ))}
           </div>
 
           <div>
