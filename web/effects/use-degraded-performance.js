787ed8290a1c9818db3ecb13ab14cd771edd71cf
jessop <jessop@lbry.com>
2020-10-27 21:29:45 -0400

    send auth token with status call

--- web/effects/use-degraded-performance.js
+++ web/effects/use-degraded-performance.js
@@ -1,5 +1,7 @@
 import { SDK_API_PATH } from 'ui';
 import { useEffect } from 'react';
+import { getAuthToken } from 'util/saved-passwords';
+import { X_LBRY_AUTH_TOKEN } from 'constants/token';
 
 import fetchWithTimeout from 'util/fetch';
 
@@ -9,13 +11,23 @@ export const STATUS_DEGRADED = 'degraded';
 export const STATUS_FAILING = 'failing';
 export const STATUS_DOWN = 'down';
 
+const getParams = () => {
+  const headers = {};
+  const token = getAuthToken();
+  if (token) {
+    headers[X_LBRY_AUTH_TOKEN] = token;
+  }
+  const params = { headers };
+  return params;
+};
+
 export function useDegradedPerformance(onDegradedPerformanceCallback) {
   useEffect(() => {
     // The status endpoint is the only endpoint at "v2" currently
     // This should be moved into the config once more endpoints are using it
     const STATUS_ENDPOINT = `${SDK_API_PATH}/status`.replace('v1', 'v2');
 
-    fetchWithTimeout(STATUS_TIMEOUT_LIMIT, fetch(STATUS_ENDPOINT))
+    fetchWithTimeout(STATUS_TIMEOUT_LIMIT, fetch(STATUS_ENDPOINT, getParams()))
       .then(response => response.json())
       .then(status => {
         if (status.general_state !== STATUS_OK) {
