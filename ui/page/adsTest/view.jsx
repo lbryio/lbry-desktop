561237bcaddd86e3078d34c2a48cecfcc6af5e68
Sean Yesmunt <sean@lbry.io>
2020-11-02 12:00:49 -0500

    add ads script to head for test

--- ui/page/adsTest/view.jsx
+++ ui/page/adsTest/view.jsx
@@ -1,14 +1,28 @@
 // @flow
 import * as React from 'react';
 import Page from 'component/page';
-import VideoJs from 'component/viewers/videoViewer/internal/videojs';
+// import VideoJs from 'component/viewers/videoViewer/internal/videojs';
 
-export default function CheckoutPage() {
+export default function AdsTestPage() {
+  const [show, setShow] = React.useState(false);
+  React.useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://sdk.adspruce.com/1/adsprucetag.js?pid=8394&sid=2"';
+    script.defer = true;
+
+    document.head.appendChild(script);
+  }, []);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 3000);
+  }, []);
+
   return (
     <Page className="ads-test">
       <h1>ads test</h1>
-      {/* $FlowFixMe */}
-      <VideoJs adsTest />
+      {/* {show && <VideoJs adsTest />} */}
     </Page>
   );
 }
