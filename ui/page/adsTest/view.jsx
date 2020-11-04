000f073efd507d70e79ae90828288da2d5e6df8a
Sean Yesmunt <sean@lbry.io>
2020-11-04 11:58:23 -0500

    add back video player on adstest pagegaa

--- ui/page/adsTest/view.jsx
+++ ui/page/adsTest/view.jsx
@@ -1,7 +1,7 @@
 // @flow
 import * as React from 'react';
 import Page from 'component/page';
-// import VideoJs from 'component/viewers/videoViewer/internal/videojs';
+import VideoJs from 'component/viewers/videoViewer/internal/videojs';
 
 export default function AdsTestPage() {
   const [show, setShow] = React.useState(false);
@@ -10,19 +10,25 @@ export default function AdsTestPage() {
     script.src = 'https://sdk.adspruce.com/1/adsprucetag.js?pid=8394&sid=2"';
     script.defer = true;
 
+    // $FlowFixMe
     document.head.appendChild(script);
   }, []);
 
   React.useEffect(() => {
     setTimeout(() => {
       setShow(true);
-    }, 3000);
+    }, 1000);
   }, []);
 
   return (
     <Page className="ads-test">
       <h1>ads test</h1>
-      {/* {show && <VideoJs adsTest />} */}
+      {show && (
+        <div style={{ marginTop: '5rem' }}>
+          {/* $FlowFixMe */}
+          <VideoJs adsTest />
+        </div>
+      )}
     </Page>
   );
 }
