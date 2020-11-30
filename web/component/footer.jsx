36f1d3c6f2e86dc6e75ee2810e9134c0546d0c89
Sean Yesmunt <sean@lbry.io>
2020-11-30 14:45:23 -0500

    add community guidlines link at bottom

--- web/component/footer.jsx
+++ web/component/footer.jsx
@@ -4,98 +4,6 @@ import React from 'react';
 import Button from 'component/button';
 import I18nMessage from 'component/i18nMessage';
 
-// const sections = [
-//   {
-//     name: 'Community',
-//     links: [
-//       {
-//         label: 'Twitter',
-//         link: 'https://twitter.com/lbrycom',
-//       },
-//       {
-//         label: 'Reddit',
-//         link: 'https://reddit.com/r/lbry',
-//       },
-//       {
-//         label: 'Chat (Discord)',
-//         link: 'https://chat.lbry.org/',
-//       },
-//       {
-//         label: 'Telegram',
-//         link: 'https://t.me/lbryofficial',
-//       },
-//       {
-//         label: 'Facebook',
-//         link: 'https://www.facebook.com/lbryio',
-//       },
-//     ],
-//   },
-//   {
-//     name: 'Resources',
-//     links: [
-//       {
-//         label: 'FAQ',
-//         link: 'https://lbry.com/faq',
-//       },
-//       {
-//         label: 'Support',
-//         link: 'https://lbry.com/faq/support',
-//       },
-//       {
-//         label: 'YouTube Partner Program',
-//         link: 'https://lbry.com/youtube',
-//       },
-//       {
-//         label: 'lbry.com',
-//         link: 'https://lbry.com',
-//       },
-//       {
-//         label: 'lbry.tech',
-//         link: 'https://lbry.tech',
-//       },
-//       {
-//         label: 'GitHub',
-//         link: 'https://github.com/lbryio',
-//       },
-//     ],
-//   },
-//   {
-//     name: 'Policies',
-//     links: [
-//       {
-//         label: 'Terms of Service',
-//         link: 'https://www.lbry.com/termsofservice',
-//       },
-//       {
-//         label: 'Privacy Policy',
-//         link: 'https://lbry.com/privacypolicy',
-//       },
-//       {
-//         label: '2257',
-//         navigate: `/$/${PAGES.CODE_2257}`,
-//       },
-//     ],
-//   },
-// ];
-
-// Disabled for experiment
-// {sections.map(({ name, links }) => {
-//   return (
-// 	<div key={name} className="footer__section">
-// 	  <div className="footer__section-title">{name}</div>
-// 	  <ul className="ul--no-style">
-// 		{/* $FlowFixMe */}
-// 		{links.map(({ label, link, navigate }) => {
-// 		  return (
-// 			<li key={label}>
-// 			  <Button className="footer__link" href={link} navigate={navigate} label={label} />
-// 			</li>
-// 		  );
-// 		})}
-// 	  </ul>
-// 	</div>
-//   );
-// })}
 export default function Footer() {
   return (
     <footer className="footer">
@@ -107,6 +15,9 @@ export default function Footer() {
       <ul className="navigation__tertiary footer__links">
         <li className="footer__link">
           <Button label={__('About --[link title in Sidebar or Footer]--')} href="https://lbry.com/about" />
+        </li>
+        <li className="footer__link">
+          <Button label={__('Community Guidlines')} href="https://odysee.com/@OdyseeHelp:b/Community-Guidelines:c" />
         </li>
         <li className="footer__link">
           <Button label={__('FAQ')} href="https://odysee.com/@OdyseeHelp:b" />
