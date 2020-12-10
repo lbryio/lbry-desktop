996a5c40b1b61cb9ba7d943aa9d0e62cbc1a9f95
Sean Yesmunt <sean@lbry.io>
2020-12-10 16:39:12 -0500

    use new cdn for images default to using one in the env

--- ui/component/fileThumbnail/view.jsx
+++ ui/component/fileThumbnail/view.jsx
@@ -42,8 +42,12 @@ function FileThumbnail(props: Props) {
   // @if TARGET='web'
   // Pass image urls through a compression proxy
   if (thumbnail && THUMBNAIL_CDN_URL && !thumbnail.includes('https://spee.ch')) {
-    url = `${THUMBNAIL_CDN_URL}${thumbnail}`;
+    url = `${THUMBNAIL_CDN_URL}${thumbnail}&quality=75&height=288&width=512`;
   }
+
+  //   else if (thumbnail && thumbnail.includes('https://spee.ch')) {
+  //     url = `${url}?height=512&width=288`;
+  //   }
   // @endif
 
   return (
