dd5e09237a413af6a34a56b52a1d9598b040471b
Sean Yesmunt <sean@lbry.io>
2020-12-10 17:31:20 -0500

    pass x-lbry-auth-token to apis status call for email verified users

--- web/effects/use-degraded-performance.js
+++ web/effects/use-degraded-performance.js
@@ -1,7 +1,7 @@
 import { SDK_API_PATH } from 'ui';
 import { useEffect } from 'react';
-// import { getAuthToken } from 'util/saved-passwords';
-// import { X_LBRY_AUTH_TOKEN } from 'constants/token';
+import { getAuthToken } from 'util/saved-passwords';
+import { X_LBRY_AUTH_TOKEN } from 'constants/token';
 
 import fetchWithTimeout from 'util/fetch';
 
@@ -11,31 +11,35 @@ export const STATUS_DEGRADED = 'degraded';
 export const STATUS_FAILING = 'failing';
 export const STATUS_DOWN = 'down';
 
-const getParams = () => {
+const getParams = user => {
   const headers = {};
-  // const token = getAuthToken();
-  // if (token) {
-  //  headers[X_LBRY_AUTH_TOKEN] = token;
-  // }
+  const token = getAuthToken();
+  if (token && user.has_verified_email) {
+    headers[X_LBRY_AUTH_TOKEN] = token;
+  }
   const params = { headers };
   return params;
 };
 
-export function useDegradedPerformance(onDegradedPerformanceCallback) {
-  useEffect(() => {
-    // The status endpoint is the only endpoint at "v2" currently
-    // This should be moved into the config once more endpoints are using it
-    const STATUS_ENDPOINT = `${SDK_API_PATH}/status`.replace('v1', 'v2');
+export function useDegradedPerformance(onDegradedPerformanceCallback, user) {
+  const hasUser = user !== undefined;
 
-    fetchWithTimeout(STATUS_TIMEOUT_LIMIT, fetch(STATUS_ENDPOINT, getParams()))
-      .then(response => response.json())
-      .then(status => {
-        if (status.general_state !== STATUS_OK) {
+  useEffect(() => {
+    if (hasUser) {
+      // The status endpoint is the only endpoint at "v2" currently
+      // This should be moved into the config once more endpoints are using it
+      const STATUS_ENDPOINT = `${SDK_API_PATH}/status`.replace('v1', 'v2');
+
+      fetchWithTimeout(STATUS_TIMEOUT_LIMIT, fetch(STATUS_ENDPOINT, getParams(user)))
+        .then(response => response.json())
+        .then(status => {
+          if (status.general_state !== STATUS_OK) {
+            onDegradedPerformanceCallback(STATUS_FAILING);
+          }
+        })
+        .catch(() => {
           onDegradedPerformanceCallback(STATUS_FAILING);
-        }
-      })
-      .catch(() => {
-        onDegradedPerformanceCallback(STATUS_FAILING);
-      });
-  }, []);
+        });
+    }
+  }, [hasUser]);
 }
