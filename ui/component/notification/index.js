540a527a61c2a622eb1d437a0cd41530a9c45ef7
Sean Yesmunt <sean@lbry.io>
2020-12-14 13:52:17 -0500

    swap seen & read notification behavior

--- ui/component/notification/index.js
+++ ui/component/notification/index.js
@@ -1,8 +1,8 @@
 import { connect } from 'react-redux';
-import { doSeeNotifications, doDeleteNotification } from 'redux/actions/notifications';
+import { doReadNotifications, doDeleteNotification } from 'redux/actions/notifications';
 import Notification from './view';
 
 export default connect(null, {
-  doSeeNotifications,
+  doReadNotifications,
   doDeleteNotification,
 })(Notification);
--- ui/component/notificationHeaderButton/index.js
+++ ui/component/notificationHeaderButton/index.js
@@ -2,19 +2,19 @@ import { connect } from 'react-redux';
 import {
   selectNotifications,
   selectIsFetchingNotifications,
-  selectUnreadNotificationCount,
+  selectUnseenNotificationCount,
 } from 'redux/selectors/notifications';
-import { doReadNotifications } from 'redux/actions/notifications';
+import { doSeeAllNotifications } from 'redux/actions/notifications';
 import { selectUser } from 'redux/selectors/user';
 import NotificationHeaderButton from './view';
 
 const select = state => ({
   notifications: selectNotifications(state),
   fetching: selectIsFetchingNotifications(state),
-  unreadCount: selectUnreadNotificationCount(state),
+  unseenCount: selectUnseenNotificationCount(state),
   user: selectUser(state),
 });
 
 export default connect(select, {
-  doReadNotifications,
+  doSeeAllNotifications,
 })(NotificationHeaderButton);
