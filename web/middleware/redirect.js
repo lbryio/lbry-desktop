f89a15636eee887cd07c016c7b8f9e8c13958d04
Sean Yesmunt <sean@lbry.io>
2020-12-22 21:26:20 -0500

    Revert "remove unnecessary redirect in server"
    This reverts commit e21b5be6f6d34b8f1bc75bfab73fec67837b3bcf.

--- web/middleware/redirect.js
+++ web/middleware/redirect.js
@@ -5,7 +5,6 @@ async function redirectMiddleware(ctx, next) {
   const requestHost = ctx.host;
   const path = ctx.path;
   const url = ctx.url;
-
   // Getting err: too many redirects on some urls because of this
   // Need a better solution
   // const decodedUrl = decodeURIComponent(url);
@@ -14,6 +13,11 @@ async function redirectMiddleware(ctx, next) {
   //   ctx.redirect(decodedUrl);
   //   return;
   // }
+
+  if (path.endsWith('/') && path.length > 1) {
+    ctx.redirect(url.replace(/\/$/, ''));
+    return;
+  }
 
   if (!path.startsWith('/$/') && path.match(/^([^@/:]+)\/([^:/]+)$/)) {
     ctx.redirect(url.replace(/^([^@/:]+)\/([^:/]+)(:(\/.*))/, '$1:$2')); // test against path, but use ctx.url to retain parameters
