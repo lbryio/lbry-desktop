d6663074e37a47b22e8e227c113ba010c038219f
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2021-02-01 19:59:34 -0300

    Fetch and display last release changes from GitHub in modal upgrade & modal auto update downloaded

--- ui/modal/modalUpgrade/view.jsx
+++ ui/modal/modalUpgrade/view.jsx
@@ -1,8 +1,7 @@
 // @flow
 import React from 'react';
 import { Modal } from 'modal/modal';
-import Button from 'component/button';
-import I18nMessage from 'component/i18nMessage';
+import LastReleaseChanges from 'component/lastReleaseChanges';
 
 type Props = {
   downloadUpgrade: () => void,
@@ -28,21 +27,7 @@ class ModalUpgrade extends React.PureComponent<Props> {
           {__('An updated version of LBRY is now available.')}{' '}
           {__('Your version is out of date and may be unreliable or insecure.')}
         </p>
-        <p className="help">
-          <I18nMessage
-            tokens={{
-              release_notes: (
-                <Button
-                  button="link"
-                  label={__('release notes')}
-                  href="https://github.com/lbryio/lbry-desktop/releases"
-                />
-              ),
-            }}
-          >
-            Want to know what has changed? See the %release_notes%.
-          </I18nMessage>
-        </p>
+        <LastReleaseChanges hideReleaseVersion />
       </Modal>
     );
   }
