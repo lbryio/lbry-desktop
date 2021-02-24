8b9c78c065ec67dcb59496c80eb87a3860f4f203
infinite-persistence <inf.persistence@gmail.com>
2021-02-24 14:02:19 +0800

    Restore 'duplicate claim URL' msg while waiting for 3704
    ## Issue
    5575: Show "URL already used" msg when publishing
    
    Seems like `uri` just got accidentally removed from db87125d

--- ui/component/publishName/index.js
+++ ui/component/publishName/index.js
@@ -10,18 +10,19 @@ import { selectActiveChannelClaim, selectIncognito } from 'redux/selectors/app';
 import { doSetActiveChannel } from 'redux/actions/app';
 import PublishPage from './view';
 
-const select = state => ({
+const select = (state) => ({
   name: makeSelectPublishFormValue('name')(state),
+  uri: makeSelectPublishFormValue('uri')(state),
   isStillEditing: selectIsStillEditing(state),
   myClaimForUri: selectMyClaimForUri(state),
   activeChannelClaim: selectActiveChannelClaim(state),
   incognito: selectIncognito(state),
 });
 
-const perform = dispatch => ({
-  updatePublishForm: value => dispatch(doUpdatePublishForm(value)),
+const perform = (dispatch) => ({
+  updatePublishForm: (value) => dispatch(doUpdatePublishForm(value)),
   prepareEdit: (claim, uri) => dispatch(doPrepareEdit(claim, uri)),
-  setActiveChannel: claimId => dispatch(doSetActiveChannel(claimId)),
+  setActiveChannel: (claimId) => dispatch(doSetActiveChannel(claimId)),
 });
 
 export default connect(select, perform)(PublishPage);
