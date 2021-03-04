df46fb82452115de01803c9aa0464c4572a4ff76
Sean Yesmunt <sean@lbry.io>
2021-03-04 17:07:09 -0500

    update livestream details

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -1,7 +1,7 @@
 //
-export const LIVE_STREAM_CHANNEL = '@NaomiBrockwell';
-export const LIVE_STREAM_CHANNEL_CLAIM_ID = '4ee7cfaf1fc50a6df858ed0b99c278d633bccca9';
-export const BITWAVE_USERNAME = 'naomibrockwelltv';
+export const LIVE_STREAM_CHANNEL = '@TheDesertLynx';
+export const LIVE_STREAM_CHANNEL_CLAIM_ID = 'ebe792fc52d170c4c4e3dd6c2eb36ce18755322e';
+export const BITWAVE_USERNAME = 'thedesertlynx';
 
 // export const LIVE_STREAM_CHANNEL = '@test-sy';
 // export const LIVE_STREAM_CHANNEL_CLAIM_ID = '5405fcc57103d72b54e84f1dea5ab77ec5252e67';
