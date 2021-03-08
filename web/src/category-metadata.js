9a630139cfa879170ca819ff79e56a52f7afef2c
infinite-persistence <inf.persistence@gmail.com>
2021-03-08 15:28:35 +0800

    Support for Category-Page Metadata definition.
    ## Issue
    Part of 4786: Add support for web metadata on category pages
    
    ## Note
    Remaining task: `category-metadata.js` needs to be filled first.

---
+++ web/src/category-metadata.js
@@ -0,0 +1,64 @@
+const PAGES = require('../../ui/constants/pages');
+
+module.exports.CATEGORY_METADATA = {
+  [PAGES.BIG_HITS]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.COMMUNITY]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.ENLIGHTENMENT]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.FINANCE]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.GAMING]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.GENERAL]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.LAB]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.NEWS]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.RABBIT_HOLE]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.SCIENCE]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.TECHNOLOGY]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+  [PAGES.WILD_WEST]: {
+    title: '',
+    description: '',
+    image: '',
+  },
+};
