5630490ae8ed6b5fb6f39cafe97c3731c4fe2337
Sean Yesmunt <sean@lbry.io>
2021-03-11 10:40:46 -0500

    update livestream for naomi

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -1,7 +1,7 @@
 //
-export const LIVE_STREAM_CHANNEL = '@DigitalCashNetwork';
-export const LIVE_STREAM_CHANNEL_CLAIM_ID = 'c5b0b17838df2f6c31162f64d55f60f34ae8bfc6';
-export const BITWAVE_USERNAME = 'thedesertlynx';
+export const LIVE_STREAM_CHANNEL = '@NaomiBrockwell';
+export const LIVE_STREAM_CHANNEL_CLAIM_ID = '4ee7cfaf1fc50a6df858ed0b99c278d633bccca9';
+export const BITWAVE_USERNAME = 'naomibrockwelltv';
 
 // export const LIVE_STREAM_CHANNEL = '@test-sy';
 // export const LIVE_STREAM_CHANNEL_CLAIM_ID = '5405fcc57103d72b54e84f1dea5ab77ec5252e67';
