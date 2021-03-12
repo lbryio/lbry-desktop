a266bb1a573816f5787c786d351bfd9ab3ec7f3d
Thomas Zarebczan <thomas.zarebczan@gmail.com>
2021-03-12 18:22:40 -0500

    Slavguns

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -1,7 +1,7 @@
 //
-export const LIVE_STREAM_CHANNEL = '@DigitalCashNetwork';
-export const LIVE_STREAM_CHANNEL_CLAIM_ID = 'c5b0b17838df2f6c31162f64d55f60f34ae8bfc6';
-export const BITWAVE_USERNAME = 'thedesertlynx';
+export const LIVE_STREAM_CHANNEL = '@SlavGuns';
+export const LIVE_STREAM_CHANNEL_CLAIM_ID = '6f3d20c0f872abb32701c07d5e1b667a64a29a95';
+export const BITWAVE_USERNAME = 'SlavGuns';
 
 // export const LIVE_STREAM_CHANNEL = '@test-sy';
 // export const LIVE_STREAM_CHANNEL_CLAIM_ID = '5405fcc57103d72b54e84f1dea5ab77ec5252e67';
