b467873f0b2ac8a17a7896816b3be7e26860bcdf
infinite-persistence <inf.persistence@gmail.com>
2021-03-16 09:42:49 +0800

    Notification bell shows is_read, not is_seen
    ## Issue
    Closes 5664: notification bell shows is_read, not is_seen

--- ui/component/sideNavigation/index.js
+++ ui/component/sideNavigation/index.js
@@ -5,17 +5,17 @@ import { selectFollowedTags } from 'redux/selectors/tags';
 import { selectUserVerifiedEmail, selectUser } from 'redux/selectors/user';
 import { selectHomepageData, selectLanguage } from 'redux/selectors/settings';
 import { doSignOut } from 'redux/actions/app';
-import { selectUnreadNotificationCount } from 'redux/selectors/notifications';
+import { selectUnseenNotificationCount } from 'redux/selectors/notifications';
 
 import SideNavigation from './view';
 
-const select = state => ({
+const select = (state) => ({
   subscriptions: selectSubscriptions(state),
   followedTags: selectFollowedTags(state),
   language: selectLanguage(state), // trigger redraw on language change
   email: selectUserVerifiedEmail(state),
   purchaseSuccess: selectPurchaseUriSuccess(state),
-  unreadCount: selectUnreadNotificationCount(state),
+  unseenCount: selectUnseenNotificationCount(state),
   user: selectUser(state),
   homepageData: selectHomepageData(state),
 });
