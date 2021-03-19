3c51e699f653ce85be8dca8679d5d3e4697d094a
Sean Yesmunt <sean@lbry.io>
2021-03-19 10:53:11 -0400

    comment out unused categories

--- web/src/category-metadata.js
+++ web/src/category-metadata.js
@@ -1,64 +1,66 @@
-const PAGES = require('../../ui/constants/pages');
+// const PAGES = require('../../ui/constants/pages');
+
+// Uncomment as you add metadata
 
 module.exports.CATEGORY_METADATA = {
-  [PAGES.BIG_HITS]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.COMMUNITY]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.ENLIGHTENMENT]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.FINANCE]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.GAMING]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.GENERAL]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.LAB]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.NEWS]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.RABBIT_HOLE]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.SCIENCE]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.TECHNOLOGY]: {
-    title: '',
-    description: '',
-    image: '',
-  },
-  [PAGES.WILD_WEST]: {
-    title: '',
-    description: '',
-    image: '',
-  },
+  //   [PAGES.BIG_HITS]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.COMMUNITY]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.ENLIGHTENMENT]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.FINANCE]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.GAMING]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.GENERAL]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.LAB]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.NEWS]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.RABBIT_HOLE]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.SCIENCE]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.TECHNOLOGY]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
+  //   [PAGES.WILD_WEST]: {
+  //     title: '',
+  //     description: '',
+  //     image: '',
+  //   },
 };
