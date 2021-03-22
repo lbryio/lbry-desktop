f18ad64527464d1ad59def277d07f01c58ace00f
DispatchCommit <dispatchcommit@gmail.com>
2021-03-22 13:22:10 -0700

    use new livestream iframe embed

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -1,2 +1,2 @@
-export const BITWAVE_EMBED_URL = 'https://bitwave.tv/embed';
+export const BITWAVE_EMBED_URL = 'https://bitwave.tv/odysee';
 export const BITWAVE_API = 'https://api.bitwave.tv/v1/odysee/live';
