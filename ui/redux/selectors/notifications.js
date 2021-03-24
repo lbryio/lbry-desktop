18279333b0df94c5794269ad9bd5b438242d52c2
infinite-persistence <inf.persistence@gmail.com>
2021-03-24 15:45:41 +0800

    Augment doNotificationList to get a filtered list.
    Initially, the filtered list was done at the component level, and the list was simply a subset of `notifications`. But due to the limit issue explained in 5694, we now query the filtered list instead.
    
    Considerations:
    - The filtered list could contain items not listed in the 'All' list. We could add a string at the bottom of 'All' that says "not all items retrieved" if this confuses the user.
    - The unseen count needs to be based on 'All' and not the filtered one, so that data needs to be stashed somehow (can't re-use the array).
    
    Use 2 arrays for now instead of trying to accumulate "all" and "filtered" into 1 array.

--- ui/redux/selectors/notifications.js
+++ ui/redux/selectors/notifications.js
@@ -1,15 +1,17 @@
 import { createSelector } from 'reselect';
 
-export const selectState = state => state.notifications || {};
+export const selectState = (state) => state.notifications || {};
 
-export const selectNotifications = createSelector(selectState, state => state.notifications);
+export const selectNotifications = createSelector(selectState, (state) => state.notifications);
 
-export const makeSelectNotificationForCommentId = id =>
-  createSelector(selectNotifications, notifications => {
+export const selectNotificationsFiltered = createSelector(selectState, (state) => state.notificationsFiltered);
+
+export const makeSelectNotificationForCommentId = (id) =>
+  createSelector(selectNotifications, (notifications) => {
     const match =
       notifications &&
       notifications.find(
-        n =>
+        (n) =>
           n.notification_parameters &&
           n.notification_parameters.dynamic &&
           n.notification_parameters.dynamic.hash === id
@@ -17,17 +19,17 @@ export const makeSelectNotificationForCommentId = id =>
     return match;
   });
 
-export const selectIsFetchingNotifications = createSelector(selectState, state => state.fetchingNotifications);
+export const selectIsFetchingNotifications = createSelector(selectState, (state) => state.fetchingNotifications);
 
-export const selectUnreadNotificationCount = createSelector(selectNotifications, notifications => {
-  return notifications ? notifications.filter(notification => !notification.is_read).length : 0;
+export const selectUnreadNotificationCount = createSelector(selectNotifications, (notifications) => {
+  return notifications ? notifications.filter((notification) => !notification.is_read).length : 0;
 });
 
-export const selectUnseenNotificationCount = createSelector(selectNotifications, notifications => {
-  return notifications ? notifications.filter(notification => !notification.is_seen).length : 0;
+export const selectUnseenNotificationCount = createSelector(selectNotifications, (notifications) => {
+  return notifications ? notifications.filter((notification) => !notification.is_seen).length : 0;
 });
 
-export const selectToast = createSelector(selectState, state => {
+export const selectToast = createSelector(selectState, (state) => {
   if (state.toasts.length) {
     const { id, params } = state.toasts[0];
     return {
@@ -39,7 +41,7 @@ export const selectToast = createSelector(selectState, state => {
   return null;
 });
 
-export const selectError = createSelector(selectState, state => {
+export const selectError = createSelector(selectState, (state) => {
   if (state.errors.length) {
     const { error } = state.errors[0];
     return {
