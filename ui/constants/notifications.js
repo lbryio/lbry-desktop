4252db91f96e80af9e83514957828c3f65a54beb
infinite-persistence <inf.persistence@gmail.com>
2021-03-24 15:26:56 +0800

    Add new notification filter api params

--- ui/constants/notifications.js
+++ ui/constants/notifications.js
@@ -4,3 +4,10 @@ export const NOTIFICATION_REPLY = 'comment-reply';
 export const DAILY_WATCH_AVAILABLE = 'daily_watch_available';
 export const DAILY_WATCH_REMIND = 'daily_watch_remind';
 export const NEW_CONTENT = 'new_content';
+
+export const NOTIFICATION_RULE_NONE = '';
+export const NOTIFICATION_RULE_COMMENTS = 'comments';
+export const NOTIFICATION_RULE_REPLIES = 'comment_replies';
+export const NOTIFICATION_RULE_FOLLOWERS = 'followers';
+export const NOTIFICATION_RULE_NEW_CONTENT = 'new_content';
+export const NOTIFICATION_RULE_OTHERS = 'default';
