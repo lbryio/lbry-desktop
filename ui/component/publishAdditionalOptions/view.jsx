e78b5d4c124884e41d4d480d5500c6e8c7046d6a
zeppi <jessopb@gmail.com>
2021-03-24 18:56:54 -0400

    publish page fixes

--- ui/component/publishAdditionalOptions/view.jsx
+++ ui/component/publishAdditionalOptions/view.jsx
@@ -8,7 +8,6 @@ import LicenseType from './license-type';
 import Card from 'component/common/card';
 import SUPPORTED_LANGUAGES from 'constants/supported_languages';
 
-import PublishName from 'component/publishName';
 // @if TARGET='app'
 // import ErrorText from 'component/common/error-text';
 // import { LbryFirst } from 'lbry-redux';
@@ -116,7 +115,6 @@ function PublishAdditionalOptions(props: Props) {
         <React.Fragment>
           {!hideSection && (
             <div className={classnames({ 'card--disabled': !name })}>
-              <PublishName />
               {/* @if TARGET='app' */}
               {/* {showLbryFirstCheckbox && (
                 <div className="section">
