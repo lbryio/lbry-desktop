48f0c5e413d345096ff53532ee9e7239003a00f9
Julian Chandra <67612037+julianchandra@users.noreply.github.com>
2021-03-30 10:34:50 -0700

    updating tagline
    updating tagline

--- ui/page/home/view.jsx
+++ ui/page/home/view.jsx
@@ -80,7 +80,7 @@ function HomePage(props: Props) {
         </div>
       )}
 
-      <h1 className="home__meme">{__("Video stonks.")}</h1>
+      <h1 className="home__meme">{__("#HelpLBRYsavecrypto")}</h1>
 
       {rowData.map(({ title, route, link, icon, help, options = {} }, index) => (
         <div key={title} className="claim-grid__wrapper">
