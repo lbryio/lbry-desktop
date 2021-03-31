aec44841a4c2b1d5618c133282ffc12727d96e41
jessopb <36554050+jessopb@users.noreply.github.com>
2021-03-31 18:32:48 -0400

    do not get stuck on publishing spinner (#5806)

--- ui/component/publishForm/view.jsx
+++ ui/component/publishForm/view.jsx
@@ -202,6 +202,13 @@ function PublishForm(props: Props) {
     }
   }
 
+  // if you enter the page and it is stuck in publishing, "stop it."
+  useEffect(() => {
+    if (publishing) {
+      clearPublish();
+    }
+  }, []);
+
   useEffect(() => {
     if (!thumbnail) {
       resetThumbnailStatus();
