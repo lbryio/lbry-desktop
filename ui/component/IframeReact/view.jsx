ac18b85c3425330d6968a41db9ed97f2ef09c6c8
Sean Yesmunt <sean@lbry.io>
2021-04-02 15:48:37 -0400

    sandbox iframe react component

--- ui/component/IframeReact/view.jsx
+++ ui/component/IframeReact/view.jsx
@@ -30,6 +30,6 @@ export default function I18nMessage(props: Props) {
   return (
     // style={{height: iframeHeight}}
     // ref={iframeRef}
-    <iframe src={src} title={title} onLoad={onLoad} />
+    <iframe src={src} title={title} onLoad={onLoad} sandbox="" />
   );
 }
