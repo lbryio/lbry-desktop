03ef30eb40736dbec0f5fe76b53d536da8129441
Sean Yesmunt <sean@lbry.io>
2021-04-03 02:21:54 -0400

    claim email_verified reward on channel/new page

--- ui/page/channelNew/index.js
+++ ui/page/channelNew/index.js
@@ -1,9 +1,20 @@
+import REWARD_TYPES from 'rewards';
 import { connect } from 'react-redux';
 import { selectBalance } from 'lbry-redux';
+import { selectUserVerifiedEmail } from 'redux/selectors/user';
+import { doClaimRewardType } from 'redux/actions/rewards';
 import ChannelNew from './view';
 
 const select = (state) => ({
   balance: selectBalance(state),
+  isAuthenticated: selectUserVerifiedEmail(state),
 });
 
-export default connect(select)(ChannelNew);
+export default connect(select, (dispatch) => ({
+  claimConfirmEmailReward: () =>
+    dispatch(
+      doClaimRewardType(REWARD_TYPES.TYPE_CONFIRM_EMAIL, {
+        notifyError: false,
+      })
+    ),
+}))(ChannelNew);
--- ui/page/channelNew/view.jsx
+++ ui/page/channelNew/view.jsx
@@ -8,14 +8,22 @@ import YrblWalletEmpty from 'component/yrblWalletEmpty';
 
 type Props = {
   balance: number,
+  claimConfirmEmailReward: () => void,
+  isAuthenticated: boolean,
 };
 
 function ChannelNew(props: Props) {
-  const { balance } = props;
+  const { balance, claimConfirmEmailReward, isAuthenticated } = props;
   const { push, location } = useHistory();
   const urlSearchParams = new URLSearchParams(location.search);
   const redirectUrl = urlSearchParams.get('redirect');
   const emptyBalance = balance === 0;
+
+  React.useEffect(() => {
+    if (isAuthenticated && emptyBalance) {
+      claimConfirmEmailReward();
+    }
+  }, [isAuthenticated, claimConfirmEmailReward, emptyBalance]);
 
   return (
     <Page noSideNavigation noFooter backout={{ title: __('Create a channel'), backLabel: __('Cancel') }}>
