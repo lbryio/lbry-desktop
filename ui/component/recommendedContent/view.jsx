a678020d4e737671666503160a5126e2f5fe61c1
saltrafael <a657ee92-7eb9-46eb-97d2-6735799df838@aleeas.com>
2021-04-06 16:10:09 -0300

    Keep just More From for now

--- ui/component/recommendedContent/view.jsx
+++ ui/component/recommendedContent/view.jsx
@@ -1,7 +1,6 @@
 // @flow
 import { SHOW_ADS, SIMPLE_SITE } from 'config';
 import React from 'react';
-import classnames from 'classnames';
 import ClaimList from 'component/claimList';
 import ClaimListDiscover from 'component/claimListDiscover';
 import Ads from 'web/component/ads';
@@ -27,14 +26,10 @@ type Props = {
 
 export default function RecommendedContent(props: Props) {
   const { uri, claim, search, mature, recommendedContent, nextRecommendedUri, isSearching, isAuthenticated } = props;
-  const [expanded, setExpanded] = React.useState(false);
   const [allRelated, setAllRelated] = React.useState(true);
   const [moreFrom, setMoreFrom] = React.useState(false);
-  const [tagList, setTagList] = React.useState(false);
   const signingChannel = claim && claim.signing_channel;
   const channelName = signingChannel ? signingChannel.name : null;
-  const claimValue = claim && claim.value;
-  const tags = claimValue ? claimValue.tags : null;
   const isMobile = useIsMobile();
   const isMedium = useIsMediumScreen();
 
@@ -82,10 +77,7 @@ export default function RecommendedContent(props: Props) {
       className="file-page__recommended"
       title={__('Related')}
       subtitle={
-        <div className={classnames({
-          'related_content-more--contracted': !expanded,
-          'related_content-more--expanded': expanded
-        })}>
+        <div className={'related_content-more'}>
           <Button
             button="alt"
             label={__('All')}
@@ -104,21 +96,6 @@ export default function RecommendedContent(props: Props) {
               setTagList(false)
             }}
           />}
-          {tags && (expanded ? (
-          <Button  button="link" label={__('Less')} onClick={() => setExpanded(!expanded)} />
-          ) : (
-          <Button button="link" label={__('More')} onClick={() => setExpanded(!expanded)} />
-          ))}
-          {tags && tags.map(name => (
-          <Button
-            button="alt"
-            label={__(`${name}`)}
-            onClick={() => {
-              setAllRelated(false);
-              setMoreFrom(false);
-              setTagList(name)
-            }}
-          />))}
         </div>
       }
       body={
@@ -170,33 +147,7 @@ export default function RecommendedContent(props: Props) {
               }
               empty={__('No related content found')}
             />
-          )
-      }
-          {tagList && (
-            <ClaimListDiscover
-              hideAdvancedFilter={true}
-              type="small"
-              claimType={['stream']}
-              feeAmount='0'
-              pageSize={20}
-              infiniteScroll={false}
-              tags={[tagList]}
-              loading={isSearching}
-              hideMenu={isMobile}
-              injectedItem={
-                SHOW_ADS && IS_WEB ? (
-                  SIMPLE_SITE ? (
-                    <Ads small type={'google'} uri={uri} />
-                  ) : (
-                    !isAuthenticated && <Ads small type={'video'} />
-                  )
-                ) : (
-                  false
-                )
-              }
-              empty={__('No related content found')}
-            />
-            )}
+          )}
         </div>
       }
     />
