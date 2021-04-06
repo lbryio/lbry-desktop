73a4462252bdf033d3e7c2ec55ef17993dda9efe
saltrafael <a657ee92-7eb9-46eb-97d2-6735799df838@aleeas.com>
2021-04-06 15:59:12 -0300

    Recommended bar: extra options for ease of discovery

--- ui/component/recommendedContent/view.jsx
+++ ui/component/recommendedContent/view.jsx
@@ -1,10 +1,13 @@
 // @flow
 import { SHOW_ADS, SIMPLE_SITE } from 'config';
 import React from 'react';
+import classnames from 'classnames';
 import ClaimList from 'component/claimList';
+import ClaimListDiscover from 'component/claimListDiscover';
 import Ads from 'web/component/ads';
 import Card from 'component/common/card';
 import { useIsMobile, useIsMediumScreen } from 'effects/use-screensize';
+import Button from 'component/button';
 
 type Props = {
   uri: string,
@@ -26,6 +29,14 @@ export default function RecommendedContent(props: Props) {
     isSearching,
     isAuthenticated,
   } = props;
+  const [expanded, setExpanded] = React.useState(false);
+  const [allRelated, setAllRelated] = React.useState(true);
+  const [moreFrom, setMoreFrom] = React.useState(false);
+  const [tagList, setTagList] = React.useState(false);
+  const signingChannel = claim && claim.signing_channel;
+  const channelName = signingChannel ? signingChannel.name : null;
+  const claimValue = claim && claim.value;
+  const tags = claimValue ? claimValue.tags : null;
   const isMobile = useIsMobile();
   const isMedium = useIsMediumScreen();
 
@@ -55,25 +66,129 @@ export default function RecommendedContent(props: Props) {
       smallTitle={!isMobile && !isMedium}
       className="file-page__recommended"
       title={__('Related')}
-      body={
-        <ClaimList
-          type="small"
-          loading={isSearching}
-          uris={reorderList(recommendedContent)}
-          hideMenu={isMobile}
-          injectedItem={
-            SHOW_ADS && IS_WEB ? (
-              SIMPLE_SITE ? (
-                <Ads small type={'google'} uri={uri} />
-              ) : (
-                !isAuthenticated && <Ads small type={'video'} />
-              )
+      subtitle={
+        <div
+          className={classnames({
+            'related_content-more--contracted': !expanded,
+            'related_content-more--expanded': expanded,
+          })}
+        >
+          <Button
+            button="alt"
+            label={__('All')}
+            onClick={() => {
+              setAllRelated(true);
+              setMoreFrom(false);
+              setTagList(false);
+            }}
+          />
+          {channelName && (
+            <Button
+              button="alt"
+              label={__('More from %claim_name%', { claim_name: channelName })}
+              onClick={() => {
+                setAllRelated(false);
+                setMoreFrom(true);
+                setTagList(false);
+              }}
+            />
+          )}
+          {tags &&
+            (expanded ? (
+              <Button button="link" label={__('Less')} onClick={() => setExpanded(!expanded)} />
             ) : (
-              false
-            )
-          }
-          empty={__('No related content found')}
-        />
+              <Button button="link" label={__('More')} onClick={() => setExpanded(!expanded)} />
+            ))}
+          {tags &&
+            tags.map((name) => (
+              <Button
+                button="alt"
+                label={__(`${name}`)}
+                onClick={() => {
+                  setAllRelated(false);
+                  setMoreFrom(false);
+                  setTagList(name);
+                }}
+              />
+            ))}
+        </div>
+      }
+      body={
+        <div>
+          {allRelated && (
+            <ClaimList
+              type="small"
+              loading={isSearching}
+              uris={reorderList(recommendedContent)}
+              hideMenu={isMobile}
+              injectedItem={
+                SHOW_ADS && IS_WEB ? (
+                  SIMPLE_SITE ? (
+                    <Ads small type={'google'} uri={uri} />
+                  ) : (
+                    !isAuthenticated && <Ads small type={'video'} />
+                  )
+                ) : (
+                  false
+                )
+              }
+              empty={__('No related content found')}
+            />
+          )}
+          {moreFrom && (
+            <ClaimListDiscover
+              hideAdvancedFilter={true}
+              tileLayout={false}
+              showHeader={false}
+              type="small"
+              claimType={['stream']}
+              orderBy="new"
+              pageSize={20}
+              infiniteScroll={false}
+              hideFilters={true}
+              channelIds={[signingChannel.claim_id]}
+              loading={isSearching}
+              hideMenu={isMobile}
+              injectedItem={
+                SHOW_ADS && IS_WEB ? (
+                  SIMPLE_SITE ? (
+                    <Ads small type={'google'} uri={uri} />
+                  ) : (
+                    !isAuthenticated && <Ads small type={'video'} />
+                  )
+                ) : (
+                  false
+                )
+              }
+              empty={__('No related content found')}
+            />
+          )}
+          {tagList && (
+            <ClaimListDiscover
+              hideAdvancedFilter={true}
+              type="small"
+              claimType={['stream']}
+              feeAmount="0"
+              pageSize={20}
+              infiniteScroll={false}
+              tags={[tagList]}
+              loading={isSearching}
+              hideMenu={isMobile}
+              injectedItem={
+                SHOW_ADS && IS_WEB ? (
+                  SIMPLE_SITE ? (
+                    <Ads small type={'google'} uri={uri} />
+                  ) : (
+                    !isAuthenticated && <Ads small type={'video'} />
+                  )
+                ) : (
+                  false
+                )
+              }
+              empty={__('No related content found')}
+            />
+          )}
+        </div>
       }
     />
   );
