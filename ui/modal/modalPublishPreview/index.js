62d959d5422629eda591baa03210ab42bc4ec87b
zeppi <jessopb@gmail.com>
2021-04-09 13:26:04 -0400

    publish preview for livestream update

--- ui/modal/modalPublishPreview/index.js
+++ ui/modal/modalPublishPreview/index.js
@@ -6,6 +6,7 @@ import {
   selectPublishFormValues,
   selectIsStillEditing,
   selectMyChannelClaims,
+  makeSelectClaimIsStreamPlaceholder,
   SETTINGS,
   doClearPublish,
 } from 'lbry-redux';
@@ -13,16 +14,22 @@ import { selectFfmpegStatus, makeSelectClientSetting } from 'redux/selectors/set
 import { doPublishDesktop } from 'redux/actions/publish';
 import { doSetClientSetting } from 'redux/actions/settings';
 
-const select = (state) => ({
-  ...selectPublishFormValues(state),
-  myChannels: selectMyChannelClaims(state),
-  isVid: makeSelectPublishFormValue('fileVid')(state),
-  publishSuccess: makeSelectPublishFormValue('publishSuccess')(state),
-  publishing: makeSelectPublishFormValue('publishing')(state),
-  isStillEditing: selectIsStillEditing(state),
-  ffmpegStatus: selectFfmpegStatus(state),
-  enablePublishPreview: makeSelectClientSetting(SETTINGS.ENABLE_PUBLISH_PREVIEW)(state),
-});
+const select = (state, props) => {
+  const editingUri = makeSelectPublishFormValue('editingURI')(state);
+
+  return {
+    ...selectPublishFormValues(state),
+    myChannels: selectMyChannelClaims(state),
+    isVid: makeSelectPublishFormValue('fileVid')(state),
+    publishSuccess: makeSelectPublishFormValue('publishSuccess')(state),
+    publishing: makeSelectPublishFormValue('publishing')(state),
+    remoteFile: makeSelectPublishFormValue('remoteFileUrl')(state),
+    isStillEditing: selectIsStillEditing(state),
+    ffmpegStatus: selectFfmpegStatus(state),
+    enablePublishPreview: makeSelectClientSetting(SETTINGS.ENABLE_PUBLISH_PREVIEW)(state),
+    isLivestreamClaim: makeSelectClaimIsStreamPlaceholder(editingUri)(state),
+  };
+};
 
 const perform = (dispatch) => ({
   publish: (filePath, preview) => dispatch(doPublishDesktop(filePath, preview)),
