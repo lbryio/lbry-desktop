bb766211f92bd4edb34d2ae3ada5a2606826148e
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2021-04-13 14:22:14 -0400

    Julian's category changes (#5878)

--- web/src/category-metadata.js
+++ web/src/category-metadata.js
@@ -1,66 +1,66 @@
-// const PAGES = require('../../ui/constants/pages');
+const PAGES = require('../../ui/constants/pages');
 
 // Uncomment as you add metadata
 
 module.exports.CATEGORY_METADATA = {
-  //   [PAGES.BIG_HITS]: {
-  //     title: '',
-  //     description: '',
-  //     image: '',
-  //   },
-  //   [PAGES.COMMUNITY]: {
-  //     title: '',
-  //     description: '',
-  //     image: '',
-  //   },
+  [PAGES.BIG_HITS]: {
+    title: 'Big Hits',
+    description: 'Animation, pop culture, comedy, and all the other weird on Odysee',
+    image: '',
+  },
+  [PAGES.COMMUNITY]: {
+    title: 'The Universe',
+    description: 'Podcasts, life, learning, and everything else on Odysee',
+    image: '',
+  },
   //   [PAGES.ENLIGHTENMENT]: {
   //     title: '',
   //     description: '',
   //     image: '',
   //   },
-  //   [PAGES.FINANCE]: {
-  //     title: '',
-  //     description: '',
-  //     image: '',
-  //   },
-  //   [PAGES.GAMING]: {
-  //     title: '',
-  //     description: '',
-  //     image: '',
-  //   },
+  [PAGES.FINANCE]: {
+    title: 'Finance 2.0',
+    description: 'Crypto, Money, Economics, Markets on Odysee ',
+    image: '',
+  },
+  [PAGES.GAMING]: {
+    title: 'Gaming',
+    description: 'Pew pew bzzz gaming on Odysee',
+    image: '',
+  },
   //   [PAGES.GENERAL]: {
   //     title: '',
   //     description: '',
   //     image: '',
   //   },
-  //   [PAGES.LAB]: {
-  //     title: '',
-  //     description: '',
-  //     image: '',
-  //   },
-  //   [PAGES.NEWS]: {
-  //     title: '',
-  //     description: '',
-  //     image: '',
-  //   },
-  //   [PAGES.RABBIT_HOLE]: {
-  //     title: '',
-  //     description: '',
-  //     image: '',
-  //   },
+  [PAGES.LAB]: {
+    title: 'Lab',
+    description: 'Science - the real kind, on Odysee',
+    image: '',
+  },
+  [PAGES.NEWS]: {
+    title: 'News & Politics',
+    description: `Stay up to date with all that's happening around the world on Odysee`,
+    image: '',
+  },
+  [PAGES.RABBIT_HOLE]: {
+    title: '',
+    description: '',
+    image: '',
+  },
   //   [PAGES.SCIENCE]: {
   //     title: '',
   //     description: '',
   //     image: '',
   //   },
-  //   [PAGES.TECHNOLOGY]: {
-  //     title: '',
-  //     description: '',
-  //     image: '',
-  //   },
-  //   [PAGES.WILD_WEST]: {
-  //     title: '',
-  //     description: '',
-  //     image: '',
-  //   },
+  [PAGES.TECHNOLOGY]: {
+    title: 'Tech',
+    description: 'Hardware, software, startups, photography on Odysee',
+    image: '',
+  },
+  [PAGES.WILD_WEST]: {
+    title: 'Wild West',
+    description: 'Boosted by user credits, this is what the community promotes on Odysee',
+    image: '',
+  },
 };
