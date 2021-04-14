f21b3b350e47fe67a512b453253a3b3541be67b9
zeppi <jessopb@gmail.com>
2021-04-14 13:45:45 -0400

    allow non video selection when not odysee

--- ui/component/publishFile/view.jsx
+++ ui/component/publishFile/view.jsx
@@ -1,5 +1,5 @@
 // @flow
-import { SITE_NAME, WEB_PUBLISH_SIZE_LIMIT_GB } from 'config';
+import { SITE_NAME, WEB_PUBLISH_SIZE_LIMIT_GB, SIMPLE_SITE } from 'config';
 import type { Node } from 'react';
 import * as ICONS from 'constants/icons';
 import React, { useState, useEffect } from 'react';
@@ -512,13 +512,13 @@ function PublishFile(props: Props) {
             {fileSelectSource === SOURCE_UPLOAD && showFileUpload && (
               <>
                 <FileSelector
-                  label={__('Video file')}
+                  label={SIMPLE_SITE ? __('Video file') : __('File')}
                   disabled={disabled}
                   currentPath={currentFile}
                   onFileChosen={handleFileChange}
                   // https://stackoverflow.com/questions/19107685/safari-input-type-file-accept-video-ignores-mp4-files
-                  accept="video/mp4,video/x-m4v,video/*"
-                  placeholder={__('Select video file to upload')}
+                  accept={SIMPLE_SITE ? 'video/mp4,video/x-m4v,video/*' : undefined}
+                  placeholder={SIMPLE_SITE ? __('Select video file to upload') : __('Select a file to upload')}
                 />
                 {getUploadMessage()}
               </>
@@ -617,8 +617,7 @@ function PublishFile(props: Props) {
               currentPath={currentFile}
               onFileChosen={handleFileChange}
               // https://stackoverflow.com/questions/19107685/safari-input-type-file-accept-video-ignores-mp4-files
-              accept="video/mp4,video/x-m4v,video/*"
-              placeholder={__('Select video file to upload')}
+              placeholder={__('Select file to upload')}
             />
           )}
           {showFileUpload && (
