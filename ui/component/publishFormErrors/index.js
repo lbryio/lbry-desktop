9689ef72a1923f0b3a87b0ee5f9335c26b685454
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2021-04-15 22:52:41 -0400

    Odysee audio support (#5897)

--- ui/component/publishFormErrors/index.js
+++ ui/component/publishFormErrors/index.js
@@ -2,13 +2,14 @@ import { connect } from 'react-redux';
 import { makeSelectPublishFormValue, selectIsStillEditing } from 'lbry-redux';
 import PublishPage from './view';
 
-const select = state => ({
+const select = (state) => ({
   bid: makeSelectPublishFormValue('bid')(state),
   name: makeSelectPublishFormValue('name')(state),
   title: makeSelectPublishFormValue('title')(state),
   bidError: makeSelectPublishFormValue('bidError')(state),
   editingURI: makeSelectPublishFormValue('editingURI')(state),
   uploadThumbnailStatus: makeSelectPublishFormValue('uploadThumbnailStatus')(state),
+  thumbnail: makeSelectPublishFormValue('thumbnail_url')(state),
   isStillEditing: selectIsStillEditing(state),
 });
 
