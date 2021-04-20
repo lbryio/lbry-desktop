762005f217b831082c471817a908979b809c5548
DispatchCommit <dispatchcommit@gmail.com>
2021-04-20 00:49:08 -0700

    remove prometheus event that doesn't exist and causes errors
    "there won’t be a metric by this name, our code is just getting errors" - andrey

--- ui/analytics.js
+++ ui/analytics.js
@@ -198,7 +198,6 @@ const analytics: Analytics = {
   },
 
   videoFetchDuration: (source, duration) => {
-    sendPromMetric('time_to_fetch', duration);
     sendMatomoEvent('Media', 'TimeToFetch', source, duration);
   },
 
