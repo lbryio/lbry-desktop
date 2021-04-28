741fbddfa2ae397e222111316ccafcfe7b2e5dc3
infinite-persistence <inf.persistence@gmail.com>
2021-04-29 01:35:03 +0800

    Fix ability to delete own comments in livestream
    ## Issue
    5832: can't remove own comments in live stream mode (if you have multiple channels?)
    
    Looks like it was just missing `commentIsMine` for the new component.

--- ui/component/livestreamComments/index.js
+++ ui/component/livestreamComments/index.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { makeSelectClaimForUri } from 'lbry-redux';
+import { makeSelectClaimForUri, selectMyChannelClaims } from 'lbry-redux';
 import { doCommentSocketConnect, doCommentSocketDisconnect } from 'redux/actions/websocket';
 import { doCommentList, doSuperChatList } from 'redux/actions/comments';
 import {
@@ -16,6 +16,7 @@ const select = (state, props) => ({
   fetchingComments: selectIsFetchingComments(state),
   superChats: makeSelectSuperChatsForUri(props.uri)(state),
   superChatsTotalAmount: makeSelectSuperChatTotalAmountForUri(props.uri)(state),
+  myChannels: selectMyChannelClaims(state),
 });
 
 export default connect(select, {
