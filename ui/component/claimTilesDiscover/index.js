123e968aeee47d29608c3edebc15cc41c495ff97
infinite-persistence <inf.persistence@gmail.com>
2021-04-28 17:53:04 +0800

    Homepage: move "LIVE" livestreams to the front.

--- ui/component/claimTilesDiscover/index.js
+++ ui/component/claimTilesDiscover/index.js
@@ -1,5 +1,11 @@
 import { connect } from 'react-redux';
-import { doClaimSearch, selectClaimSearchByQuery, selectFetchingClaimSearchByQuery, SETTINGS } from 'lbry-redux';
+import {
+  doClaimSearch,
+  selectClaimSearchByQuery,
+  selectFetchingClaimSearchByQuery,
+  SETTINGS,
+  selectClaimsByUri,
+} from 'lbry-redux';
 import { doToggleTagFollowDesktop } from 'redux/actions/tags';
 import { makeSelectClientSetting, selectShowMatureContent } from 'redux/selectors/settings';
 import { selectModerationBlockList } from 'redux/selectors/comments';
@@ -8,6 +14,7 @@ import ClaimListDiscover from './view';
 
 const select = (state) => ({
   claimSearchByQuery: selectClaimSearchByQuery(state),
+  claimsByUri: selectClaimsByUri(state),
   fetchingClaimSearchByQuery: selectFetchingClaimSearchByQuery(state),
   showNsfw: selectShowMatureContent(state),
   hideReposts: makeSelectClientSetting(SETTINGS.HIDE_REPOSTS)(state),
