b9a7f9673d38fdb0f1474317fc0b88ad3f2829fb
Sean Yesmunt <sean@lbry.io>
2021-04-30 16:45:18 -0400

    remove has_source param from livestream claim_search on homepage

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -244,7 +244,9 @@ function ClaimTilesDiscover(props: Props) {
     if (shouldPerformSearch) {
       const searchOptions = JSON.parse(optionsStringForEffect);
       doClaimSearch(searchOptions);
+
       if (liveLivestreamsFirst) {
+        delete searchOptions.has_source;
         doClaimSearch({ ...searchOptions, has_no_source: true });
       }
     }
