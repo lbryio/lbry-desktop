1f0d8c120f7ad055f65ff85bc6bb952c398bdfe0
jessopb <36554050+jessopb@users.noreply.github.com>
2021-05-03 15:39:46 -0400

    Revert "wip"
    This reverts commit d17e477fe0e6633709ea30bdc403448825db4c71.

--- ui/component/userChannelFollowIntro/view.jsx
+++ ui/component/userChannelFollowIntro/view.jsx
@@ -14,7 +14,6 @@ type Props = {
   channelSubscribe: (sub: Subscription) => void,
   homepageData: any,
   prefsReady: boolean,
-  onBack?: () => void,
 };
 
 const channelsToSubscribe = AUTO_FOLLOW_CHANNELS.trim()
@@ -49,16 +48,6 @@ function UserChannelFollowIntro(props: Props) {
       )}
       actions={
         <React.Fragment>
-          {onBack && (
-            <div className="section__actions--between">
-              <Button button="secondary" onClick={onBack} label={__('Back')} />
-              <Button
-                button={subscribedChannels.length < 1 ? 'alt' : 'primary'}
-                onClick={onContinue}
-                label={subscribedChannels.length < 1 ? __('Skip') : __('Continue')}
-              />
-            </div>
-          )}
           <div className="section__body">
             <ClaimListDiscover
               hideFilters
