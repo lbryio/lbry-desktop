e8ddb148cb73c701634ed6c520ae8b66bd31d108
drewhancock <80921344+drewhancock@users.noreply.github.com>
2021-05-21 10:28:23 -0500

    changed pins
    removed heads of tech
    added crackermilk

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -242,7 +242,7 @@ function ClaimTilesDiscover(props: Props) {
   const shouldPerformSearch = !isLoading && uris.length === 0;
 
   const fixUris = [
-    'lbry://@headsoftech#5/hx-phantom-2.0-self-balancing-hoverboard#d',
+    'lbry://@CrackerMilk#6/the-campfire-experience#0',
   ];
   if (pin && uris && uris.length > 2 && window.location.pathname === '/') {
     fixUris.forEach((fixUri) => {
