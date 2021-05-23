2b075395188777afe86f4515ad38770834273be2
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2021-05-22 23:54:54 -0400

    Update view.jsx
    Force rebuild

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -37,7 +37,7 @@ export function prioritizeActiveLivestreams(
     //    for that channel actually point to the same source).
     // 2. 'liveChannelIds' needs to be pruned after being accounted for,
     //    otherwise all livestream-claims will be "live" (we'll only take the
-    //    latest one as "live").
+    //    latest one as "live" ).
     return (
       claim &&
       claim.value_type === 'stream' &&
