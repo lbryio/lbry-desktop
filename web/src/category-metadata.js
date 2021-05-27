fd2f664870d4985365fe662cff5013aaa097f2f0
Thomas Zarebczan <thomas.zarebczan@gmail.com>
2021-05-27 17:49:50 -0400

    last fix for metadata

--- web/src/category-metadata.js
+++ web/src/category-metadata.js
@@ -26,12 +26,12 @@ module.exports.CATEGORY_METADATA = {
   [PAGES.GAMING]: {
     title: 'Gaming',
     description: 'Pew pew bzzz gaming on Odysee',
-    image: 'https://spee.ch/category-gaming:5.jpg?quality=80&height=1200&width=627',
+    image: 'https://spee.ch/category-gaming:5.jpg?quality=80&height=1200&width=630',
   },
   [PAGES.GENERAL]: {
     title: 'Cheese',
     description: 'Cheese is the answer to life, the universe, and everything. We have primo cheese on Odysee',
-    image: 'https://spee.ch/category-primary:5.jpg?quality=80&height=1200&width=627',
+    image: 'https://spee.ch/category-primary1:5.jpg?quality=80&height=1200&width=630',
   },
   [PAGES.LAB]: {
     title: 'Lab',
@@ -46,13 +46,13 @@ module.exports.CATEGORY_METADATA = {
   [PAGES.MOVIES]: {
     title: 'Movies',
     description: `Do you love B rated movies? We've got you covered on Odysee`,
-    image: 'https://spee.ch/category-movies:2.jpg?quality=80&height=1200&width=627',
+    image: 'https://spee.ch/category-movies:2.jpg?quality=80&height=1200&width=630',
   },
-  [PAGES.MUSIC]: {
-    title: 'Music',
-    description: 'All the music you love on Odysee',
-    image: 'https://spee.ch/category-music:8.jpg?quality=80&height=1200&width=627',
-  },
+  // [PAGES.MUSIC]: {
+  //   title: 'Music',
+  //   description: 'All the music you love on Odysee',
+  //   image: 'https://spee.ch/category-music:8.jpg?quality=80&height=1200&width=630',
+  // },
   [PAGES.TECHNOLOGY]: {
     title: 'Tech',
     description: 'Hardware, software, startups, photography on Odysee',
@@ -61,6 +61,6 @@ module.exports.CATEGORY_METADATA = {
   [PAGES.WILD_WEST]: {
     title: 'Wild West',
     description: 'Boosted by user credits, this is what the community promotes on Odysee',
-    image: 'https://spee.ch/category-wildwest:1.jpg?quality=80&height=1200&width=627',
+    image: 'https://spee.ch/category-wildwest:1.jpg?quality=80&height=1200&width=630',
   },
 };
