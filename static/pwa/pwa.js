304bb3a99971deb2b93a06f10a125c67503efa18
Anthony <contact@anthonymayfield.com>
2021-06-02 21:50:47 +0200

    add fetch script to same file

--- static/pwa/pwa.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('fetch', function(event) {
-  // console.log(`start server worker`); // service worker started
-});
--- static/pwa/serviceWorker.js
+++ static/pwa/serviceWorker.js
@@ -1,7 +1,11 @@
+// register service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./public/pwa/serviceWorker.js').then(function(reg) {
-    // console.log('Successfully registered service worker', reg);
+  navigator.serviceWorker.register('./serviceWorker.js').then(function(reg) {
+    // reg.scope must be '/' to allow '/' as start url
   }).catch(function(err) {
     // console.warn('Error whilst registering service worker', err);
   });
 }
+
+// used to fetch the manifest file
+self.addEventListener('fetch', () => {});
