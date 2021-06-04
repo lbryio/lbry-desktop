eb7d4e1eef93b0276d30b5a43ea6b6d0f2c2401d
zeppi <jessopb@gmail.com>
2021-06-04 15:22:40 -0400

    lint

--- web/static/pwa/serviceWorker.js
+++ web/static/pwa/serviceWorker.js
@@ -2,10 +2,10 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('./serviceWorker.js')
-    .then(function (reg) {
+    .then(function(reg) {
       // reg.scope must be '/' to allow '/' as start url
     })
-    .catch(function (err) {
+    .catch(function(err) {
       // console.warn('Error whilst registering service worker', err);
     });
 }
