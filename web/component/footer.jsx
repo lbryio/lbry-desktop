46d258c439b4e699be8768ba43734076b36a147b
infinite-persistence <inf.persistence@gmail.com>
2021-06-08 12:48:15 +0800

    Fix footer in master (patch)
    - fix invalid <li> nesting

--- web/component/footer.jsx
+++ web/component/footer.jsx
@@ -80,18 +80,20 @@ const sections = [
 export default function Footer() {
   return (
     <footer className="footer">
-      <ul className="navigation__tertiary footer__links bright ul--no-style">
+      <ul className="navigation__tertiary footer__links ul--no-style">
         {sections.map(({ name, links }) => {
           return (
             <li key={name} className="footer__section">
-              <div className="footer__section-title">{__(name)}</div>
-              {links.map(({ label, link, navigate }) => {
-                return (
-                  <li key={label}>
-                    <Button className="footer__link" label={__(label)} href={link} navigate={navigate} />
-                  </li>
-                );
-              })}
+              <ul className="ul--no-style">
+                <div className="footer__section-title">{__(name)}</div>
+                {links.map(({ label, link, navigate }) => {
+                  return (
+                    <li key={label}>
+                      <Button className="footer__link" label={__(label)} href={link} navigate={navigate} />
+                    </li>
+                  );
+                })}
+              </ul>
             </li>
           );
         })}
