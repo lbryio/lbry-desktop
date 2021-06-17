97e172780f117830a539bfea821254a1d0aa412d
Anthony <contact@anthonymayfield.com>
2021-06-17 20:48:38 +0200

    seems to be coming along

--- ui/page/settingsStripeCard/view.jsx
+++ ui/page/settingsStripeCard/view.jsx
@@ -56,7 +56,7 @@ class CardVerify extends React.Component {
     scriptLoading = true;
 
     const script = document.createElement('script');
-    script.src = 'https://checkout.stripe.com/checkout.js';
+    script.src = 'https://js.stripe.com/v3/';
     script.async = true;
 
     this.loadPromise = (() => {
@@ -110,15 +110,122 @@ class CardVerify extends React.Component {
   }
 
   onScriptLoaded = () => {
-    if (!CardVerify.stripeHandler) {
-      CardVerify.stripeHandler = StripeCheckout.configure({
-        key: 'pk_test_NoL1JWL7i1ipfhVId5KfDZgo',
-      });
+    // public key of the stripe account
+    var publicKey = 'pk_test_NoL1JWL7i1ipfhVId5KfDZgo';
 
-      if (this.hasPendingClick) {
-        this.showStripeDialog();
-      }
-    }
+    // client secret of the SetupIntent (don't share with anyone but customer)
+    var clientSecret = 'seti_1J06NBIrsVv9ySuhNe8kilMp_secret_JdN7X7QEudCP69ZpnL7njukN2ytXhlk';
+
+    // run after the dom is loaded
+    window.addEventListener("DOMContentLoaded", function() {
+      var stripeElements = function(publicKey, setupIntent) {
+        var stripe = Stripe(publicKey);
+        var elements = stripe.elements();
+
+        // Element styles
+        var style = {
+          base: {
+            fontSize: "16px",
+            color: "#32325d",
+            fontFamily:
+              "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif",
+            fontSmoothing: "antialiased",
+            "::placeholder": {
+              color: "rgba(0,0,0,0.4)"
+            }
+          }
+        };
+
+        var card = elements.create("card", { style: style });
+
+        card.mount("#card-element");
+
+        // Element focus ring
+        card.on("focus", function() {
+          var el = document.getElementById("card-element");
+          el.classList.add("focused");
+        });
+
+        card.on("blur", function() {
+          var el = document.getElementById("card-element");
+          el.classList.remove("focused");
+        });
+
+        // Handle payment submission when user clicks the pay button.
+        var button = document.getElementById("submit");
+        button.addEventListener("click", function(event) {
+          event.preventDefault();
+          changeLoadingState(true);
+          var email = document.getElementById("email").value;
+
+          stripe
+            .confirmCardSetup(clientSecret, {
+              payment_method: {
+                card: card,
+                billing_details: { email: email }
+              }
+            })
+            .then(function(result) {
+              if (result.error) {
+                changeLoadingState(false);
+                var displayError = document.getElementById("card-errors");
+                displayError.textContent = result.error.message;
+              } else {
+                // The PaymentMethod was successfully set up
+                orderComplete(stripe, clientSecret);
+              }
+            });
+        });
+      };
+
+      stripeElements(publicKey, clientSecret);
+
+      // Show a spinner on payment submission
+      var changeLoadingState = function(isLoading) {
+        if (isLoading) {
+          document.querySelector("button").disabled = true;
+          document.querySelector("#spinner").classList.remove("hidden");
+          document.querySelector("#button-text").classList.add("hidden");
+        } else {
+          document.querySelector("button").disabled = false;
+          document.querySelector("#spinner").classList.add("hidden");
+          document.querySelector("#button-text").classList.remove("hidden");
+        }
+      };
+
+      /* Shows a success / error message when the payment is complete */
+      var orderComplete = function(stripe, clientSecret) {
+        stripe.retrieveSetupIntent(clientSecret).then(function(result) {
+
+          console.log(result);
+
+          var setupIntent = result.setupIntent;
+          var setupIntentJson = JSON.stringify(setupIntent, null, 2);
+
+          document.querySelector(".sr-payment-form").classList.add("hidden");
+          document.querySelector(".sr-result").classList.remove("hidden");
+          document.querySelector("pre").textContent = setupIntentJson;
+          setTimeout(function() {
+            document.querySelector(".sr-result").classList.add("expand");
+          }, 200);
+
+          changeLoadingState(false);
+        });
+      };
+
+      // getPublicKey();
+    }, false);
+
+
+    // if (!CardVerify.stripeHandler) {
+    //   CardVerify.stripeHandler = StripeCheckout.configure({
+    //     key: 'pk_test_NoL1JWL7i1ipfhVId5KfDZgo',
+    //   });
+    //
+    //   if (this.hasPendingClick) {
+    //     this.showStripeDialog();
+    //   }
+    // }
   };
 
   onScriptError = (...args) => {
@@ -130,25 +237,25 @@ class CardVerify extends React.Component {
   };
 
   updateStripeHandler() {
-    if (!CardVerify.stripeHandler) {
-      CardVerify.stripeHandler = StripeCheckout.configure({
-        key: this.props.stripeKey,
-      });
-    }
+    // if (!CardVerify.stripeHandler) {
+    //   CardVerify.stripeHandler = StripeCheckout.configure({
+    //     key: this.props.stripeKey,
+    //   });
+    // }
   }
 
   showStripeDialog() {
     this.setState({ open: true });
-    CardVerify.stripeHandler.open({
-      allowRememberMe: false,
-      closed: this.onClosed,
-      description: __('Confirm Identity'),
-      email: this.props.email,
-      locale: 'auto',
-      panelLabel: 'Verify',
-      token: this.props.token,
-      zipCode: true,
-    });
+    // CardVerify.stripeHandler.open({
+    //   allowRememberMe: false,
+    //   closed: this.onClosed,
+    //   description: __('Confirm Identity'),
+    //   email: this.props.email,
+    //   locale: 'auto',
+    //   panelLabel: 'Verify',
+    //   token: this.props.token,
+    //   zipCode: true,
+    // });
   }
 
   onClick = () => {
@@ -179,6 +286,39 @@ class CardVerify extends React.Component {
           title={__('App notifications')}
           subtitle={__('Notification settings for the desktop app.')}
         />
+
+        <div className="sr-root">
+          <div className="sr-main">
+            <div className="sr-payment-form card">
+              <div className="sr-form-row">
+                <label>
+                  Account details
+                </label>
+                <input type="text" id="email" placeholder="Email address" />
+              </div>
+
+              <div className="sr-form-row">
+                <label>
+                  Payment details
+                </label>
+                <div className="sr-input sr-element sr-card-element" id="card-element">
+                </div>
+              </div>
+              <div className="sr-field-error" id="card-errors" role="alert"></div>
+              <button id="submit">
+                <div className="spinner hidden" id="spinner"></div>
+                <span id="button-text">Link your card to your account</span>
+              </button>
+            </div>
+            <div className="sr-result hidden">
+              <p>Card setup completed<br /></p>
+              <pre>
+            <code></code>
+          </pre>
+            </div>
+          </div>
+        </div>
+
       </Page>
     );
   }
