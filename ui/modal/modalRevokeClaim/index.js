2594d1036b25c6a3e44fa17e9212e47deb6440af
saltrafael <a657ee92-7eb9-46eb-97d2-6735799df838@aleeas.com>
2021-06-17 14:57:04 -0300

    Fix repost deletion

--- ui/modal/modalRevokeClaim/index.js
+++ ui/modal/modalRevokeClaim/index.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { doHideModal } from 'redux/actions/app';
-import { doAbandonTxo, doAbandonClaim, selectTransactionItems } from 'lbry-redux';
+import { doAbandonTxo, doAbandonClaim, selectTransactionItems, doResolveUri } from 'lbry-redux';
 import { doToast } from 'redux/actions/notifications';
 import ModalRevokeClaim from './view';
 
@@ -13,6 +13,7 @@ const perform = dispatch => ({
   closeModal: () => dispatch(doHideModal()),
   abandonTxo: (txo, cb) => dispatch(doAbandonTxo(txo, cb)),
   abandonClaim: (txid, nout, cb) => dispatch(doAbandonClaim(txid, nout, cb)),
+  doResolveUri: (uri) => dispatch(doResolveUri(uri)),
 });
 
 export default connect(select, perform)(ModalRevokeClaim);
