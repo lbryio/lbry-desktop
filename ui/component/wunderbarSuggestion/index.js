8e17546933e5b9db102d49c4c0399d7a86677f71
infinite-persistence <inf.persistence@gmail.com>
2021-06-23 14:44:10 +0800

    Wunderbar: immediate feedback to convey status
    Make immediate GUI feedback to convey the current status, which can be the following:
    - typing
    - waiting lighthouse results
    - waiting claim resolve
    - no results or failed.

--- ui/component/wunderbarSuggestion/index.js
+++ ui/component/wunderbarSuggestion/index.js
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
-import { makeSelectClaimForUri } from 'lbry-redux';
+import { makeSelectClaimForUri, makeSelectIsUriResolving } from 'lbry-redux';
 import WunderbarSuggestion from './view';
 
 const select = (state, props) => ({
   claim: makeSelectClaimForUri(props.uri)(state),
+  isResolvingUri: makeSelectIsUriResolving(props.uri)(state),
 });
 
 export default connect(select)(WunderbarSuggestion);
