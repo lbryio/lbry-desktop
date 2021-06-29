5096a0baa7a04d06ee1d654c6caba3356d0d5c0f
zeppi <jessopb@gmail.com>
2021-06-29 13:02:10 -0400

    summit pin

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -286,9 +286,11 @@ function ClaimTilesDiscover(props: Props) {
 
   const modifiedUris = uris ? uris.slice() : [];
   const fixUris = [
+    'lbry://@JungleSurvival#f/incredible-secret-millionaire#a',
+    'lbry://@JamesJulierArt#0/ipad-painting-tutorial-lava-flow-art-in#8',
     'lbry://@summitforwellness#9/trail-run-to-cherry-creek-falls-(trying#3',
                   ];
-                   
+
   if (pin && modifiedUris && modifiedUris.length > 2 && window.location.pathname === '/') {
     fixUris.forEach((fixUri) => {
       if (modifiedUris.indexOf(fixUri) !== -1) {
