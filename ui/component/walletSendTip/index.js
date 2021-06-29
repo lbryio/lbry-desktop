322b16cd745c259cd54aebdf096674a1a736ffeb
Anthony <contact@anthonymayfield.com>
2021-06-30 00:20:26 +0200

    sending tip with frontend

--- ui/component/walletSendTip/index.js
+++ ui/component/walletSendTip/index.js
@@ -14,6 +14,7 @@ import { doOpenModal, doHideModal } from 'redux/actions/app';
 import { withRouter } from 'react-router';
 import { makeSelectClientSetting } from 'redux/selectors/settings';
 import { selectActiveChannelClaim, selectIncognito } from 'redux/selectors/app';
+import { doToast } from 'redux/actions/notifications';
 
 const select = (state, props) => ({
   isPending: selectIsSendingSupport(state),
@@ -32,6 +33,7 @@ const perform = dispatch => ({
   openModal: (modal, props) => dispatch(doOpenModal(modal, props)),
   closeModal: () => dispatch(doHideModal()),
   sendSupport: (params, isSupport) => dispatch(doSendTip(params, isSupport)),
+  doToast: (options) => dispatch(doToast(options)),
 });
 
 export default withRouter(connect(select, perform)(WalletSendTip));
