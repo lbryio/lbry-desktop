4a266c0b8ab8494f63b4550b8ce9bb89a301744e
drewhancock <80921344+drewhancock@users.noreply.github.com>
2021-06-30 17:31:41 -0500

    pinned samtime

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -286,7 +286,7 @@ function ClaimTilesDiscover(props: Props) {
 
   const modifiedUris = uris ? uris.slice() : [];
   const fixUris = [
-    'lbry://@passionforfood#3/making-chutney-with-dried-fruit-mrs#2',
+    'lbry://@samtime#1/how-to-make-your-own-netflix#0',
                   ];
 
   if (pin && modifiedUris && modifiedUris.length > 2 && window.location.pathname === '/') {
