1446ae2a2962197164385b02cc7188dfb0371b5b
drewhancock <80921344+drewhancock@users.noreply.github.com>
2021-07-01 21:03:20 -0500

    change pins

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -286,7 +286,7 @@ function ClaimTilesDiscover(props: Props) {
 
   const modifiedUris = uris ? uris.slice() : [];
   const fixUris = [
-    'lbry://@kauffj#f/finkelsteininterview#3',
+    'lbry://@TheFront#e/which-countries-had-most-brutal-basic#a',
                   ];
 
   if (pin && modifiedUris && modifiedUris.length > 2 && window.location.pathname === '/') {
