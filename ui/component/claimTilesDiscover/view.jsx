619e632c5b32a5e986ac46dc6572cb0908a2231a
drewhancock <80921344+drewhancock@users.noreply.github.com>
2021-07-08 12:01:30 -0500

    changed pin

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -286,7 +286,7 @@ function ClaimTilesDiscover(props: Props) {
 
   const modifiedUris = uris ? uris.slice() : [];
   const fixUris = [
-    'lbry://@Odysee#8/social-media-pre-bans#d',
+    'lbry://@SomeOrdinaryGamers#a/the-nintendo-switch-oled-is#9',
                   ];
 
   if (pin && modifiedUris && modifiedUris.length > 2 && window.location.pathname === '/') {
