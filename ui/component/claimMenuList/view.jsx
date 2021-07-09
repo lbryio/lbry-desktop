cd9337f2d4af730185c8afb8677ac51663ace9fe
infinite-persistence <inf.persistence@gmail.com>
2021-07-09 17:42:14 +0800

    Skip RSS URL generation if it's not a channel.

--- ui/component/claimMenuList/view.jsx
+++ ui/component/claimMenuList/view.jsx
@@ -103,7 +103,7 @@ function ClaimMenuList(props: Props) {
   }
 
   const shareUrl: string = generateShareUrl(SHARE_DOMAIN, uri);
-  const rssUrl: string = generateRssUrl(URL, uri);
+  const rssUrl: string = isChannel ? generateRssUrl(URL, uri) : '';
   const isCollectionClaim = claim && claim.value_type === 'collection';
   // $FlowFixMe
   const isPlayable =
