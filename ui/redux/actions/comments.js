106d17887bd446155781947f4bb6181c8d4eb3c1
Anthony <contact@anthonymayfield.com>
2021-07-15 20:51:10 +0200

    working with hardcoded payment intent id

--- ui/redux/actions/comments.js
+++ ui/redux/actions/comments.js
@@ -370,16 +370,39 @@ export function doCommentReact(commentId: string, type: string) {
   };
 }
 
+/**
+ *
+ * @param comment
+ * @param claim_id - File claim id
+ * @param parent_id - What is this?
+ * @param uri
+ * @param livestream
+ * @param {string} [txid] Optional transaction id
+ * @param {string} [payment_intent_id] Optional transaction id
+ * @param {string} [environment] Optional environment for Stripe (test|live)
+ * @returns {(function(Dispatch, GetState): Promise<undefined|void|*>)|*}
+ */
 export function doCommentCreate(
   comment: string = '',
   claim_id: string = '',
   parent_id?: string,
   uri: string,
   livestream?: boolean = false,
-  txid?: string
+  txid?: string,
+  payment_intent_id?: string,
+  environment?: string,
 ) {
   return async (dispatch: Dispatch, getState: GetState) => {
     const state = getState();
+
+    console.log('comment claim_id parent_id uri livestream txid payment_intent_id environment');
+    console.log(comment, claim_id, parent_id, uri, livestream, txid, payment_intent_id, environment);
+
+    // payment_intent_id = 'pi_1JDDZUIrsVv9ySuhbVmQvXs2'
+    //
+    // environment = 'test'
+
+    // get active channel that will receive comment and optional tip
     const activeChannelClaim = selectActiveChannelClaim(state);
 
     if (!activeChannelClaim) {
@@ -401,6 +424,7 @@ export function doCommentCreate(
       } catch (e) {}
     }
 
+    // send a notification
     if (parent_id) {
       const notification = makeSelectNotificationForCommentId(parent_id)(state);
       if (notification && !notification.is_seen) {
@@ -412,6 +436,8 @@ export function doCommentCreate(
       return dispatch(doToast({ isError: true, message: __('Unable to verify your channel. Please try again.') }));
     }
 
+    // Comments is a function which helps make calls to the backend
+    // these params passed in POST call.
     return Comments.comment_create({
       comment: comment,
       claim_id: claim_id,
@@ -420,9 +446,12 @@ export function doCommentCreate(
       parent_id: parent_id,
       signature: signatureData.signature,
       signing_ts: signatureData.signing_ts,
-      ...(txid ? { support_tx_id: txid } : {}),
+      ...(txid ? { support_tx_id: txid } : {}), // add transaction id if it exists
+      ...(payment_intent_id ? { payment_intent_id } : {}), // add payment_intent_id if it exists
+      ...(environment ? { environment } : {}), // add environment for stripe if it exists
     })
       .then((result: CommentCreateResponse) => {
+        console.log(result);
         dispatch({
           type: ACTIONS.COMMENT_CREATE_COMPLETED,
           data: {
@@ -435,6 +464,7 @@ export function doCommentCreate(
         return result;
       })
       .catch((error) => {
+        console.log(error);
         dispatch({
           type: ACTIONS.COMMENT_CREATE_FAILED,
           data: error,
