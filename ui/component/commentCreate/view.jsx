8fcf0e87c3bccaf7426704dd2111f4e48a810099
Anthony <contact@anthonymayfield.com>
2021-07-15 23:41:16 +0200

    bugfixed

--- ui/component/commentCreate/view.jsx
+++ ui/component/commentCreate/view.jsx
@@ -203,6 +203,12 @@ export function CommentCreate(props: Props) {
           const paymentIntendId = customerTipResponse.payment_intent_id;
 
           handleCreateComment(null, paymentIntendId, stripeEnvironment);
+
+          setCommentValue('');
+          setIsReviewingSupportComment(false);
+          setIsSupportComment(false);
+          setCommentFailure(false);
+          setIsSubmitting(false);
           // handleCreateComment(null);
         })
         .catch(function(error) {
@@ -214,21 +220,6 @@ export function CommentCreate(props: Props) {
 
           // doToast({ message: displayError, isError: true });
         });
-
-      const paymentIntentId = 'pi_1JDDZUIrsVv9ySuhbVmQvXs2';
-
-      handleCreateComment(null, paymentIntentId, stripeEnvironment);
-
-      // TODO: get transaction id, blah blah
-      // hit create comment with fiat amount etc
-
-      setCommentValue('');
-      setIsReviewingSupportComment(false);
-      setIsSupportComment(false);
-      setCommentFailure(false);
-      setIsSubmitting(false);
-
-      // alert('Sorry fiat tip is not implemented yet!');
     }
   }
 
