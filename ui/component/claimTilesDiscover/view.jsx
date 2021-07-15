7e6c772eb247f23f99162936bdd503b1d7cd7579
drewhancock <80921344+drewhancock@users.noreply.github.com>
2021-07-15 12:00:33 -0500

    cherrypick pins

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -292,7 +292,7 @@ function ClaimTilesDiscover(props: Props) {
 
   const modifiedUris = uris ? uris.slice() : [];
   const fixUris = pinUrls || [
-    'lbry://@EmperorLemon#e/sacrifices-to-the-church-of-nintendo#0'
+    'lbry://@EmperorLemon#e/this-is-what-true-fear-looks-like.#c', 'lbry://@AlisonMorrow#6/LBRY#8'
   ];
 
   if (pin && modifiedUris && modifiedUris.length > 2 && window.location.pathname === '/') {
@@ -310,13 +310,14 @@ function ClaimTilesDiscover(props: Props) {
     <ul className="claim-grid">
       {modifiedUris && modifiedUris.length
         ? modifiedUris.map((uri, index) => (
-          <ClaimPreviewTile
-            showNoSourceClaims={hasNoSource || showNoSourceClaims}
-            key={uri}
-            uri={uri}
-            properties={renderProperties}
-            live={resolveLive(index)}
-          />          ))
+            <ClaimPreviewTile
+              showNoSourceClaims={hasNoSource || showNoSourceClaims}
+              key={uri}
+              uri={uri}
+              properties={renderProperties}
+              live={resolveLive(index)}
+            />
+          ))
         : new Array(pageSize)
             .fill(1)
             .map((x, i) => (
