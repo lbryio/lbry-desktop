40948a7c5693948c6bc9e450a7b1ad07350f3d6b
saltrafael <a657ee92-7eb9-46eb-97d2-6735799df838@aleeas.com>
2021-07-16 13:38:10 -0300

    Fix Channel List being called every setting change

--- ui/component/syncPassword/index.js
+++ ui/component/syncPassword/index.js
@@ -15,7 +15,7 @@ const select = state => ({
 const perform = dispatch => ({
   getSync: (cb, password) => dispatch(doGetSyncDesktop(cb, password)),
   setClientSetting: (key, value) => dispatch(doSetClientSetting(key, value)),
-  handleSyncComplete: (error, hasDataChanged) => dispatch(doHandleSyncComplete(error, hasDataChanged)),
+  handleSyncComplete: (error) => dispatch(doHandleSyncComplete(error)),
   signOut: () => dispatch(doSignOut()),
 });
 
