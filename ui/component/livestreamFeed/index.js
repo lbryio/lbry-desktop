ff61303d4f8dcb603b297f72dd7b8ec1fa225b3c
zeppi <jessopb@gmail.com>
2021-07-18 12:39:19 -0400

    delete stuff

--- ui/component/livestreamFeed/index.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { makeSelectClaimForUri } from 'lbry-redux';
-import LivestreamFeed from './view';
-
-const select = (state, props) => ({
-  claim: makeSelectClaimForUri(props.uri)(state),
-});
-
-export default connect(select)(LivestreamFeed);
