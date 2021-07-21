ee1435f871a9c8c5f7d4a90a56d290f0220b8ed3
drewhancock <80921344+drewhancock@users.noreply.github.com>
2021-07-21 15:51:40 -0500

    chris williamson

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -291,7 +291,7 @@ function ClaimTilesDiscover(props: Props) {
   };
 
   const modifiedUris = uris ? uris.slice() : [];
-  const fixUris = pinUrls || ['lbry://@Odysee#8/Does-Google-Own-Odysee#4'];
+  const fixUris = pinUrls || ['lbry://@ChrisWilliamson#6/danny-trejo-recalls-being-hypnotised-by#7'];
 
   if (pin && modifiedUris && modifiedUris.length > 2 && window.location.pathname === '/') {
     fixUris.forEach((fixUri) => {
