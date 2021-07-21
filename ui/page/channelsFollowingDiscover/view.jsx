573ed1e88c84262cfa6ec5528806d09494ccc1de
zeppi <jessopb@gmail.com>
2021-07-21 16:42:15 -0400

    recon followDisc

--- ui/page/channelsFollowingDiscover/view.jsx
+++ ui/page/channelsFollowingDiscover/view.jsx
@@ -8,7 +8,7 @@ import ClaimTilesDiscover from 'component/claimTilesDiscover';
 import ClaimListDiscover from 'component/claimListDiscover';
 import * as CS from 'constants/claim_search';
 import { toCapitalCase } from 'util/string';
-import { CUSTOM_HOMEPAGE } from 'config';
+import { CUSTOM_HOMEPAGE, SIMPLE_SITE } from 'config';
 
 const MORE_CHANNELS_ANCHOR = 'MoreChannels';
 
@@ -46,16 +46,6 @@ function ChannelsFollowingDiscover(props: Props) {
       pageSize: 12,
       claimType: 'channel',
       orderBy: ['effective_amount'],
-    },
-  });
-
-  rowData.push({
-    title: 'Latest From @lbrycast',
-    link: `/@lbrycast:4`,
-    options: {
-      orderBy: ['release_time'],
-      pageSize: 8,
-      channelIds: ['4c29f8b013adea4d5cca1861fb2161d5089613ea'],
     },
   });
 
@@ -105,29 +95,32 @@ function ChannelsFollowingDiscover(props: Props) {
 
   return (
     <Page>
-      {rowDataWithGenericOptions.map(({ title, link, help, options = {} }) => (
-        <div key={title} className="claim-grid__wrapper">
-          <h1 className="section__actions">
-            {link ? (
-              <Button
-                className="claim-grid__title"
-                button="link"
-                navigate={link}
-                iconRight={ICONS.ARROW_RIGHT}
-                label={__(title)}
-              />
-            ) : (
-              <span className="claim-grid__title">{__(title)}</span>
-            )}
-            {help}
-          </h1>
+      {!SIMPLE_SITE &&
+        rowDataWithGenericOptions.map(({ title, link, help, options = {} }) => (
+          <div key={title} className="claim-grid__wrapper">
+            <h1 className="section__actions">
+              {link ? (
+                <Button
+                  className="claim-grid__title"
+                  button="link"
+                  navigate={link}
+                  iconRight={ICONS.ARROW_RIGHT}
+                  label={__(title)}
+                />
+              ) : (
+                <span className="claim-grid__title">{__(title)}</span>
+              )}
+              {help}
+            </h1>
 
-          <ClaimTilesDiscover {...options} />
-        </div>
-      ))}
-      <h1 id={MORE_CHANNELS_ANCHOR} className="claim-grid__title">
-        {__('More Channels')}
-      </h1>
+            <ClaimTilesDiscover {...options} />
+          </div>
+        ))}
+      {!SIMPLE_SITE && (
+        <h1 id={MORE_CHANNELS_ANCHOR} className="claim-grid__title">
+          {__('More Channels')}
+        </h1>
+      )}
       <ClaimListDiscover
         defaultOrderBy={CS.ORDER_BY_TRENDING}
         defaultFreshness={CS.FRESH_ALL}
