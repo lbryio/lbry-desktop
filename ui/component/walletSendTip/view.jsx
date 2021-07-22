a9f0f385d5bd064ab9d3a28e178a710491eb0640
Anthony <contact@anthonymayfield.com>
2021-07-22 22:37:49 +0200

    fix frontend bug

--- ui/component/walletSendTip/view.jsx
+++ ui/component/walletSendTip/view.jsx
@@ -444,7 +444,7 @@ function WalletSendTip(props: Props) {
 
               {/* short explainer under the button */}
               <div className="section__subtitle">
-                {explainerText}
+                {explainerText + ' '}
                 {/* {activeTab === TAB_FIAT && !hasCardSaved && <Button navigate={`/$/${PAGES.SETTINGS_STRIPE_CARD}`} label={__('Add A Card')} button="link" />} */}
                 {<Button label={__('Learn more')} button="link" href="https://lbry.com/faq/tipping" />}
               </div>
