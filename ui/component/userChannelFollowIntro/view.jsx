038431f95cb1547935162109164699abc6f044b6
zeppi <jessopb@gmail.com>
2021-07-22 23:51:29 -0400

    channel follow first run on master

--- ui/component/userChannelFollowIntro/view.jsx
+++ ui/component/userChannelFollowIntro/view.jsx
@@ -6,12 +6,11 @@ import Nag from 'component/common/nag';
 import { parseURI } from 'lbry-redux';
 import Button from 'component/button';
 import Card from 'component/common/card';
-import { AUTO_FOLLOW_CHANNELS, CUSTOM_HOMEPAGE } from 'config';
+import { AUTO_FOLLOW_CHANNELS, CUSTOM_HOMEPAGE, SIMPLE_SITE, SITE_NAME } from 'config';
 
 type Props = {
   subscribedChannels: Array<Subscription>,
   onContinue: () => void,
-  onBack: () => void,
   channelSubscribe: (sub: Subscription) => void,
   homepageData: any,
   prefsReady: boolean,
@@ -22,7 +21,7 @@ const channelsToSubscribe = AUTO_FOLLOW_CHANNELS.trim()
   .filter((x) => x !== '');
 
 function UserChannelFollowIntro(props: Props) {
-  const { subscribedChannels, channelSubscribe, onContinue, onBack, homepageData, prefsReady } = props;
+  const { subscribedChannels, channelSubscribe, onContinue, homepageData, prefsReady } = props;
   const { PRIMARY_CONTENT } = homepageData;
   let channelIds;
   if (PRIMARY_CONTENT && CUSTOM_HOMEPAGE) {
@@ -49,25 +48,27 @@ function UserChannelFollowIntro(props: Props) {
     <Card
       title={__('Find channels to follow')}
       subtitle={__(
-        'LBRY works better if you find and follow a couple creators you like. You can also block channels you never want to see.'
+        '%SITE_NAME% works better if you find and follow a couple creators you like. You can also block channels you never want to see.',
+        { SITE_NAME }
       )}
       actions={
         <React.Fragment>
-          <div className="section__actions--between">
-            <Button button="secondary" onClick={onBack} label={__('Back')} />
-            <Button
-              button={subscribedChannels.length < 1 ? 'alt' : 'primary'}
-              onClick={onContinue}
-              label={subscribedChannels.length < 1 ? __('Skip') : __('Continue')}
-            />
-          </div>
           <div className="section__body">
             <ClaimListDiscover
-              defaultOrderBy={CS.ORDER_BY_TOP}
+              hideFilters={SIMPLE_SITE}
+              meta={
+                <Button
+                  button={subscribedChannels.length < 1 ? 'alt' : 'primary'}
+                  onClick={onContinue}
+                  label={subscribedChannels.length < 1 ? __('Skip') : __('Continue')}
+                />
+              }
+              defaultOrderBy={SIMPLE_SITE ? CS.ORDER_BY_TOP : CS.ORDER_BY_TRENDING}
               defaultFreshness={CS.FRESH_ALL}
               claimType="channel"
               claimIds={CUSTOM_HOMEPAGE && channelIds ? channelIds : undefined}
               defaultTags={followingCount > 3 ? CS.TAGS_FOLLOWED : undefined}
+              maxPages={SIMPLE_SITE ? 3 : undefined}
             />
             {followingCount > 0 && (
               <Nag
