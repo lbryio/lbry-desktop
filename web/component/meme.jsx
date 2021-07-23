71325aff0d7d3f93acb7c5014a3a12a1a9e35822
zeppi <jessopb@gmail.com>
2021-07-23 00:26:01 -0400

    meme setting
    bugfix
    
    bugfix
    
    fix
    
    meme fix
    
    gr

--- web/component/meme.jsx
+++ web/component/meme.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from 'component/button';
+const memes = require('memes');
 
 export default function Meme() {
   return (
     <h1 className="home__meme">
-      <Button button="link" href="https://odysee.com/@Odysee:8?view=discussion">
-        {`don't dead open inside`}
+      <Button button="link" href={memes.url}>
+        {memes.text}
       </Button>
     </h1>
   );
