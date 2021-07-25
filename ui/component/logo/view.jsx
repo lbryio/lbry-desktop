dcdd5b2d6ef18ef91e18fb863a97570389855eff
zeppi <jessopb@gmail.com>
2021-07-25 18:03:48 -0400

    logo component on embeds/embeds ended

--- ui/component/logo/view.jsx
+++ ui/component/logo/view.jsx
@@ -1,8 +1,9 @@
 // @flow
 import React from 'react';
 import * as ICONS from 'constants/icons';
-import { LOGO, LOGO_TEXT_LIGHT, LOGO_TEXT_DARK } from 'config';
+import { LOGO_TITLE, LOGO, LOGO_TEXT_LIGHT, LOGO_TEXT_DARK } from 'config';
 import Icon from 'component/common/icon';
+import { useIsMobile } from 'effects/use-screensize';
 
 type Props = {
   type: string,
@@ -11,19 +12,35 @@ type Props = {
 
 export default function Logo(props: Props) {
   const { type, currentTheme } = props;
-  if (type === 'small') {
-    return LOGO ? <img src={LOGO} /> : <Icon icon={ICONS.LBRY} />;
+  const isMobile = useIsMobile();
+  const defaultWithLabel = (
+    <>
+      <label>{LOGO_TITLE}</label>
+      <Icon icon={ICONS.LBRY} />
+    </>
+  );
+
+  if (type === 'small' || (isMobile && type !== 'embed')) {
+    return LOGO ? <img className="header__odysee" src={LOGO} /> : <Icon icon={ICONS.LBRY} />;
+  } else if (type === 'embed') {
+    if (LOGO_TEXT_LIGHT) {
+      return (
+        <>
+          <img src={LOGO_TEXT_LIGHT} className="header__odysee" />
+        </>
+      );
+    } else {
+      return defaultWithLabel;
+    }
   } else {
     if (LOGO_TEXT_LIGHT && LOGO_TEXT_DARK) {
       return (
         <>
-          {/*<img src={LOGO} className="mobile-only header__odysee" />*/}
           <img src={currentTheme === 'light' ? LOGO_TEXT_DARK : LOGO_TEXT_LIGHT} className="header__odysee" />
         </>
       );
     } else {
-      return <Icon icon={ICONS.LBRY} />;
+      return defaultWithLabel;
     }
   }
 }
-//mobile-hidden
