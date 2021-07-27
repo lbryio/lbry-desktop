148e6a0fa1060dc35c6fc3ad971f5bdaca07f34c
zeppi <jessopb@gmail.com>
2021-07-27 13:35:02 -0400

    livestream endpoint

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -1,3 +1,3 @@
 export const BITWAVE_EMBED_URL = 'https://bitwave.tv/odysee';
-export const BITWAVE_LIVE_API = 'https://api.bitwave.tv/v1/odysee/live';
-export const BITWAVE_REPLAY_API = 'https://api.bitwave.tv/v1/replays/odysee';
+export const BITWAVE_LIVE_API = 'https://api.live.odysee.com/v1/odysee/live';
+export const BITWAVE_REPLAY_API = 'https://api.live.odysee.com/v1/replays/odysee';
