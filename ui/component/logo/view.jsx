2a69c42e5b727ac758991e62e6df251227c693e8
zeppi <jessopb@gmail.com>
2021-07-27 15:11:33 -0400

    back to img tag

--- ui/component/logo/view.jsx
+++ ui/component/logo/view.jsx
@@ -4,7 +4,6 @@ import * as ICONS from 'constants/icons';
 import { LOGO_TITLE, LOGO, LOGO_TEXT_LIGHT, LOGO_TEXT_DARK } from 'config';
 import Icon from 'component/common/icon';
 import { useIsMobile } from 'effects/use-screensize';
-import OptimizedImage from 'component/optimizedImage';
 
 type Props = {
   type: string,
@@ -27,12 +26,12 @@ export default function Logo(props: Props) {
   );
 
   if (type === 'small' || (isMobile && type !== 'embed')) {
-    return LOGO ? <OptimizedImage src={LOGO} /> : <Icon icon={ICONS.LBRY} />;
+    return LOGO ? <img src={LOGO} /> : <Icon icon={ICONS.LBRY} />;
   } else if (type === 'embed') {
     if (LOGO_TEXT_LIGHT) {
       return (
         <>
-          <OptimizedImage src={LOGO_TEXT_LIGHT} />
+          <img src={LOGO_TEXT_LIGHT} />
         </>
       );
     } else {
@@ -42,7 +41,7 @@ export default function Logo(props: Props) {
     if (LOGO_TEXT_LIGHT && LOGO_TEXT_DARK) {
       return (
         <>
-          <OptimizedImage src={currentTheme === 'light' ? LOGO_TEXT_DARK : LOGO_TEXT_LIGHT} />
+          <img src={currentTheme === 'light' ? LOGO_TEXT_DARK : LOGO_TEXT_LIGHT} />
         </>
       );
     } else {
