4ff0a2f9162d3d37cb56b89315a4fcb7dc019024
infinite-persistence <inf.persistence@gmail.com>
2021-07-29 15:53:56 +0800

    Fix missing file-thumbnail in Notifications
    ## Issue
    6721 Notifications: Thumbnail for "new content" not showing up in Desktop layout
    
    ## Change
    The `className` wasn't propagated during the `FileThumbnail` refactoring to `Thumbs`.

--- ui/component/fileThumbnail/thumb.jsx
+++ ui/component/fileThumbnail/thumb.jsx
@@ -7,15 +7,16 @@ import useLazyLoading from 'effects/use-lazy-loading';
 type Props = {
   thumb: string,
   children?: Node,
+  className?: string,
 };
 
 const Thumb = (props: Props) => {
-  const { thumb, children } = props;
+  const { thumb, children, className } = props;
   const thumbnailRef = React.useRef(null);
   useLazyLoading(thumbnailRef);
 
   return (
-    <div ref={thumbnailRef} data-background-image={thumb} className={classnames('media__thumb')}>
+    <div ref={thumbnailRef} data-background-image={thumb} className={classnames('media__thumb', className)}>
       {children}
     </div>
   );
