dcac5ebcc98c2d7fb3d1e5257a7a6f30cc666b64
zeppi <jessopb@gmail.com>
2021-08-10 10:38:53 -0400

    fix collection preview links

--- ui/page/file/index.js
+++ ui/page/file/index.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { doSetContentHistoryItem, doSetPrimaryUri } from 'redux/actions/content';
-import { withRouter } from 'react-router';
+import { withRouter } from 'react-router-dom';
 import {
   doFetchFileInfo,
   makeSelectFileInfoForUri,
