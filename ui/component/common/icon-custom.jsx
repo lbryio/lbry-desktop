9e3424792a78a7a205c87fcd2727552242b06790
saltrafael <a657ee92-7eb9-46eb-97d2-6735799df838@aleeas.com>
2021-08-11 13:56:06 -0300

    Add Shuffle icon

--- ui/component/common/icon-custom.jsx
+++ ui/component/common/icon-custom.jsx
@@ -2447,4 +2447,13 @@ export const icons = {
       <path d="M21 13v2a4 4 0 0 1-4 4H3" />
     </g>
   ),
+  [ICONS.SHUFFLE]: buildIcon(
+    <g>
+      <polyline points="16 3 21 3 21 8" />
+      <line x1="4" y1="20" x2="21" y2="3" />
+      <polyline points="21 16 21 21 16 21" />
+      <line x1="15" y1="15" x2="21" y2="21" />
+      <line x1="4" y1="4" x2="9" y2="9" />
+    </g>
+  ),
 };
