cc75771d25f6fe6340eb37dd177c27d1703bcc1d
zeppi <jessopb@gmail.com>
2021-08-18 19:21:19 -0400

    remove commented

--- ui/component/viewers/videoViewer/internal/videojs.jsx
+++ ui/component/viewers/videoViewer/internal/videojs.jsx
@@ -664,20 +664,6 @@ export default React.memo<Props>(function VideoJs(props: Props) {
     });
   }, [source, reload]);
 
-  // // Load IMA3 SDK for aniview
-  // useEffect(() => {
-  //   const script = document.createElement('script');
-  //   script.src = `https://imasdk.googleapis.com/js/sdkloader/ima3.js`;
-  //   script.async = true;
-  //   // $FlowFixMe
-  //   document.body.appendChild(script);
-  //
-  //   return () => {
-  //     // $FlowFixMe
-  //     document.body.removeChild(script);
-  //   };
-  // });
-
   return (
     // $FlowFixMe
     <div className={classnames('video-js-parent', { 'video-js-parent--ios': IS_IOS })} ref={containerRef}>
