ee609c654f8bcc7d3b66450633fa55fbe81c280f
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2021-08-26 11:04:24 -0400

    Fix stream type for livestreams (#6967)
    * Fix stream type for livestreams
    
    Was picking up collections 
    
    + other tweaks
    
    * Update search.js

--- ui/component/claimTilesDiscover/view.jsx
+++ ui/component/claimTilesDiscover/view.jsx
@@ -203,6 +203,7 @@ function ClaimTilesDiscover(props: Props) {
 
   if (ENABLE_NO_SOURCE_CLAIMS && hasNoSource) {
     options.has_no_source = true;
+    options.claim_type = ['stream'];
   } else if (hasSource || (!ENABLE_NO_SOURCE_CLAIMS && (!claimType || claimType === 'stream'))) {
     options.has_source = true;
   }
