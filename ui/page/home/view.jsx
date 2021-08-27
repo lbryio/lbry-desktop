2aad18ecf8bbf935033e7e8045b3782ece4fe0b5
zeppi <jessopb@gmail.com>
2021-08-27 09:27:49 -0400

    removeNags

--- ui/page/home/view.jsx
+++ ui/page/home/view.jsx
@@ -1,15 +1,13 @@
 // @flow
 import * as ICONS from 'constants/icons';
 import * as PAGES from 'constants/pages';
-import { SITE_NAME, SIMPLE_SITE, DOMAIN, ENABLE_NO_SOURCE_CLAIMS } from 'config';
+import { SITE_NAME, SIMPLE_SITE, ENABLE_NO_SOURCE_CLAIMS } from 'config';
 import React from 'react';
 import Page from 'component/page';
 import Button from 'component/button';
 import ClaimTilesDiscover from 'component/claimTilesDiscover';
 import ClaimPreviewTile from 'component/claimPreviewTile';
 import Icon from 'component/common/icon';
-import I18nMessage from 'component/i18nMessage';
-import LbcSymbol from 'component/common/lbc-symbol';
 import WaitUntilOnPage from 'component/common/wait-until-on-page';
 import useGetLivestreams from 'effects/use-get-livestreams';
 import { GetLinksData } from 'util/buildHomepage';
@@ -99,34 +97,6 @@ function HomePage(props: Props) {
 
   return (
     <Page fullWidthPage>
-      {IS_WEB && DOMAIN === 'lbry.tv' && (
-        <div className="notice-message--loud">
-          <h1 className="section__title">
-            <I18nMessage
-              tokens={{
-                odysee: <Button label={__('odysee.com')} button="link" href="https://odysee.com?src=lbrytv-retired" />,
-              }}
-            >
-              lbry.tv is being retired in favor of %odysee%
-            </I18nMessage>
-          </h1>
-          <p className="section__subtitle">
-            <I18nMessage
-              tokens={{
-                desktop_app: (
-                  <Button label={__('desktop app')} button="link" href="https://lbry.com/get?src=lbrytv-retired" />
-                ),
-                odysee: <Button label={__('odysee.com')} button="link" href="https://odysee.com?src=lbrytv-retired" />,
-                credits: <LbcSymbol />,
-              }}
-            >
-              You will have to switch to the %desktop_app% or %odysee% in the near future. Your existing login details
-              will work on %odysee% and all of your %credits% and other settings will be there.
-            </I18nMessage>
-          </p>
-        </div>
-      )}
-
       {!SIMPLE_SITE && (authenticated || !IS_WEB) && !subscribedChannels.length && (
         <div className="notice-message">
           <h1 className="section__title">
