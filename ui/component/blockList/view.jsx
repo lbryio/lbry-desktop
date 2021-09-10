7afc482ed1ec8ebacac2e87ae381aca4b97bc62a
infinite-persistence <inf.persistence@gmail.com>
2021-09-10 16:21:39 +0800

    Add search bar to BlockList
    6834
    
    - Only supports channel-name search, per 6834. Channel-title search would probably be too heavy on the client side.
    - Fuzzy search is possible, but is too slow on huge lists. Ended up with a simpler `matchSorter.rankings.CONTAINS`, which I think would cover typical cases.

--- ui/component/blockList/view.jsx
+++ ui/component/blockList/view.jsx
@@ -1,12 +1,33 @@
 // @flow
+import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from '@reach/combobox';
+// import '@reach/combobox/styles.css'; --> 'scss/third-party.scss'
+import { matchSorter } from 'match-sorter';
 import React from 'react';
 import classnames from 'classnames';
 import Button from 'component/button';
 import ClaimList from 'component/claimList';
+import Icon from 'component/common/icon';
 import Paginate from 'component/common/paginate';
 import Yrbl from 'component/yrbl';
+import * as ICONS from 'constants/icons';
+import useThrottle from 'effects/use-throttle';
 
 const PAGE_SIZE = 10;
+
+function reduceUriToChannelName(uri: string) {
+  // 'parseURI' is too slow to handle a large list. Since our list should be
+  // kosher in the first place, just do a quick substring call. Add a
+  // try-catch just in case.
+  try {
+    return uri.substring(uri.indexOf('@') + 1, uri.indexOf('#'));
+  } catch {
+    return uri;
+  }
+}
+
+// ****************************************************************************
+// BlockList
+// ****************************************************************************
 
 type Props = {
   uris: Array<string>,
@@ -26,6 +47,7 @@ export default function BlockList(props: Props) {
   const hasLocalList = localList && localList.length > 0;
   const justBlocked = list && localList && localList.length < list.length;
 
+  const [searchList, setSearchList] = React.useState(null); // null: not searching; []: no results;
   const [page, setPage] = React.useState(1);
 
   let totalPages = 0;
@@ -43,6 +65,14 @@ export default function BlockList(props: Props) {
       return (claim) => <div className="section__actions">{getActionButtons(claim.permanent_url)}</div>;
     }
     return undefined;
+  }
+
+  function formatSearchSuggestion(suggestion: string) {
+    return reduceUriToChannelName(suggestion);
+  }
+
+  function filterSearchResults(results: ?Array<string>) {
+    setSearchList(results);
   }
 
   // **************************************************************************
@@ -87,9 +117,17 @@ export default function BlockList(props: Props) {
   return (
     <>
       <div className="help--notice">{help}</div>
-      <div className={classnames('block-list', className)}>
+      <div className="section">
+        <SearchList
+          list={localList}
+          placeholder={__('e.g. odysee')}
+          formatter={formatSearchSuggestion}
+          onResultsUpdated={filterSearchResults}
+        />
+      </div>
+      <div className={classnames('section block-list', className)}>
         <ClaimList
-          uris={paginatedLocalList}
+          uris={searchList || paginatedLocalList}
           showUnresolvedClaims
           showHiddenByUser
           hideMenu
@@ -99,4 +137,68 @@ export default function BlockList(props: Props) {
       <Paginate totalPages={totalPages} disableHistory onPageChange={(p) => setPage(p)} />
     </>
   );
+}
+
+// ****************************************************************************
+// SearchList
+// ****************************************************************************
+
+type LsbProps = {
+  list: ?Array<string>,
+  placeholder?: string,
+  formatter?: (suggestion: string) => string,
+  onResultsUpdated?: (?Array<string>) => void,
+};
+
+function SearchList(props: LsbProps) {
+  const { list, placeholder, formatter, onResultsUpdated } = props;
+  const [term, setTerm] = React.useState('');
+  const results = useAuthorMatch(term, list);
+  const handleChange = (event) => setTerm(event.target.value);
+  const handleSelect = (e) => setTerm(e);
+
+  React.useEffect(() => {
+    if (onResultsUpdated) {
+      onResultsUpdated(results);
+    }
+  }, [results]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div className="wunderbar__wrapper">
+      <label>{__('Search blocked channel name')}</label>
+      <Combobox className="wunderbar" onSelect={handleSelect}>
+        <Icon icon={ICONS.SEARCH} />
+        <ComboboxInput selectOnClick className="wunderbar__input" onChange={handleChange} placeholder={placeholder} />
+        {results && (
+          <ComboboxPopover className="wunderbar__suggestions" portal={false}>
+            {results.length > 0 ? (
+              <ComboboxList>
+                {results.slice(0, 10).map((result, index) => (
+                  <ComboboxOption
+                    className="wunderbar__more-results"
+                    key={index}
+                    value={formatter ? formatter(result) : result}
+                  />
+                ))}
+              </ComboboxList>
+            ) : (
+              <span style={{ display: 'block', margin: 8 }}>{__('No results found')}</span>
+            )}
+          </ComboboxPopover>
+        )}
+      </Combobox>
+    </div>
+  );
+}
+
+function useAuthorMatch(term, list) {
+  const throttledTerm = useThrottle(term, 200);
+  return React.useMemo(() => {
+    return !throttledTerm || throttledTerm.trim() === ''
+      ? null
+      : matchSorter(list, throttledTerm, {
+          keys: [(item) => reduceUriToChannelName(item)],
+          threshold: matchSorter.rankings.CONTAINS,
+        });
+  }, [throttledTerm]); // eslint-disable-line react-hooks/exhaustive-deps
 }
