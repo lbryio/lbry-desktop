e9687b8a089ddcc67a01ff764dd6ce3bd6869ecb
zeppi <jessopb@gmail.com>
2021-09-13 12:44:58 -0400

    fix recommended follows

--- ui/component/userChannelFollowIntro/view.jsx
+++ ui/component/userChannelFollowIntro/view.jsx
@@ -25,7 +25,7 @@ function UserChannelFollowIntro(props: Props) {
   const { PRIMARY_CONTENT } = homepageData;
   let channelIds;
   if (PRIMARY_CONTENT && CUSTOM_HOMEPAGE) {
-    channelIds = PRIMARY_CONTENT.channelIds;
+    channelIds = PRIMARY_CONTENT.ids;
   }
   const followingCount = (subscribedChannels && subscribedChannels.length) || 0;
   const followingCountIgnoringAutoFollows = (subscribedChannels || []).filter(
