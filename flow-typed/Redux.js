d62f63aff882115215309d7ce0e84628afc0afb6
Dan Peterson <dan@dan-peterson.ca>
2021-10-20 13:33:31 -0500

    delete duplicate flow type files (#105)
    * delete duplicate flow type files
    
    * merge types from deleted files
    
    * revert dispatch to type any. (linting issues)

--- flow-typed/Redux.js
+++
@@ -1,6 +0,0 @@
-// @flow
-/* eslint-disable no-use-before-define */
-declare type GetState = () => any;
-declare type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
-declare type Dispatch = (action: {} | Promise<*> | Array<{}> | ThunkAction) => any; // Need to refer to ThunkAction
-/* eslint-enable */
