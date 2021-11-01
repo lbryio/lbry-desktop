fa029e0c094752805c87c26a1a2b7f0d7073bce8
jessopb <36554050+jessopb@users.noreply.github.com>
2021-11-01 09:58:28 -0400

    channel parsing bugfix (#199)

--- ui/util/remark-lbry.js
+++ ui/util/remark-lbry.js
@@ -9,7 +9,7 @@ const mentionToken = '@';
 // const mentionTokenCode = 64; // @
 
 const invalidRegex = /[-_.+=?!@#$%^&*:;,{}<>\w/\\]/;
-const mentionRegex = /@[^\s()"-_.+=?!@$%^&*;,{}<>/\\]*/gm;
+const mentionRegex = /@[^\s()"_.+=?!@$%^&*;,{}<>/\\]*/gm;
 
 function handlePunctuation(value) {
   const protocolIndex = value.indexOf('lbry://') === 0 ? protocol.length - 1 : 0;
