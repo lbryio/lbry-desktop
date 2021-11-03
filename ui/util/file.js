9d25d82bed1c3dc179e9b58129f40e44db32115e
infinite-persistence <inf.persistence@gmail.com>
2021-11-03 14:54:32 +0800

    Exclude "modified date" for Firefox/Android
    ## Issue
    It appears that the modification date of the Android file changes when selected, so that file was deemed "different" when trying to resume upload.
    
    ## Change
    Exclude modification date for now. Let's assume a smart user.

--- ui/util/file.js
+++ ui/util/file.js
@@ -1,8 +1,5 @@
 // @flow
 
 export function serializeFileObj(file: File) {
-  // $FlowFixMe - these are non standard, but try to include anyway.
-  const aux = `${String(file.lastModifiedDate)}#${String(file.webkitRelativePath)}`;
-
-  return `${file.name}#${file.type}#${file.size}#${file.lastModified}#${aux}`;
+  return `${file.name}#${file.type}#${file.size}`;
 }
