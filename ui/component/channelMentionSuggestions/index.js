e35069de1cbde404f5a3ccd3fa77734bf7b058e3
infinite-persistence <64950861+infinite-persistence@users.noreply.github.com>
2021-11-17 08:47:56 -0800

    Cache the processing of ChannelMentionSuggestions (#309)
    ## Issue
    One of the bottlenecks of livestream page.
    
    The component probably needs a re-design:
    - Don't perpetually mount -- only mount when activated by the user through "@". This would avoid the heavy processing entirely.
    - Better way of resolving uris (too many arrays, too many loops).
    - Tom also mentioned that we should not be resolving every commenter as we see encounter them in a livestream. This is currently the case because the component is always mounted.
    
    ## Changes
    Until the re-design occurs, attempt to cache the heavy processing. Also, trimmed down the amount of loops.

--- ui/component/channelMentionSuggestions/index.js
+++ ui/component/channelMentionSuggestions/index.js
@@ -3,38 +3,23 @@ import { MAX_LIVESTREAM_COMMENTS } from 'constants/livestream';
 import { selectShowMatureContent } from 'redux/selectors/settings';
 import { selectSubscriptionUris } from 'redux/selectors/subscriptions';
 import { withRouter } from 'react-router';
-import { makeSelectClaimForUri } from 'redux/selectors/claims';
+import { selectCanonicalUrlForUri } from 'redux/selectors/claims';
 import { doResolveUris } from 'redux/actions/claims';
-import { selectTopLevelCommentsForUri } from 'redux/selectors/comments';
+import { selectChannelMentionData } from 'redux/selectors/livestream';
 import ChannelMentionSuggestions from './view';
 
 const select = (state, props) => {
-  const subscriptionUris = selectSubscriptionUris(state);
-  const topLevelComments = selectTopLevelCommentsForUri(
-    state,
-    props.uri,
-    props.isLivestream ? MAX_LIVESTREAM_COMMENTS : -1
-  );
-
-  const commentorUris = [];
-  // Avoid repeated commentors
-  topLevelComments.map(({ channel_url }) => !commentorUris.includes(channel_url) && commentorUris.push(channel_url));
-
-  const getUnresolved = (uris) =>
-    uris.map((uri) => !makeSelectClaimForUri(uri)(state) && uri).filter((uri) => uri !== false);
-  const getCanonical = (uris) =>
-    uris
-      .map((uri) => makeSelectClaimForUri(uri)(state) && makeSelectClaimForUri(uri)(state).canonical_url)
-      .filter((uri) => Boolean(uri));
+  const maxComments = props.isLivestream ? MAX_LIVESTREAM_COMMENTS : -1;
+  const data = selectChannelMentionData(state, props.uri, maxComments);
 
   return {
-    commentorUris,
-    subscriptionUris,
-    unresolvedCommentors: getUnresolved(commentorUris),
-    unresolvedSubscriptions: getUnresolved(subscriptionUris),
-    canonicalCreator: getCanonical([props.creatorUri])[0],
-    canonicalCommentors: getCanonical(commentorUris),
-    canonicalSubscriptions: getCanonical(subscriptionUris),
+    commentorUris: data.commentorUris,
+    subscriptionUris: selectSubscriptionUris(state),
+    unresolvedCommentors: data.unresolvedCommentors,
+    unresolvedSubscriptions: data.unresolvedSubscriptions,
+    canonicalCreator: selectCanonicalUrlForUri(state, props.creatorUri),
+    canonicalCommentors: data.canonicalCommentors,
+    canonicalSubscriptions: data.canonicalSubscriptions,
     showMature: selectShowMatureContent(state),
   };
 };
