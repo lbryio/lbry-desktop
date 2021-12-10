8710b1334f5f0c3cd73546c6b3edb21e6f2c1a83
Thomas Zarebczan <thomas.zarebczan@gmail.com>
2021-12-10 12:34:00 -0500

    new player domain

--- ui/constants/livestream.js
+++ ui/constants/livestream.js
@@ -1,4 +1,4 @@
-export const LIVESTREAM_EMBED_URL = 'https://player.live.odysee.com/odysee';
+export const LIVESTREAM_EMBED_URL = 'https://player.odysee.live/odysee';
 export const LIVESTREAM_LIVE_API = 'https://api.live.odysee.com/v1/odysee/live';
 export const LIVESTREAM_REPLAY_API = 'https://api.live.odysee.com/v1/replays/odysee';
 export const LIVESTREAM_RTMP_URL = 'rtmp://stream.odysee.com/live';
