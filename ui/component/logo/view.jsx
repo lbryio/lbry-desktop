89d84e0776236d98787e51c44b4b66e78e1cbdc9
Rafael <rafael.saes@odysee.com>
2021-12-20 11:00:22 -0300

    Refactor Logo

--- ui/component/logo/view.jsx
+++ ui/component/logo/view.jsx
@@ -1,64 +1,39 @@
 // @flow
-import React from 'react';
-import * as ICONS from 'constants/icons';
-import { LOGO_TITLE, LOGO, LOGO_TEXT_LIGHT, LOGO_TEXT_DARK } from 'config';
-import Icon from 'component/common/icon';
+import { LOGO_TITLE, LOGO, LOGO_WHITE_TEXT, LOGO_DARK_TEXT } from 'config';
 import { useIsMobile } from 'effects/use-screensize';
+import * as ICONS from 'constants/icons';
+import Icon from 'component/common/icon';
+import React from 'react';
 
 type Props = {
-  type: string,
   currentTheme: string,
+  type: string,
 };
 
 export default function Logo(props: Props) {
-  const { type, currentTheme } = props;
+  const { currentTheme, type } = props;
+
   const isMobile = useIsMobile();
+  const isLightTheme = currentTheme === 'light';
+
   const defaultWithLabel = (
     <>
       <Icon icon={ICONS.LBRY} />
-      {/* @if TARGET='app' */}
-      <div className={'button__label'}>{'LBRY'}</div>
-      {/* @endif */}
-      {/* @if TARGET='web' */}
-      <div className={'button__label'}>{LOGO_TITLE}</div>
-      {/* @endif */}
+      <div className="button__label">{LOGO_TITLE}</div>
     </>
   );
 
-  if (type === 'small' || (isMobile && type !== 'embed')) {
-    return LOGO ? <img className={'header__navigation-logo'} src={LOGO} /> : <Icon icon={ICONS.LBRY} />;
-  } else if (type === 'embed') {
-    if (LOGO_TEXT_LIGHT) {
-      return (
-        <>
-          <img className={'embed__overlay-logo'} src={LOGO_TEXT_LIGHT} />
-        </>
-      );
-    } else {
-      return defaultWithLabel;
-    }
-  } else if (type === 'embed-ended') {
-    if (LOGO_TEXT_LIGHT) {
-      return (
-        <>
-          <img className={'embed__overlay-logo'} src={LOGO_TEXT_LIGHT} />
-        </>
-      );
-    } else {
-      return defaultWithLabel;
-    }
-  } else {
-    if (LOGO_TEXT_LIGHT && LOGO_TEXT_DARK) {
-      return (
-        <>
-          <img
-            className={'header__navigation-logo'}
-            src={currentTheme === 'light' ? LOGO_TEXT_DARK : LOGO_TEXT_LIGHT}
-          />
-        </>
-      );
-    } else {
-      return defaultWithLabel;
-    }
+  if (LOGO_WHITE_TEXT && (type === 'embed' || type === 'embed-ended')) {
+    return <img className="embed__overlay-logo" src={LOGO_WHITE_TEXT} />;
   }
+
+  if (type === 'small' || isMobile) {
+    return <img className="header__navigation-logo" src={LOGO} />;
+  }
+
+  if (LOGO_WHITE_TEXT && LOGO_DARK_TEXT) {
+    return <img className="header__navigation-logo" src={isLightTheme ? LOGO_DARK_TEXT : LOGO_WHITE_TEXT} />;
+  }
+
+  return defaultWithLabel;
 }
