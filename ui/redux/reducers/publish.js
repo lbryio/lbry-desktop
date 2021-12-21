9a0fde3f334e30332d74feed8b75451e2cad2cc4
infinite-persistence <64950861+infinite-persistence@users.noreply.github.com>
2021-12-20 17:25:15 -0800

    Publish: fix Resume button not appearing after 'conflict' (#543)
    ## Issue
    Apparently, a user is experiencing 423 locked errors from the server, which should not happen given the locking mechanism plus the user wasn't trying to do concurrent uploads.
    
    ## Fix
    Anyway, fix the Resume button so that at least they can try to resume.

--- ui/redux/reducers/publish.js
+++ ui/redux/reducers/publish.js
@@ -184,7 +184,7 @@ export const publishReducer = handleActions(
         }
       } else if (status) {
         currentUploads[key].status = status;
-        if (status === 'error') {
+        if (status === 'error' || status === 'conflict') {
           delete currentUploads[key].uploader;
         }
       }
