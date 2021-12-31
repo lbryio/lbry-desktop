e94f66a5fe2d0a9da0219e3ca05cf6e3cbaad0c0
Rafael <rafael.saes@odysee.com>
2021-12-31 08:45:16 -0300

    Fix stakedLevel looking looking for wrong prop

--- ui/component/livestreamComment/index.js
+++ ui/component/livestreamComment/index.js
@@ -2,10 +2,15 @@ import { connect } from 'react-redux';
 import { selectStakedLevelForChannelUri, selectClaimForUri, selectMyClaimIdsRaw } from 'redux/selectors/claims';
 import LivestreamComment from './view';
 
-const select = (state, props) => ({
-  claim: selectClaimForUri(state, props.uri),
-  stakedLevel: selectStakedLevelForChannelUri(state, props.authorUri),
-  myChannelIds: selectMyClaimIdsRaw(state),
-});
+const select = (state, props) => {
+  const { uri, comment } = props;
+  const { channel_url: authorUri } = comment;
+
+  return {
+    claim: selectClaimForUri(state, uri),
+    stakedLevel: selectStakedLevelForChannelUri(state, authorUri),
+    myChannelIds: selectMyClaimIdsRaw(state),
+  };
+};
 
 export default connect(select)(LivestreamComment);
