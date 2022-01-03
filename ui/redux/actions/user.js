d03f80c6b1780eebd80e35f3fae464dfa3b651c8
infinite-persistence <inf.persistence@gmail.com>
2022-01-03 17:12:18 +0800

    Skip 'install/new' if `web` already exists
    ## Issue
    Part of 385 "Defer api.odysee.com calls to their respective pages / install new"

--- ui/redux/actions/user.js
+++ ui/redux/actions/user.js
@@ -127,7 +127,7 @@ export function doAuthenticate(
           if (shareUsageData) {
             dispatch(doRewardList());
             dispatch(doFetchInviteStatus(false));
-            if (callInstall) {
+            if (callInstall && !user?.device_types?.includes('web')) {
               doInstallNew(appVersion, callbackForUsersWhoAreSharingData, DOMAIN);
             }
           }
