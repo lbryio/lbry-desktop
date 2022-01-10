e1909e55705e58c8dcb788e27606a8ec5ce8e407
infinite-persistence <inf.persistence@gmail.com>
2022-01-10 10:43:10 +0800

    File page: use compact view count (w/ tooltip for full res)

--- ui/component/fileViewCount/index.js
+++ ui/component/fileViewCount/index.js
@@ -1,13 +1,16 @@
 import { connect } from 'react-redux';
-import { selectClaimForUri } from 'redux/selectors/claims';
+import { selectClaimIdForUri } from 'redux/selectors/claims';
 import { doFetchViewCount, selectViewCountForUri } from 'lbryinc';
 import { doAnalyticsView } from 'redux/actions/app';
 import FileViewCount from './view';
 
-const select = (state, props) => ({
-  claim: selectClaimForUri(state, props.uri),
-  viewCount: selectViewCountForUri(state, props.uri),
-});
+const select = (state, props) => {
+  const claimId = selectClaimIdForUri(state, props.uri);
+  return {
+    claimId,
+    viewCount: selectViewCountForUri(state, props.uri),
+  };
+};
 
 const perform = (dispatch) => ({
   fetchViewCount: (claimId) => dispatch(doFetchViewCount(claimId)),
