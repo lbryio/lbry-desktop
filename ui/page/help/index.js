89b46265abb4c9ab0048a3813235bebcf383aba5
infinite-persistence <inf.persistence@gmail.com>
2022-01-19 20:47:43 +0800

    Help Page: remove 'app' to get rid of old 'accessToken' calls

--- ui/page/help/index.js
+++ ui/page/help/index.js
@@ -1,19 +1,3 @@
-import * as PAGES from 'constants/pages';
-import { connect } from 'react-redux';
-import { doFetchAccessToken } from 'redux/actions/user';
-import { selectAccessToken, selectUser } from 'redux/selectors/user';
-import { selectDaemonSettings } from 'redux/selectors/settings';
 import HelpPage from './view';
 
-const select = state => ({
-  user: selectUser(state),
-  accessToken: selectAccessToken(state),
-  deamonSettings: selectDaemonSettings(state),
-});
-
-const perform = (dispatch, ownProps) => ({
-  doAuth: () => ownProps.history.push(`/$/${PAGES.AUTH}?redirect=/$/${PAGES.HELP}`),
-  fetchAccessToken: () => dispatch(doFetchAccessToken()),
-});
-
-export default connect(select, perform)(HelpPage);
+export default HelpPage;
