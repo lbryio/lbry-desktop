b9b90263e0597c31fce0149b47274cb7016802b5
zeppi <jessopb@gmail.com>
2022-02-03 10:13:54 -0500

    rename commentserver files

--- ui/component/common/input-radio-panel.jsx
+++ ui/component/common/input-radio-panel.jsx
@@ -18,14 +18,17 @@ type Props = {
   [ https://myserver.com:50001 x (selected)]
  */
 
-const ItemPanel = (props: Props) => {
+const InputTogglePanel = (props: Props) => {
   const { onClick, active, serverDetails, onRemove } = props;
 
   return (
-    <div onClick={() => onClick(serverDetails)} className={classnames('itemPanel', { 'itemPanel--active': active })}>
-      <div className={'itemPanel__details'}>
-        <div className={'itemPanel__name'}>{`${serverDetails.name}`}</div>
-        <div className={'itemPanel__url'}>{`${serverDetails.url}`}</div>
+    <div
+      onClick={() => onClick(serverDetails)}
+      className={classnames('input-toggle-panel', { 'input-toggle-panel--active': active })}
+    >
+      <div className={'input-toggle-panel__details'}>
+        <div className={'input-toggle-panel__name'}>{`${serverDetails.name}`}</div>
+        <div className={'input-toggle-panel__url'}>{`${serverDetails.url}`}</div>
       </div>
       {onRemove && (
         <Button
@@ -40,4 +43,4 @@ const ItemPanel = (props: Props) => {
   );
 };
 
-export default ItemPanel;
+export default InputTogglePanel;
