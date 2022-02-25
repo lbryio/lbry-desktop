9665f7f4c219b4165b79f83ab262c9d0868fc12b
infinite-persistence <inf.persistence@gmail.com>
2022-02-25 18:34:53 +0800

    Add ability to highlight a Settings Row

--- ui/component/settingsRow/view.jsx
+++ ui/component/settingsRow/view.jsx
@@ -8,16 +8,18 @@ type Props = {
   multirow?: boolean, // Displays the Value widget(s) below the Label instead of on the right.
   useVerticalSeparator?: boolean, // Show a separator line between Label and Value. Useful when there are multiple Values.
   disabled?: boolean,
+  highlighted?: boolean,
   children?: React$Node,
 };
 
 export default function SettingsRow(props: Props) {
-  const { title, subtitle, multirow, useVerticalSeparator, disabled, children } = props;
+  const { title, subtitle, multirow, useVerticalSeparator, disabled, highlighted, children } = props;
   return (
     <div
       className={classnames('card__main-actions settings__row', {
         'section__actions--between': !multirow,
         'opacity-30': disabled,
+        'card--highlightedActive': highlighted,
       })}
     >
       <div className="settings__row--title">
