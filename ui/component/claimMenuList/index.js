b375a5e897b71c2ab1e1bcf3e236d1ce236edb01
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2022-03-08 13:56:49 -0300

    Small refactor to get the last used collection.

--- ui/component/claimMenuList/index.js
+++ ui/component/claimMenuList/index.js
@@ -3,6 +3,7 @@ import { selectClaimForUri, selectClaimIsMine } from 'redux/selectors/claims';
 import { doCollectionEdit, doFetchItemsInCollection } from 'redux/actions/collections';
 import { doPrepareEdit } from 'redux/actions/publish';
 import {
+  makeSelectCollectionForId,
   makeSelectCollectionForIdHasClaimUrl,
   makeSelectCollectionIsMine,
   makeSelectEditedCollectionForId,
@@ -44,7 +45,8 @@ const select = (state, props) => {
   const shuffleList = selectListShuffle(state);
   const shuffle = shuffleList && shuffleList.collectionId === collectionId && shuffleList.newUrls;
   const playNextUri = shuffle && shuffle[0];
-  const lastUsedCollection = selectLastUsedCollection(state);
+  const lastUsedCollectionId = selectLastUsedCollection(state);
+  const lastUsedCollection = makeSelectCollectionForId(lastUsedCollectionId)(state);
 
   return {
     claim,
@@ -73,12 +75,13 @@ const select = (state, props) => {
     resolvedList: makeSelectUrlsForCollectionId(collectionId)(state),
     playNextUri,
     lastUsedCollection,
-    hasClaimInLastUsedCollection:
-      lastUsedCollection && makeSelectCollectionForIdHasClaimUrl(lastUsedCollection.id, contentPermanentUri)(state),
+    hasClaimInLastUsedCollection: makeSelectCollectionForIdHasClaimUrl(
+      lastUsedCollectionId,
+      contentPermanentUri
+    )(state),
     lastUsedCollectionIsNotBuiltin:
-      lastUsedCollection &&
-      lastUsedCollection.id !== COLLECTIONS_CONSTS.WATCH_LATER_ID &&
-      lastUsedCollection.id !== COLLECTIONS_CONSTS.FAVORITES_ID,
+      lastUsedCollectionId !== COLLECTIONS_CONSTS.WATCH_LATER_ID &&
+      lastUsedCollectionId !== COLLECTIONS_CONSTS.FAVORITES_ID,
   };
 };
 
