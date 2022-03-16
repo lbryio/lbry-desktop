f8b54cabbc916978c8eded1b03ae35bf3a1cc8c7
infinite-persistence <inf.persistence@gmail.com>
2022-03-16 11:49:08 +0800

    FYP: update i18n entry for the readme

--- web/page/fyp/view.jsx
+++ web/page/fyp/view.jsx
@@ -15,10 +15,11 @@ export default function PageFyp() {
   No. The current algorithm itself has a tendency to favor channels with more viewers. But this is just because we have more evidence for those ones. Otherwise, we aren't making editorial decisions or picking favorites.
 
   ## My results suck, wtf?
-  The more videos you watch, the better it should be. But this system is brand new and it will take a bit of time to tune it. Please have patience. Or, if you want to complain or suggest something, please email: recommendations@odysee.com
+  The more videos you watch, the better it should be. But this system is brand new and it will take a bit of time to tune it. Please have patience. Or, if you want to complain or suggest something, please email: mailto:recommendations@odysee.com
 
   ## Why don't I have any recommendations?
-  Right now, it's a premium feature. But you also might not be using Odysee enough. It's hard to make recommendations without knowing much about you. Otherwise, if you use uBlock Origin or Brave, make sure they are disabled on Odysee, as they interfere with us learning what you like.`;
+  Right now, it's a Premium feature. But you also might not be using Odysee enough. It's hard to make recommendations without knowing much about you. Otherwise, if you use uBlock Origin or Brave, make sure they are disabled on Odysee, as they interfere with us learning what you like.`;
+
   return (
     <Page>
       <MarkdownPreview content={__(content)} simpleLinks />
