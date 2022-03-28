0f407914f35bc030707d73d5125e860a6e203753
Anthony <contact@anthonymayfield.com>
2022-03-28 15:21:53 +0200

    link livestream comments to membership page from badge

--- ui/component/livestreamComment/view.jsx
+++ ui/component/livestreamComment/view.jsx
@@ -120,7 +120,7 @@ export default function LivestreamComment(props: Props) {
           {isGlobalMod && <CommentBadge label={__('Admin')} icon={ICONS.BADGE_ADMIN} size={16} />}
           {isModerator && <CommentBadge label={__('Moderator')} icon={ICONS.BADGE_MOD} size={16} />}
           {isStreamer && <CommentBadge label={__('Streamer')} icon={ICONS.BADGE_STREAMER} size={16} />}
-          <PremiumBadge membership={odyseeMembership} />
+          <PremiumBadge membership={odyseeMembership} linkPage />
 
           {/* Use key to force timestamp update */}
           <DateTime date={timePosted} timeAgo key={forceUpdate} genericSeconds />
