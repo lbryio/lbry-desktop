89feddee0dc8acc5ce732dc8ab8a629727b0d61d
infinite-persistence <inf.persistence@gmail.com>
2022-04-04 13:51:45 +0800

    Publish: handle failed 'notify' at the server
    Ticket: 1256
    
    For `notify`, "file is currently locked" and "no such file or directory" is indication that the previous "failed" SDK call actually worked. Tell the user to check the transactions.
    
    This is the band aid until odysee-api/401 is addressed.

--- ui/constants/errors.js
+++ ui/constants/errors.js
@@ -1,2 +1,3 @@
 export const ALREADY_CLAIMED = 'once the invite reward has been claimed the referrer cannot be changed';
 export const REFERRER_NOT_FOUND = 'A odysee account could not be found for the referrer you provided.';
+export const PUBLISH_TIMEOUT_BUT_LIKELY_SUCCESSFUL = 'There was a network error, but the publish may have been completed. Wait a few minutes, then check your Uploads or Wallet page to confirm.';
