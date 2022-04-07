dff64f5a4781826aed77af428b55b9f0ba3d0f72
Rafael <rafael.saes@odysee.com>
2022-04-07 12:20:47 -0300

    Improve plugins format

--- ui/component/viewers/videoViewer/internal/plugins/videojs-mobile-ui/plugin.js
+++ ui/component/viewers/videoViewer/internal/plugins/videojs-mobile-ui/plugin.js
@@ -61,9 +61,7 @@ const onPlayerReady = (player, options) => {
     !player.el_.ownerDocument.querySelector('.bc-iframe')
   ) {
     player.tech_.el_.setAttribute('playsinline', 'playsinline');
-    player.tech_.supportsFullScreen = function () {
-      return false;
-    };
+    player.tech_.supportsFullScreen = () => false;
   }
 
   const controlBar = player.getChild('ControlBar');
@@ -150,14 +148,12 @@ const onPlayerReady = (player, options) => {
  *           Whether to disable when the video ends (e.g., if there is an endscreen)
  *           Never shows if the endscreen plugin is present
  */
-const mobileUi = function (options) {
+function mobileUi(options) {
   // if (videojs.browser.IS_ANDROID || videojs.browser.IS_IOS) {
   if (videojs.browser.IS_ANDROID) {
-    this.ready(() => {
-      onPlayerReady(this, videojs.mergeOptions(defaults, options));
-    });
+    this.ready(() => onPlayerReady(this, videojs.mergeOptions(defaults, options)));
   }
-};
+}
 
 // Register the plugin with video.js.
 registerPlugin('mobileUi', mobileUi);
