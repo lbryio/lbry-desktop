524eeff745f9aef9b115813025ce67ee7da1f4ef
Rafael <rafael.saes@odysee.com>
2022-04-20 15:38:55 -0300

    Move video quality constants

--- ui/constants/player.js
+++ ui/constants/player.js
@@ -1,1 +1,7 @@
 export const VIDEO_ALMOST_FINISHED_THRESHOLD = 0.8;
+
+// Quality Options
+export const AUTO = 'auto';
+export const ORIGINAL = 'original';
+
+export const VIDEO_QUALITY_OPTIONS = [AUTO, ORIGINAL, 144, 240, 360, 480, 720, 1080];
--- ui/constants/video.js
+++
@@ -1,5 +0,0 @@
-// Quality Options
-export const AUTO = 'auto';
-export const ORIGINAL = 'original';
-
-export const VIDEO_QUALITY_OPTIONS = [AUTO, ORIGINAL, 144, 240, 360, 480, 720, 1080];
