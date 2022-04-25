43c45c1f6235760a03ee1552399b63427c61ef7d
Kenneth C <kcrem@hfhosting.us>
2022-04-25 17:19:08 -0400

    Update LBRY.tv to just LBRY.
    Noticed that quite a few files still reference the defunct LBRY.tv, so now they should be simply "LBRY" or a similar relevant string.

--- extras/lbryinc/constants/errors.js
+++ extras/lbryinc/constants/errors.js
@@ -1,4 +1,4 @@
 export const ALREADY_CLAIMED =
   'once the invite reward has been claimed the referrer cannot be changed';
 export const REFERRER_NOT_FOUND =
-  'A lbry.tv account could not be found for the referrer you provided.';
+  'An LBRY account could not be found for the referrer you provided.';
