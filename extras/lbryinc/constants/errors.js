7abe50a21f3d74e3af56a7a6438e0db3b3795302
jessopb <36554050+jessopb@users.noreply.github.com>
2022-04-26 10:23:21 -0400

    Revert "Update LBRY.tv to just LBRY."
    This reverts commit 43c45c1f6235760a03ee1552399b63427c61ef7d.

--- extras/lbryinc/constants/errors.js
+++ extras/lbryinc/constants/errors.js
@@ -1,4 +1,4 @@
 export const ALREADY_CLAIMED =
   'once the invite reward has been claimed the referrer cannot be changed';
 export const REFERRER_NOT_FOUND =
-  'An LBRY account could not be found for the referrer you provided.';
+  'A lbry.tv account could not be found for the referrer you provided.';
