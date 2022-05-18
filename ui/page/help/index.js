d70f0f1d6dc4cbee431a23ff01b392dbe7520aeb
infinite-persistence <inf.persistence@gmail.com>
2022-05-18 17:14:28 +0800

    Add "What's New" button in the Help Page
    This allows user to re-access the modal if they accidentally dismissed it, never got it (bug?), or simply just want to see it again.

--- ui/page/help/index.js
+++ ui/page/help/index.js
@@ -1,3 +1,14 @@
 import HelpPage from './view';
+import { connect } from 'react-redux';
+import { doOpenModal } from 'redux/actions/app';
+import { selectHomepageAnnouncement } from 'redux/selectors/settings';
 
-export default HelpPage;
+const select = (state) => ({
+  announcement: selectHomepageAnnouncement(state),
+});
+
+const perform = {
+  doOpenModal,
+};
+
+export default connect(select, perform)(HelpPage);
