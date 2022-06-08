fab69450c0d828f9d9560280aba63f3850c8b02c
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2022-06-08 18:11:05 -0300

    Hide watched progress if claim is being played. (#7606)

--- ui/component/fileThumbnail/index.js
+++ ui/component/fileThumbnail/index.js
@@ -1,16 +1,25 @@
 import { connect } from 'react-redux';
 import { doResolveUri } from 'redux/actions/claims';
 import { makeSelectClaimForUri } from 'redux/selectors/claims';
-import { makeSelectContentWatchedPercentageForUri } from 'redux/selectors/content';
+import { selectPrimaryUri, makeSelectContentWatchedPercentageForUri } from 'redux/selectors/content';
 import CardMedia from './view';
 import { makeSelectClientSetting } from 'redux/selectors/settings';
 import * as SETTINGS from 'constants/settings';
 
 const select = (state, props) => {
+  const claimUriBeingPlayed = selectPrimaryUri(state);
+  let isBeingPlayed = false;
+
+  if (claimUriBeingPlayed) {
+    const claim = makeSelectClaimForUri(props.uri)(state);
+    const claimBeingPlayed = makeSelectClaimForUri(claimUriBeingPlayed)(state);
+    isBeingPlayed = claim.claim_id === claimBeingPlayed.claim_id;
+  }
+
   return {
     watchedPercentage: makeSelectContentWatchedPercentageForUri(props.uri)(state),
     claim: makeSelectClaimForUri(props.uri)(state),
-    showPercentage: makeSelectClientSetting(SETTINGS.PERSIST_WATCH_TIME)(state),
+    showPercentage: !isBeingPlayed && makeSelectClientSetting(SETTINGS.PERSIST_WATCH_TIME)(state),
   };
 };
 
