54ee4ee94ad7157fb50818a52f4eeb96ddc90b92
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2022-06-09 16:29:56 +0200

    Notification menu (#1652)
    * Save notification menu prototype
    
    * Add dynamic links
    
    * Add timestamps
    
    * Mark as seen on click
    
    * Fix guest mode
    
    * Fix discussion links & channel thumbnails
    
    * Adjust some details
    
    * Adjust theme
    
    * Replaxe Menu with MuiMenu
    
    * Fix Mui behavior & transitions
    
    * Adjust Mui menu behavior
    
    * Adjust some padding
    
    * Fix read & see
    
    * Clean code
    
    * Adjust border on top notification
    
    * Add case for comment replies
    
    * Save
    
    * Make alignment pixel perfect
    
    * Clean code
    
    * Adjust gif avatars, stickers & tips
    
    * Add delete function
    
    * Add delete icon hover effect
    
    * Add outline to delete icon
    
    * Fix seeNotification call
    
    * Add case for empty notification listä

--- ui/component/headerNotificationButton/view.jsx
+++ ui/component/headerNotificationButton/view.jsx
@@ -5,37 +5,229 @@ import { ENABLE_UI_NOTIFICATIONS } from 'config';
 import { useHistory } from 'react-router';
 import * as ICONS from 'constants/icons';
 import * as PAGES from 'constants/pages';
-import Button from 'component/button';
 import Icon from 'component/common/icon';
 import NotificationBubble from 'component/notificationBubble';
 import React from 'react';
 import Tooltip from 'component/common/tooltip';
+import { formatLbryUrlForWeb } from 'util/url';
+import Notification from 'component/notification';
+import DateTime from 'component/dateTime';
+import ChannelThumbnail from 'component/channelThumbnail';
+import { Menu as MuiMenu } from '@mui/material';
+import Button from 'component/button';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import { RULE } from 'constants/notifications';
+import UriIndicator from 'component/uriIndicator';
+import { generateNotificationTitle } from '../notification/helpers/title';
+import { generateNotificationText } from '../notification/helpers/text';
+import { parseURI } from 'util/lbryURI';
 
 type Props = {
+  notifications: Array<Notification>,
   unseenCount: number,
   user: ?User,
+  authenticated: boolean,
+  readNotification: (Array<number>) => void,
+  seeNotification: (Array<number>) => void,
+  deleteNotification: (number) => void,
   doSeeAllNotifications: () => void,
 };
 
 export default function NotificationHeaderButton(props: Props) {
-  const { unseenCount, user, doSeeAllNotifications } = props;
+  const {
+    notifications,
+    unseenCount,
+    user,
+    authenticated,
+    readNotification,
+    seeNotification,
+    deleteNotification,
+    doSeeAllNotifications,
+  } = props;
+  const list = notifications.slice(0, 5);
 
   const { push } = useHistory();
-  const notificationsEnabled = ENABLE_UI_NOTIFICATIONS || (user && user.experimental_ui);
+  const notificationsEnabled = authenticated && (ENABLE_UI_NOTIFICATIONS || (user && user.experimental_ui));
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [clicked, setClicked] = React.useState(false);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(!anchorEl ? event.currentTarget : null);
+  const handleClose = () => setAnchorEl(null);
+
+  const menuProps = {
+    id: 'notification-menu',
+    anchorEl,
+    open,
+    onClose: handleClose,
+    MenuListProps: {
+      'aria-labelledby': 'basic-button',
+      sx: { padding: 'var(--spacing-xs)' },
+    },
+    className: 'menu__list--header menu__list--notifications',
+    sx: { 'z-index': 2 },
+    PaperProps: { className: 'MuiMenu-list--paper' },
+    disableScrollLock: true,
+  };
+
+  const handleClickAway = () => {
+    if (!clicked) {
+      if (open) setClicked(true);
+    } else {
+      setAnchorEl(null);
+      setClicked(false);
+    }
+  };
+
+  const creatorIcon = (channelUrl, channelThumbnail) => (
+    <UriIndicator uri={channelUrl} link showAtSign channelInfo={{ uri: channelUrl, name: '' }}>
+      <ChannelThumbnail small thumbnailPreview={channelThumbnail} uri={channelThumbnail ? undefined : channelUrl} />
+    </UriIndicator>
+  );
 
   function handleMenuClick() {
     if (unseenCount > 0) doSeeAllNotifications();
     push(`/$/${PAGES.NOTIFICATIONS}`);
   }
 
+  function handleNotificationDelete(e, id) {
+    e.stopPropagation();
+    deleteNotification(id);
+  }
+
+  React.useEffect(() => {
+    if (!open) setClicked(false);
+  }, [open]);
+
   if (!notificationsEnabled) return null;
 
+  function handleNotificationClick(notification) {
+    const { id, notification_parameters, is_read } = notification;
+
+    if (!is_read) {
+      seeNotification([id]);
+      readNotification([id]);
+    }
+    let notificationLink = formatLbryUrlForWeb(notification_parameters.device.target);
+    if (notification_parameters.dynamic.hash) {
+      notificationLink += '?lc=' + notification_parameters.dynamic.hash + '&view=discussion';
+    }
+    push(notificationLink);
+  }
+
+  function menuEntry(notification) {
+    const { id, active_at, notification_rule, notification_parameters, is_read, type } = notification;
+
+    let channelUrl;
+    let icon;
+    switch (notification_rule) {
+      case RULE.CREATOR_SUBSCRIBER:
+        icon = <Icon icon={ICONS.SUBSCRIBE} sectionIcon />;
+        break;
+      case RULE.COMMENT:
+      case RULE.CREATOR_COMMENT:
+        channelUrl = notification_parameters.dynamic.comment_author;
+        icon = creatorIcon(channelUrl, notification_parameters?.dynamic?.comment_author_thumbnail);
+        break;
+      case RULE.COMMENT_REPLY:
+        channelUrl = notification_parameters.dynamic.reply_author;
+        icon = creatorIcon(channelUrl, notification_parameters?.dynamic?.comment_author_thumbnail);
+        break;
+      case RULE.NEW_CONTENT:
+        channelUrl = notification_parameters.dynamic.channel_url;
+        icon = creatorIcon(channelUrl, notification_parameters?.dynamic?.channel_thumbnail);
+        break;
+      case RULE.NEW_LIVESTREAM:
+        channelUrl = notification_parameters.dynamic.channel_url;
+        icon = creatorIcon(channelUrl, notification_parameters?.dynamic?.channel_thumbnail);
+        break;
+      case RULE.WEEKLY_WATCH_REMINDER:
+      case RULE.DAILY_WATCH_AVAILABLE:
+      case RULE.DAILY_WATCH_REMIND:
+      case RULE.MISSED_OUT:
+      case RULE.REWARDS_APPROVAL_PROMPT:
+        icon = <Icon icon={ICONS.LBC} sectionIcon />;
+        break;
+      case RULE.FIAT_TIP:
+        icon = <Icon icon={ICONS.FINANCE} sectionIcon />;
+        break;
+      default:
+        icon = <Icon icon={ICONS.NOTIFICATION} sectionIcon />;
+    }
+
+    let channelName;
+    if (channelUrl) {
+      try {
+        ({ claimName: channelName } = parseURI(channelUrl));
+      } catch (e) {}
+    }
+
+    return (
+      <>
+        <a onClick={() => handleNotificationClick(notification)}>
+          <div
+            className={
+              is_read ? 'menu__list--notification' : 'menu__list--notification menu__list--notification-unread'
+            }
+            key={id}
+          >
+            <div className="notification__icon">{icon}</div>
+            <div className="menu__list--notification-info">
+              <div className="menu__list--notification-type">
+                {generateNotificationTitle(notification_rule, notification_parameters, channelName)}
+              </div>
+              <div
+                className={
+                  type === 'comments' ? 'menu__list--notification-title blockquote' : 'menu__list--notification-title'
+                }
+              >
+                {generateNotificationText(notification_rule, notification_parameters)}
+              </div>
+              {!is_read && <span>•</span>}
+              <DateTime timeAgo date={active_at} />
+            </div>
+            <div className="delete-notification" onClick={(e) => handleNotificationDelete(e, id)}>
+              <Icon icon={ICONS.DELETE} sectionIcon />
+            </div>
+          </div>
+        </a>
+      </>
+    );
+  }
+
   return (
-    <Tooltip title={__('Notifications')}>
-      <Button onClick={handleMenuClick} className="header__navigationItem--icon">
-        <Icon size={18} icon={ICONS.NOTIFICATION} aria-hidden />
-        <NotificationBubble />
-      </Button>
-    </Tooltip>
+    notificationsEnabled && (
+      <>
+        <Tooltip title={__('Notifications')}>
+          <Button className="header__navigationItem--icon" onClick={handleClick}>
+            <Icon size={18} icon={ICONS.NOTIFICATION} aria-hidden />
+            <NotificationBubble />
+          </Button>
+        </Tooltip>
+
+        <ClickAwayListener onClickAway={handleClickAway}>
+          <MuiMenu {...menuProps}>
+            <div className="menu__list--notifications-header" />
+            <div className="menu__list--notifications-list">
+              {list.map((notification) => {
+                return menuEntry(notification);
+              })}
+              {list.length === 0 && (
+                <div className="menu__list--notification-empty">
+                  <div className="menu__list--notification-empty-title">{__('No notifications')}</div>
+                  <div className="menu__list--notification-empty-text">
+                    {__("You don't have any notifications yet, but they will be here when you do!")}
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <a onClick={handleMenuClick}>
+              <div className="menu__list--notifications-more">{__('View all')}</div>
+            </a>
+          </MuiMenu>
+        </ClickAwayListener>
+      </>
+    )
   );
 }
--- ui/component/headerProfileMenuButton/view.jsx
+++ ui/component/headerProfileMenuButton/view.jsx
@@ -15,6 +15,7 @@ import React from 'react';
 import Skeleton from '@mui/material/Skeleton';
 import ChannelSelector from 'component/channelSelector';
 import Button from 'component/button';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 type HeaderMenuButtonProps = {
   myChannelClaimIds: ?Array<string>,
@@ -28,6 +29,7 @@ export default function HeaderProfileMenuButton(props: HeaderMenuButtonProps) {
   const { myChannelClaimIds, activeChannelClaim, authenticated, email, signOut } = props;
 
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [clicked, setClicked] = React.useState(false);
   const open = Boolean(anchorEl);
   const handleClick = (event) => setAnchorEl(!anchorEl ? event.currentTarget : null);
   const handleClose = () => setAnchorEl(null);
@@ -36,6 +38,19 @@ export default function HeaderProfileMenuButton(props: HeaderMenuButtonProps) {
   // activeChannel will be: undefined = fetching, null = nothing, or { channel claim }
   const noActiveChannel = activeChannelUrl === null;
   const pendingChannelFetch = !noActiveChannel && myChannelClaimIds === undefined;
+
+  const handleClickAway = () => {
+    if (!clicked) {
+      if (open) setClicked(true);
+    } else {
+      setAnchorEl(null);
+      setClicked(false);
+    }
+  };
+
+  React.useEffect(() => {
+    if (!open) setClicked(false);
+  }, [open]);
 
   const menuProps = {
     id: 'basic-menu',
@@ -49,6 +64,7 @@ export default function HeaderProfileMenuButton(props: HeaderMenuButtonProps) {
     className: 'menu__list--header',
     sx: { 'z-index': 2 },
     PaperProps: { className: 'MuiMenu-list--paper' },
+    disableScrollLock: true,
   };
 
   return (
@@ -87,46 +103,50 @@ export default function HeaderProfileMenuButton(props: HeaderMenuButtonProps) {
           </Button>
         )}
         {authenticated ? (
-          <MuiMenu {...menuProps}>
-            <ChannelSelector storeSelection isHeaderMenu />
+          <ClickAwayListener onClickAway={handleClickAway}>
+            <MuiMenu {...menuProps}>
+              <ChannelSelector storeSelection isHeaderMenu />
 
-            <hr className="menu__separator" />
-            <HeaderMenuLink useMui page={PAGES.UPLOADS} icon={ICONS.PUBLISH} name={__('Uploads')} />
-            <HeaderMenuLink useMui page={PAGES.CHANNELS} icon={ICONS.CHANNEL} name={__('Channels')} />
-            <HeaderMenuLink
-              useMui
-              page={PAGES.CREATOR_DASHBOARD}
-              icon={ICONS.ANALYTICS}
-              name={__('Creator Analytics')}
-            />
+              <hr className="menu__separator" />
+              <HeaderMenuLink useMui page={PAGES.UPLOADS} icon={ICONS.PUBLISH} name={__('Uploads')} />
+              <HeaderMenuLink useMui page={PAGES.CHANNELS} icon={ICONS.CHANNEL} name={__('Channels')} />
+              <HeaderMenuLink
+                useMui
+                page={PAGES.CREATOR_DASHBOARD}
+                icon={ICONS.ANALYTICS}
+                name={__('Creator Analytics')}
+              />
 
-            <hr className="menu__separator" />
-            <HeaderMenuLink useMui page={PAGES.REWARDS} icon={ICONS.REWARDS} name={__('Rewards')} />
-            <HeaderMenuLink useMui page={PAGES.INVITE} icon={ICONS.INVITE} name={__('Invites')} />
-            <HeaderMenuLink useMui page={PAGES.ODYSEE_MEMBERSHIP} icon={ICONS.UPGRADE} name={__('Odysee Premium')} />
+              <hr className="menu__separator" />
+              <HeaderMenuLink useMui page={PAGES.REWARDS} icon={ICONS.REWARDS} name={__('Rewards')} />
+              <HeaderMenuLink useMui page={PAGES.INVITE} icon={ICONS.INVITE} name={__('Invites')} />
+              <HeaderMenuLink useMui page={PAGES.ODYSEE_MEMBERSHIP} icon={ICONS.UPGRADE} name={__('Odysee Premium')} />
 
-            <hr className="menu__separator" />
-            <HeaderMenuLink useMui page={PAGES.SETTINGS} icon={ICONS.SETTINGS} name={__('Settings')} />
-            <HeaderMenuLink useMui page={PAGES.HELP} icon={ICONS.HELP} name={__('Help')} />
+              <hr className="menu__separator" />
+              <HeaderMenuLink useMui page={PAGES.SETTINGS} icon={ICONS.SETTINGS} name={__('Settings')} />
+              <HeaderMenuLink useMui page={PAGES.HELP} icon={ICONS.HELP} name={__('Help')} />
 
-            <hr className="menu__separator" />
-            <MuiMenuItem onClick={signOut}>
-              <div className="menu__link" style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
-                <div className="menu__link-label">
-                  <Icon aria-hidden icon={ICONS.SIGN_OUT} />
-                  {__('Sign Out')}
+              <hr className="menu__separator" />
+              <MuiMenuItem onClick={signOut}>
+                <div className="menu__link" style={{ flexDirection: 'column', alignItems: 'flex-start' }}>
+                  <div className="menu__link-label">
+                    <Icon aria-hidden icon={ICONS.SIGN_OUT} />
+                    {__('Sign Out')}
+                  </div>
+                  <span className="menu__link-help">{email}</span>
                 </div>
-                <span className="menu__link-help">{email}</span>
-              </div>
-            </MuiMenuItem>
-          </MuiMenu>
+              </MuiMenuItem>
+            </MuiMenu>
+          </ClickAwayListener>
         ) : (
-          <MuiMenu {...menuProps}>
-            <HeaderMenuLink useMui page={PAGES.AUTH_SIGNIN} icon={ICONS.SIGN_IN} name={__('Log In')} />
-            <HeaderMenuLink useMui page={PAGES.AUTH} icon={ICONS.SIGN_UP} name={__('Sign Up')} />
-            <HeaderMenuLink useMui page={PAGES.SETTINGS} icon={ICONS.SETTINGS} name={__('Settings')} />
-            <HeaderMenuLink useMui page={PAGES.HELP} icon={ICONS.HELP} name={__('Help')} />
-          </MuiMenu>
+          <ClickAwayListener onClickAway={handleClickAway}>
+            <MuiMenu {...menuProps}>
+              <HeaderMenuLink useMui page={PAGES.AUTH_SIGNIN} icon={ICONS.SIGN_IN} name={__('Log In')} />
+              <HeaderMenuLink useMui page={PAGES.AUTH} icon={ICONS.SIGN_UP} name={__('Sign Up')} />
+              <HeaderMenuLink useMui page={PAGES.SETTINGS} icon={ICONS.SETTINGS} name={__('Settings')} />
+              <HeaderMenuLink useMui page={PAGES.HELP} icon={ICONS.HELP} name={__('Help')} />
+            </MuiMenu>
+          </ClickAwayListener>
         )}
       </div>
     </>
