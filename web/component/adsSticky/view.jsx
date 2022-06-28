d429ad7a9d0344393f5ff3aceb29328bf065ca12
infinite-persistence <inf.persistence@gmail.com>
2022-06-28 23:30:59 +0800

    Ad: remove css overrides -- start from scratch
    Most of the items will become obsolete as adnim provides us with the new version.
    
    Hard to tell which is ours and theirs ... hopefully I got it right

--- web/component/adsSticky/view.jsx
+++ web/component/adsSticky/view.jsx
@@ -51,18 +51,6 @@ export default function AdsSticky(props: Props) {
     return pathIsCategory || isChannelClaim || isContentClaim || pathname === '/';
   }
 
-  function closeSticky() {
-    const container = document.getElementsByClassName('AR_28')[0];
-    if (
-      container &&
-      container.parentElement &&
-      container.parentElement.parentElement &&
-      container.parentElement.parentElement.parentElement
-    ) {
-      container.parentElement.parentElement.parentElement.remove();
-    }
-  }
-
   React.useEffect(() => {
     if (shouldLoadSticky) {
       window.googletag = window.googletag || { cmd: [] };
@@ -79,14 +67,6 @@ export default function AdsSticky(props: Props) {
   React.useEffect(() => {
     const container = window[OUTBRAIN_CONTAINER_KEY];
     if (container) {
-      if (document.getElementsByClassName('close-sticky').length < 1) {
-        const closeButton = window.document.createElement('div');
-        closeButton.classList.add('close-sticky');
-        closeButton.innerHTML = '✖';
-        closeButton.onclick = closeSticky;
-        container.appendChild(closeButton);
-      }
-
       container.style.display = inAllowedPath ? '' : 'none';
     }
   }, [inAllowedPath, refresh]);
