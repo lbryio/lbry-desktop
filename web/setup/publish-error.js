1b4635f67486043a9bbfee5a1527adb18fb41881
infinite-persistence <inf.persistence@gmail.com>
2022-07-15 22:41:21 +0800

    (patch) Publish: add additional info in the error log
    Patch for d41b9ff1
    
    - Add os and browser details to confirm that the "failed to upload chunk" is due to timestamp changes (see Issue 631)

--- web/setup/publish-error.js
+++ web/setup/publish-error.js
@@ -1,14 +1,14 @@
 // @flow
 import * as tus from 'tus-js-client';
 import { COMMIT_ID } from 'config';
+import { platform } from 'util/platform';
 
 function generateCause(v1Type: ?string, xhr: ?any, uploader: ?TusUploader = null) {
   return {
     cause: {
-      appCommitId: COMMIT_ID,
+      app: `${COMMIT_ID ? COMMIT_ID.slice(0, 8) : '?'} | ${platform.os()} | ${platform.browser()}`,
       ...(xhr ? { xhrResponseURL: xhr.responseURL } : {}),
-      ...(xhr ? { xhrStatus: xhr.status } : {}),
-      ...(xhr ? { xhrStatusText: xhr.statusText } : {}),
+      ...(xhr ? { xhrStatus: `${xhr.status} (${xhr.statusText})` } : {}),
       ...(v1Type ? { type: v1Type } : {}),
       ...(uploader?._fingerprint ? { fingerprint: uploader?._fingerprint } : {}),
       ...(uploader?._retryAttempt ? { retryAttempt: uploader?._retryAttempt } : {}),
