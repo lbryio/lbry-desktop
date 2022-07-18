3284c5fc67fea4e71d60eab5280f244078ac0d0b
Rafael <rafael.saes@odysee.com>
2022-07-18 09:35:47 -0300

    Disable chat auto-open on mobile

--- ui/component/livestreamLayout/view.jsx
+++ ui/component/livestreamLayout/view.jsx
@@ -105,7 +105,7 @@ export default function LivestreamLayout(props: Props) {
             {isMobile && !isLandscapeRotated && !hideComments && (
               <React.Suspense fallback={null}>
                 <SwipeableDrawer
-                  startOpen
+                  // startOpen
                   type={DRAWERS.CHAT}
                   title={
                     <ChatModeSelector
