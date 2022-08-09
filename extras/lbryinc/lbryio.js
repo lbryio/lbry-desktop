2b6e86b9752b9b6aa51d60bea446717276307306
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2022-08-09 10:54:57 -0400

    Revert "Default api calls from 'get' to 'post'"
    This reverts commit 87d9e3e57371ce8eb99ee3923810a885b301568e.

--- extras/lbryinc/lbryio.js
+++ extras/lbryinc/lbryio.js
@@ -19,7 +19,7 @@ Lbryio.setLocalApi = (endpoint) => {
   Lbryio.CONNECTION_STRING = endpoint.replace(/\/*$/, '/'); // exactly one slash at the end;
 };
 
-Lbryio.call = (resource, action, params = {}, method = 'post') => {
+Lbryio.call = (resource, action, params = {}, method = 'get') => {
   if (!Lbryio.enabled) {
     return Promise.reject(new Error(__('LBRY internal API is disabled')));
   }
