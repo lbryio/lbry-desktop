029cce0c9792e0c277f9b456a45cdfb5404a40c2
Raphael Wickihalder <raphael.wickihalder@odysee.com>
2022-08-13 09:22:40 +0200

    Disable Twemoji parser in markdown text

--- ui/component/common/markdown-preview.jsx
+++ ui/component/common/markdown-preview.jsx
@@ -20,8 +20,8 @@ import remarkEmoji from 'remark-emoji';
 import remarkFrontMatter from 'remark-frontmatter';
 import remarkStrip from 'strip-markdown';
 import ZoomableImage from 'component/zoomableImage';
-import { TWEMOTEARRAY } from 'constants/emotes';
-const visit = require('unist-util-visit');
+// import { TWEMOTEARRAY } from 'constants/emotes';
+// const visit = require('unist-util-visit');
 const RE_EMOTE = /:\+1:|:-1:|:[\w-]+:/;
 
 function isEmote(title, src) {
@@ -77,6 +77,7 @@ const SimpleText = (props: SimpleTextProps) => {
   return <span>{props.children}</span>;
 };
 
+/*
 const remarkTwemoji = (tree) => {
   const RE_TWEMOJI = new RegExp(
     '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])'
@@ -113,6 +114,7 @@ const remarkTwemoji = (tree) => {
 
   return transformer;
 };
+*/
 
 // ****************************************************************************
 // ****************************************************************************
@@ -316,7 +318,7 @@ export default React.memo<MarkdownProps>(function MarkdownPreview(props: Markdow
           .use(disableTimestamps || isMarkdownPost ? null : inlineTimestamp)
           .use(disableTimestamps || isMarkdownPost ? null : formattedTimestamp)
           // Emojis
-          .use(remarkTwemoji)
+          // .use(remarkTwemoji)
           .use(inlineEmote)
           .use(formattedEmote) // :code:
           .use(remarkEmoji)
