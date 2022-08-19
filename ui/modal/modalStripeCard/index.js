1eeedf4005703f775b242d6c4bef637e813cd6b6
Rafael <rafael.saes@odysee.com>
2022-08-19 14:56:10 -0300

    fix: modal OK and Cancel doing the same thing

--- ui/modal/modalStripeCard/index.js
+++ ui/modal/modalStripeCard/index.js
@@ -1,11 +1,16 @@
 import { connect } from 'react-redux';
 import { doOpenModal, doHideModal } from 'redux/actions/app';
+import { selectHasSavedCard } from 'redux/selectors/stripe';
 
 import ModalStripeCard from './view';
+
+const select = (state) => ({
+  hasSavedCard: selectHasSavedCard(state),
+});
 
 const perform = {
   doOpenModal,
   doHideModal,
 };
 
-export default connect(null, perform)(ModalStripeCard);
+export default connect(select, perform)(ModalStripeCard);
--- ui/modal/modalStripeCard/view.jsx
+++ ui/modal/modalStripeCard/view.jsx
@@ -10,28 +10,31 @@ import StripeCard from 'component/settingsStripeCard';
 type Props = {
   previousModal?: string, // in case this modal was called from another modal
   previousProps?: any,
+  // -- redux --
+  hasSavedCard: boolean,
   doHideModal: () => void,
   doOpenModal: (modalType: string, props?: any) => void,
 };
 
 const ModalStripeCard = (props: Props) => {
-  const { previousModal, previousProps, doHideModal, doOpenModal } = props;
+  const { previousModal, previousProps, hasSavedCard, doHideModal, doOpenModal } = props;
 
-  function handleClose() {
+  function handleConfirm() {
     doHideModal();
+    // makes sense to open previous and continue only on confirm,
+    // in case I give up, close/cancel will just abort
     if (previousModal) doOpenModal(previousModal, previousProps);
   }
 
   return (
-    <Modal onAborted={handleClose} isOpen type="card">
+    <Modal onAborted={doHideModal} isOpen type="card">
       <Card
         title={__('Add your Card')}
         body={<StripeCard />}
         actions={
           <div className="section__actions">
-            <Button button="primary" label={__('OK')} onClick={handleClose} />
-
-            <Button button="link" label={__('Cancel')} onClick={handleClose} />
+            <Button button="primary" label={__('OK')} onClick={handleConfirm} disabled={!hasSavedCard} />
+            <Button button="link" label={__('Cancel')} onClick={doHideModal} />
           </div>
         }
       />
