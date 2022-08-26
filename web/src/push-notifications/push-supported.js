8e6021a53de39d9df327b8c571303278c7178cd9
infinite-persistence <inf.persistence@gmail.com>
2022-08-26 17:59:36 +0800

    Handle push-notification init exceptions
    sentry_24971

--- web/src/push-notifications/push-supported.js
+++ web/src/push-notifications/push-supported.js
@@ -4,13 +4,16 @@ import { isSupported } from 'firebase/messaging';
 
 export const isPushSupported = async (): Promise<boolean> => {
   if ('serviceWorker' in navigator) {
-    // Some browsers incognito expose sw but not the registration, while other don't expose sw at all.
-    // $FlowIssue[incompatible-type]
-    const activeRegistrations: Array<ServiceWorkerRegistration> = await navigator.serviceWorker.getRegistrations();
-    const swRegistered = activeRegistrations.length > 0;
-    const firebaseSupported = await isSupported();
-    const notificationFeature = 'Notification' in window;
-    return swRegistered && firebaseSupported && notificationFeature;
+    try {
+      // Some browsers incognito expose sw but not the registration, while other don't expose sw at all.
+      // $FlowIssue[incompatible-type]
+      const activeRegistrations: Array<ServiceWorkerRegistration> = await navigator.serviceWorker.getRegistrations();
+      const swRegistered = activeRegistrations.length > 0;
+      const firebaseSupported = await isSupported();
+      const notificationFeature = 'Notification' in window;
+      return swRegistered && firebaseSupported && notificationFeature;
+    } catch (e) {}
   }
+
   return false;
 };
