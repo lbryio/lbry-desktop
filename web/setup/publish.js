00bb59bd558bb518d5a8ca46e495f3d802de59d0
infinite-persistence <inf.persistence@gmail.com>
2022-09-07 20:16:50 +0800

    Publish: switch to v3 endpoint and file
    - The file is just a duplicate of v2 as a start ... changes coming in next commit.
    - Removed the custom `notify` error handling ... no longer required with v3 since we can query the status at will.

--- web/setup/publish.js
+++ web/setup/publish.js
@@ -3,8 +3,7 @@ import * as tus from 'tus-js-client';
 import { v4 as uuid } from 'uuid';
 import { generateError } from './publish-error';
 import { makeUploadRequest } from './publish-v1';
-import { makeResumableUploadRequest } from './publish-v2';
-import { PUBLISH_TIMEOUT_BUT_LIKELY_SUCCESSFUL, SDK_LEDGER_TRANSACTION_TIMEOUT } from 'constants/errors';
+import { makeResumableUploadRequest as makeUploadRequestV3 } from './publish-v3';
 
 // A modified version of Lbry.apiCall that allows
 // to perform calling methods at arbitrary urls
@@ -47,9 +46,7 @@ export default function apiPublishCallViaWeb(
   }
 
   const useV1 = remoteUrl || isMarkdown || preview || !tus.isSupported;
-
-  // Note: both function signature (params) should match.
-  const makeRequest = useV1 ? makeUploadRequest : makeResumableUploadRequest;
+  const makeRequest = useV1 ? makeUploadRequest : makeUploadRequestV3;
 
   return makeRequest(token, params, fileField, preview)
     .then((xhr) => {
@@ -63,17 +60,6 @@ export default function apiPublishCallViaWeb(
         if (xhr.status >= 200 && xhr.status < 300 && !xhr.response.error) {
           return resolve(xhr.response.result);
         } else if (xhr.response.error) {
-          if (xhr.responseURL.endsWith('/notify')) {
-            const errMsg = xhr.response.error.message;
-            if (
-              errMsg === 'file currently locked' || // until odysee-api/issues/401 is addressed
-              errMsg.endsWith('no such file or directory') || // until odysee-api/issues/401 is addressed
-              errMsg.startsWith(SDK_LEDGER_TRANSACTION_TIMEOUT)
-            ) {
-              return Promise.reject(generateError(PUBLISH_TIMEOUT_BUT_LIKELY_SUCCESSFUL, params, xhr));
-            }
-          }
-
           error = generateError(xhr.response.error.message, params, xhr);
         } else {
           error = generateError(__('Upload likely timed out. Try a smaller file while we work on this.'), params, xhr);
